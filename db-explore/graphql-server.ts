a73ab7ea8ef33bbdd938d3ae3e54a89a6d268ce8
Jonas Bengtsson <jonas.b@gmail.com>
2020-10-30 20:16:54 +0100

    Generate GraphQL schema from entity type definitions

--- db-explore/graphql-server.ts
+++ db-explore/graphql-server.ts
@@ -1,14 +1,93 @@
 #!/usr/bin/env npx ts-node
 var express = require('express');
 var { graphqlHTTP } = require('express-graphql');
-var { buildSchema } = require('graphql');
+import graphql, {
+  GraphQLEnumType,
+  GraphQLEnumValueConfigMap,
+  GraphQLFieldConfigMap,
+  GraphQLID,
+  GraphQLInterfaceType,
+  GraphQLNamedType,
+  GraphQLNonNull,
+  GraphQLObjectType,
+  GraphQLSchema,
+  GraphQLString,
+} from 'graphql';
+import * as TypeSpecifications from './TypeSpecifications';
+import { EntityFieldType } from './TypeSpecifications';
 
-// Construct a schema, using GraphQL schema language
-var schema = buildSchema(`
-  type Query {
-    hello: String
+function createSchema() {
+  const entityTypes = TypeSpecifications.getAllEntitySpecifications();
+
+  const types: GraphQLNamedType[] = [];
+
+  const nodeInterface = new GraphQLInterfaceType({
+    name: 'Node',
+    fields: {
+      id: { type: new GraphQLNonNull(GraphQLID) },
+    },
+  });
+  types.push(nodeInterface);
+
+  const entityTypeEnumValues: GraphQLEnumValueConfigMap = {};
+  entityTypes.forEach((et) => (entityTypeEnumValues[et.name] = {}));
+  const entityTypeEnum = new GraphQLEnumType({
+    name: 'EntityType',
+    values: entityTypeEnumValues,
+  });
+  types.push(entityTypeEnum);
+
+  const entityInterface = new GraphQLInterfaceType({
+    name: 'Entity',
+    interfaces: [nodeInterface],
+    fields: {
+      id: { type: new GraphQLNonNull(GraphQLID) },
+      type: { type: new GraphQLNonNull(entityTypeEnum) },
+      name: { type: GraphQLString },
+    },
+  });
+  types.push(entityInterface);
+
+  for (const entityType of entityTypes) {
+    const fields: GraphQLFieldConfigMap<any, any> = {
+      id: { type: new GraphQLNonNull(GraphQLID) },
+      type: { type: new GraphQLNonNull(entityTypeEnum) },
+      name: { type: GraphQLString },
+    };
+    for (const field of entityType.fields) {
+      switch (field.type) {
+        case EntityFieldType.BasicString:
+          fields[field.name] = { type: GraphQLString };
+          break;
+        case EntityFieldType.ReferenceSet:
+          continue; //TODO handle references
+        default:
+          throw new Error(`Unexpected type (${field.type})`);
+      }
+    }
+    types.push(
+      new GraphQLObjectType({
+        name: entityType.name,
+        interfaces: [nodeInterface, entityInterface],
+        fields,
+      })
+    );
   }
-`);
+
+  const queryType = new GraphQLObjectType({
+    name: 'Query',
+    fields: {
+      node: {
+        type: nodeInterface,
+        args: {
+          id: { type: new GraphQLNonNull(GraphQLID) },
+        },
+      },
+    },
+  });
+
+  return new GraphQLSchema({ query: queryType, types });
+}
 
 // The root provides a resolver function for each API endpoint
 var root = {
@@ -21,7 +100,7 @@ var app = express();
 app.use(
   '/graphql',
   graphqlHTTP({
-    schema: schema,
+    schema: createSchema(),
     rootValue: root,
     graphiql: true,
   })
