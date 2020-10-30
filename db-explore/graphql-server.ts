01d0264e42b56d968aecae055c5f2f2792afd387
Jonas Bengtsson <jonas.b@gmail.com>
2020-10-30 18:23:35 +0100

    Add example graphql server

---
+++ db-explore/graphql-server.ts
@@ -0,0 +1,30 @@
+#!/usr/bin/env npx ts-node
+var express = require('express');
+var { graphqlHTTP } = require('express-graphql');
+var { buildSchema } = require('graphql');
+
+// Construct a schema, using GraphQL schema language
+var schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+
+// The root provides a resolver function for each API endpoint
+var root = {
+  hello: () => {
+    return 'Hello world!';
+  },
+};
+
+var app = express();
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true,
+  })
+);
+app.listen(4000);
+console.log('Running a GraphQL API server at http://localhost:4000/graphql');
