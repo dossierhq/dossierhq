9a5d9fdd763a257c0e71d62629d43b4e54d5eb98
Jonas Bengtsson <jonas.b@gmail.com>
2020-11-03 16:01:21 +0100

    Cleanup benchmark and generation json

--- db-explore/BenchPress.ts
+++ db-explore/BenchPress.ts
@@ -21,6 +21,25 @@ interface BenchPressResult {
   successDuration: bigint;
 }
 
+interface BenchPressReportOptions {
+  name: string;
+  percentiles: number[];
+  folder: string;
+  baseName: string;
+}
+
+interface BenchPressProcessedResult {
+  iterationCount: number;
+  successCount: number;
+  failureCount: number;
+  successDuration_ms: number;
+  mean_ms: number;
+  min_ms: number;
+  max_ms: number;
+  percentiles_ms: Record<string, number>;
+  iterations_ms: number[];
+}
+
 function delay(delay_ms: number) {
   return new Promise((resolve) => setTimeout(resolve, delay_ms));
 }
@@ -95,10 +114,10 @@ function percentile_ms(resultSorted_ns: bigint[], percentile: number) {
   return ns_to_ms(resultSorted_ns[(resultSorted_ns.length * percentile) / 100]);
 }
 
-async function reportResult(
+function processResults(
   result: BenchPressResult,
-  options: { percentiles: number[]; folder: string; baseName: string }
-) {
+  options: BenchPressReportOptions
+): BenchPressProcessedResult {
   const resultSorted_ns = [...result.result].sort((a, b) => Number(a - b));
   const iterations_ms = result.result.map(ns_to_ms);
   const min_ms = ns_to_ms(resultSorted_ns[0]);
@@ -109,36 +128,93 @@ async function reportResult(
   const percentiles_ms = Object.fromEntries(
     options.percentiles.map((p) => [p, percentile_ms(resultSorted_ns, p)])
   );
+  return {
+    iterationCount: result.iterations,
+    successCount: result.successCount,
+    failureCount: result.iterations - result.successCount,
+    successDuration_ms: ns_to_ms(result.successDuration),
+    mean_ms,
+    min_ms,
+    max_ms,
+    percentiles_ms,
+    iterations_ms,
+  };
+}
 
-  console.log(
-    `Number of successful iterations: ${result.successCount}/${result.iterations}`
-  );
-  console.log(
-    `Number of failed iterations: ${result.iterations - result.successCount}`
-  );
-  console.log(
-    `Duration of successful iteration: ${ns_to_ms(result.successDuration)} ms`
-  );
-  console.log();
-  console.log(
-    `Avg iteration: ${mean_ms} ms (${ms_to_hz(mean_ms).toFixed(2)} ops/s)`
-  );
-  console.log(
-    `Min iteration: ${min_ms} ms (${ms_to_hz(min_ms).toFixed(2)} ops/s)`
-  );
-  for (const [percentile, duration_ms] of Object.entries(percentiles_ms)) {
-    console.log(
-      `Percentile ${percentile}: ${duration_ms} ms (${ms_to_hz(
-        duration_ms
-      ).toFixed(2)} ops/s)`
-    );
-  }
-  console.log(
-    `Max iteration: ${max_ms} ms (${ms_to_hz(max_ms).toFixed(2)} ops/s)`
-  );
+async function reportResult(
+  result: BenchPressResult,
+  options: BenchPressReportOptions
+) {
+  const processed = processResults(result, options);
+  reportResultConsole(processed);
 
   console.log();
   await fs.promises.mkdir(options.folder, { recursive: true });
+
+  await reportResultJson(processed, options);
+  await reportResultGnuPlot(processed, options);
+}
+
+function reportResultConsole(processed: BenchPressProcessedResult) {
+  console.log(
+    `Number of successful iterations: ${processed.successCount} / ${processed.iterationCount}`
+  );
+  console.log(`Number of failed iterations: ${processed.failureCount}`);
+  console.log(
+    `Duration of successful iteration: ${processed.successDuration_ms} ms`
+  );
+
+  function logMetric(name: string, duration_ms: number) {
+    console.log(
+      `${name}: ${duration_ms} ms (${ms_to_hz(duration_ms).toFixed(2)} ops/s)`
+    );
+  }
+
+  console.log();
+
+  logMetric('Avg iteration', processed.mean_ms);
+  logMetric('Min iteration', processed.min_ms);
+  for (const [percentile, duration_ms] of Object.entries(
+    processed.percentiles_ms
+  )) {
+    logMetric(`Percentile ${percentile}`, duration_ms);
+  }
+  logMetric('Max iteration', processed.max_ms);
+}
+
+async function reportResultJson(
+  processed: BenchPressProcessedResult,
+  options: BenchPressReportOptions
+) {
+  const percentileData: Record<string, number> = {};
+  for (const [percentile, duration_ms] of Object.entries(
+    processed.percentiles_ms
+  )) {
+    percentileData[`percentile_${percentile}`] = duration_ms;
+  }
+
+  const data = {
+    name: options.name,
+    unit: 'ms',
+    iterationCount: processed.iterationCount,
+    successCount: processed.successCount,
+    failureCount: processed.failureCount,
+    successDuration: processed.successDuration_ms,
+    mean: processed.mean_ms,
+    min: processed.min_ms,
+    ...percentileData,
+    max: processed.max_ms,
+  };
+
+  const jsonPath = path.join(options.folder, `${options.baseName}.json`);
+  console.log(`Writing to ${jsonPath}`);
+  await fs.promises.writeFile(jsonPath, JSON.stringify(data, null, 2));
+}
+
+async function reportResultGnuPlot(
+  processed: BenchPressProcessedResult,
+  options: BenchPressReportOptions
+) {
   const gnuPlotScriptPath = path.join(
     options.folder,
     `${options.baseName}.gnu`
@@ -156,21 +232,22 @@ set xlabel 'Iteration'
 set ylabel 'Duration (ms)'
 set yrange [0:]
 
-plot '${path.basename(gnuPlotDataPath)}' title '${options.baseName}',\\
-  ${mean_ms} title 'avg',\\
-  ${max_ms} title 'max',\\
-${Object.entries(percentiles_ms)
+plot '${path.basename(gnuPlotDataPath)}' title '${options.name}',\\
+  ${processed.mean_ms} title 'avg',\\
+  ${processed.max_ms} title 'max',\\
+${Object.entries(processed.percentiles_ms)
   .reverse()
   .map(([p, v_ms]) => `  ${v_ms} dashtype 3 title 'p${p}',\\`)
   .join('\n')}
-  ${min_ms} title 'min'`;
-  const gnuPlotData = iterations_ms.join('\n');
+  ${processed.min_ms} title 'min'`;
 
-  console.log(`Writing to ${gnuPlotScriptPath}...`);
+  const gnuPlotData = processed.iterations_ms.join('\n');
+
+  console.log(`Writing to ${gnuPlotScriptPath}`);
   await fs.promises.writeFile(gnuPlotScriptPath, gnuPlotScript);
-  console.log(`Writing to ${gnuPlotDataPath}...`);
+  console.log(`Writing to ${gnuPlotDataPath}`);
   await fs.promises.writeFile(gnuPlotDataPath, gnuPlotData);
-  console.log(`Executing gnuplot (generating ${gnuPlotPngPath})...`);
+  console.log(`Executing gnuplot (generating ${gnuPlotPngPath})`);
   await promisify(childProcess.execFile)('gnuplot', [gnuPlotScriptPath], {
     cwd: options.folder,
   });
@@ -190,10 +267,18 @@ async function main() {
     },
     { warmup: 5, iterations: 100 }
   );
+
+  // yyyy-mm-dd-hh-mm-ss
+  const timestamp = new Date()
+    .toISOString()
+    .replace(/[T:]/g, '-')
+    .replace(/\..+$/, '');
+
   await reportResult(result, {
+    name: 'test',
     percentiles: [50, 90, 95],
     folder: path.join(__dirname, 'output'),
-    baseName: 'test',
+    baseName: `test-${timestamp}`,
   });
 }
 
