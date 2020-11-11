a23cfa3d507566e924e3cf6adff09f85bffec0b5
Jonas Bengtsson <jonas.b@gmail.com>
2020-11-11 23:00:35 +0100

    Exctract Clock

--- benchpress/src/BenchPress.ts
+++ benchpress/src/BenchPress.ts
@@ -2,11 +2,8 @@ import childProcess from 'child_process';
 import fs from 'fs';
 import path from 'path';
 import { promisify } from 'util';
-
-export interface BenchPressClock {
-  start: () => void;
-  stop: () => void;
-}
+import type { BenchPressClock } from '.';
+import { createClock } from './Clock';
 
 export interface BenchPressOptions {
   testName: string;
@@ -46,42 +43,6 @@ interface BenchPressProcessedResult {
 
 export function delay(delay_ms: number) {
   return new Promise((resolve) => setTimeout(resolve, delay_ms));
-}
-
-function createClock() {
-  let startTime: bigint | null = null;
-  let duration: bigint | null = null;
-
-  return {
-    clock: {
-      start: () => {
-        if (startTime !== null) {
-          throw new Error('Called start() twice in a row');
-        } else {
-          startTime = process.hrtime.bigint();
-        }
-      },
-      stop: () => {
-        const now = process.hrtime.bigint();
-        if (startTime !== null) {
-          duration = now - startTime;
-        } else {
-          throw new Error('Called stop() before start()');
-        }
-      },
-    },
-    controlClock: {
-      reset: () => {
-        startTime = duration = null;
-      },
-      duration_ns: () => {
-        if (duration === null) {
-          throw new Error('Did not call clock.stop()');
-        }
-        return duration;
-      },
-    },
-  };
 }
 
 export async function runTest(
