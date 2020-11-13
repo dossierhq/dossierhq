337540865937f37fe112a5d8124a9ff19e0693e5
Jonas Bengtsson <jonas.b@gmail.com>
2020-11-13 18:27:14 +0100

    admin: support for deleteEntity

--- core/src/EntityAdmin.test.ts
+++ core/src/EntityAdmin.test.ts
@@ -82,3 +82,80 @@ describe('createEntity()', () => {
     expectErrorResult(result, ErrorType.BadRequest, 'Missing entity._name');
   });
 });
+
+describe('deleteEntity()', () => {
+  test('Delete & publish published BlogPost', async () => {
+    const createResult = await EntityAdmin.createEntity(
+      context,
+      { _type: 'BlogPost', _name: 'Delete', title: 'Delete' },
+      { publish: true }
+    );
+    if (expectOkResult(createResult)) {
+      const { id } = createResult.value;
+      const deleteResult = await EntityAdmin.deleteEntity(context, id, { publish: true });
+      if (expectOkResult(deleteResult)) {
+        const fetchResult = await PublishedEntity.getEntity(context, id);
+        expectErrorResult(fetchResult, ErrorType.NotFound, 'No such entity');
+      }
+    }
+  });
+
+  test('Delete & publish never published BlogPost', async () => {
+    const createResult = await EntityAdmin.createEntity(
+      context,
+      { _type: 'BlogPost', _name: 'Draft', title: 'Draft' },
+      { publish: false }
+    );
+    if (expectOkResult(createResult)) {
+      const { id } = createResult.value;
+      const deleteResult = await EntityAdmin.deleteEntity(context, id, { publish: true });
+      if (expectOkResult(deleteResult)) {
+        const fetchResult = await PublishedEntity.getEntity(context, id);
+        expectErrorResult(fetchResult, ErrorType.NotFound, 'No such entity');
+      }
+    }
+  });
+
+  test('Delete w/o publish published BlogPost', async () => {
+    const createResult = await EntityAdmin.createEntity(
+      context,
+      { _type: 'BlogPost', _name: 'Delete', title: 'Delete' },
+      { publish: true }
+    );
+    if (expectOkResult(createResult)) {
+      const { id } = createResult.value;
+      const deleteResult = await EntityAdmin.deleteEntity(context, id, { publish: false });
+      if (expectOkResult(deleteResult)) {
+        const fetchResult = await PublishedEntity.getEntity(context, id);
+        if (expectOkResult(fetchResult)) {
+          const { id: fetchId, ...entityExceptId } = fetchResult.value.item;
+          expect(fetchId).toBe(id);
+          expect(entityExceptId).toEqual({ _type: 'BlogPost', _name: 'Delete', title: 'Delete' });
+        }
+      }
+    }
+  });
+
+  test('Delete w/o publish never published BlogPost', async () => {
+    const createResult = await EntityAdmin.createEntity(
+      context,
+      { _type: 'BlogPost', _name: 'Draft', title: 'Draft' },
+      { publish: false }
+    );
+    if (expectOkResult(createResult)) {
+      const { id } = createResult.value;
+      const deleteResult = await EntityAdmin.deleteEntity(context, id, { publish: false });
+      if (expectOkResult(deleteResult)) {
+        const fetchResult = await PublishedEntity.getEntity(context, id);
+        expectErrorResult(fetchResult, ErrorType.NotFound, 'No such entity');
+      }
+    }
+  });
+
+  test('Error: Delete with invalid id', async () => {
+    const result = await EntityAdmin.deleteEntity(context, '6746350e-b38a-48ed-a8b3-9c81ca5f9d7b', {
+      publish: true,
+    });
+    expectErrorResult(result, ErrorType.NotFound, 'No such entity');
+  });
+});
