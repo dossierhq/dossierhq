6ddfefe9119c1754a6420d5221dce9f967a7d7b9
Jonas Bengtsson <jonas.b@gmail.com>
2020-11-18 19:17:45 +0100

    Use printSchema instead of introspection for testing

--- graphql/src/GraphQLSchemaGenerator.test.ts
+++ graphql/src/GraphQLSchemaGenerator.test.ts
@@ -1,56 +1,12 @@
 import { EntityFieldType, Schema } from '@datadata/core';
 import type { SchemaSpecification } from '@datadata/core';
-import { ExecutionResult, graphql, specifiedScalarTypes } from 'graphql';
+import { printSchema } from 'graphql';
 import { GraphQLSchemaGenerator } from './GraphQLSchemaGenerator';
-
-const schemaQuery = `
-query {
-  __schema {
-    types {
-      name
-      kind
-      interfaces {
-        name
-      }
-      fields {
-        name
-        type {
-          name
-          kind
-          ofType {
-            name
-          }
-        }
-      }
-      enumValues {
-        name
-      }
-    }
-  }
-}`;
-
-function isNameOfScalar(name: string) {
-  return specifiedScalarTypes.findIndex((x) => x.name === name) >= 0;
-}
-
-function cleanupSchemaResponse(response: ExecutionResult) {
-  const types = response.data?.__schema.types;
-  if (types) {
-    for (let i = types.length - 1; i >= 0; i -= 1) {
-      const type = types[i];
-      if (type.name.startsWith('__') || isNameOfScalar(type.name)) {
-        types.splice(i, 1);
-        continue;
-      }
-    }
-  }
-}
 
 async function describeGeneratedSchema(schemaSpec: SchemaSpecification) {
   const generator = new GraphQLSchemaGenerator(new Schema(schemaSpec));
-  const result = await graphql(generator.buildSchema(), schemaQuery);
-  cleanupSchemaResponse(result);
-  return result;
+  const graphQLSchema = generator.buildSchema();
+  return printSchema(graphQLSchema);
 }
 
 describe('Empty schema spec', () => {
