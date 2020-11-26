c1985d379ea0183984cebeafaf785aae4e9cdd89
Jonas Bengtsson <jonas.b@gmail.com>
2020-11-26 22:55:27 +0100

    gql: Resolve references in created result

--- graphql/src/Mutations.ts
+++ graphql/src/Mutations.ts
@@ -1,6 +1,7 @@
 import { EntityAdmin } from '@datadata/core';
 import type { AdminEntity, AdminEntityCreate } from '@datadata/core';
 import type { SessionGraphQLContext } from '.';
+import { buildResolversForAdminEntity } from './DataLoaders';
 import { getSessionContext } from './Utils';
 
 export async function createEntity<TContext extends SessionGraphQLContext>(
@@ -13,7 +14,7 @@ export async function createEntity<TContext extends SessionGraphQLContext>(
   if (result.isError()) {
     throw result.toError();
   }
-  return result.value;
+  return buildResolversForAdminEntity(sessionContext, result.value);
 }
 
 export async function deleteEntity<TContext extends SessionGraphQLContext>(
