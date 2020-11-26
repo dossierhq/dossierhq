a33a8fc7ebdaca54a67fecf4ba4b92b7a5568979
Jonas Bengtsson <jonas.b@gmail.com>
2020-11-26 22:40:14 +0100

    sql: Add support for createXxxEntity()

--- graphql/src/Mutations.ts
+++ graphql/src/Mutations.ts
@@ -1,7 +1,20 @@
 import { EntityAdmin } from '@datadata/core';
-import type { AdminEntity } from '@datadata/core';
+import type { AdminEntity, AdminEntityCreate } from '@datadata/core';
 import type { SessionGraphQLContext } from '.';
 import { getSessionContext } from './Utils';
+
+export async function createEntity<TContext extends SessionGraphQLContext>(
+  context: TContext,
+  entity: AdminEntityCreate,
+  publish: boolean
+): Promise<AdminEntity> {
+  const sessionContext = getSessionContext(context);
+  const result = await EntityAdmin.createEntity(sessionContext, entity, { publish });
+  if (result.isError()) {
+    throw result.toError();
+  }
+  return result.value;
+}
 
 export async function deleteEntity<TContext extends SessionGraphQLContext>(
   context: TContext,
