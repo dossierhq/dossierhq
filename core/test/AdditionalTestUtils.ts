095d0c515f19d02561fc1a83f18fbbc689bc4b23
Jonas Bengtsson <jonas.b@gmail.com>
2020-11-29 21:19:44 +0100

    Cleanup entity history

--- core/test/AdditionalTestUtils.ts
+++ core/test/AdditionalTestUtils.ts
@@ -1,10 +1,10 @@
-import type { EntityHistory } from '../src';
+import type { AdminEntityHistory } from '../src';
 
 export const uuidMatcher = /[a-f0-9]{8}-[a-f0-9]{4}-4[a-f0-9]{3}-[89aAbB][a-f0-9]{3}-[a-f0-9]{12}/;
 
 export function expectEntityHistoryVersions(
-  actual: EntityHistory,
-  expectedVersions: Omit<EntityHistory['versions'][0], 'createdAt'>[]
+  actual: AdminEntityHistory,
+  expectedVersions: Omit<AdminEntityHistory['versions'][0], 'createdAt'>[]
 ): void {
   // Skip createdAt since dates are unpredictable
   const actualVersions = actual.versions.map((x) => {
