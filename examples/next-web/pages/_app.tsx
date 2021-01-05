e5d247559b523c4840194bc27cbcbcf6fe96640b
Jonas Bengtsson <jonas.b@gmail.com>
2021-01-05 19:00:11 +0100

    next-web: fix eslint

--- examples/next-web/pages/_app.tsx
+++ examples/next-web/pages/_app.tsx
@@ -1,6 +1,13 @@
 import '../styles/globals.css';
 
-function MyApp({ Component, pageProps }) {
+function MyApp({
+  Component,
+  pageProps,
+}: {
+  Component: React.ComponentClass;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  pageProps: any;
+}): JSX.Element {
   return <Component {...pageProps} />;
 }
 
