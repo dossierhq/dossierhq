4fba7646f8a24a157d8af206b8d392d9d6f062ba
Jonas Bengtsson <jonas.b@gmail.com>
2021-01-07 23:48:19 +0100

    next-web: Fix usage of hooks
    https://www.npmjs.com/package/next-transpile-modules\#i-have-trouble-with-duplicated-dependencies-or-the-invalid-hook-call-error-in-react

--- examples/next-web/next.config.js
+++ examples/next-web/next.config.js
@@ -1,7 +1,15 @@
+const path = require('path');
+
 const withTranspileModules = require('next-transpile-modules')([
   '@datadata/admin-react-components',
   '@datadata/core',
   '@datadata/server',
 ]);
 
-module.exports = withTranspileModules({ reactStrictMode: true });
+module.exports = withTranspileModules({
+  reactStrictMode: true,
+  webpack: (config) => {
+    config.resolve.alias['react'] = path.resolve(__dirname, '.', 'node_modules', 'react');
+    return config;
+  },
+});
