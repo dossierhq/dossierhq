845bc1c986494bbab80aa3b2aecd6143a790669e
Jonas Bengtsson <jonas.b@gmail.com>
2021-01-25 10:20:54 +0100

    arc: Fix tests by  making ids predictable

--- admin-react-components/src/EntityFieldEditor/EntityFieldEditor.tsx
+++ admin-react-components/src/EntityFieldEditor/EntityFieldEditor.tsx
@@ -4,14 +4,22 @@ import { isStringField } from '@datadata/core';
 import { FormField, InputText } from '..';
 
 interface Props {
+  idPrefix: string;
   fieldSpec: FieldSpecification;
   value: unknown;
   onValueChanged: (value: unknown) => void;
 }
 
-export function EntityFieldEditor({ fieldSpec, value, onValueChanged }: Props): JSX.Element {
+export function EntityFieldEditor({
+  idPrefix,
+  fieldSpec,
+  value,
+  onValueChanged,
+}: Props): JSX.Element {
+  const id = `${idPrefix}-${fieldSpec.name}`;
   return (
     <FormField
+      controlId={id}
       label={fieldSpec.name}
       render={({ id }) => {
         if (isStringField(fieldSpec, value)) {
