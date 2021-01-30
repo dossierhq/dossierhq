674adc08e42a74a5d28c8e52eb30d44c96b11d2d
Jonas Bengtsson <jonas.b@gmail.com>
2021-01-30 23:37:10 +0100

    arc: Automate icons generation

---
+++ admin-react-components/scripts/generate-icons-css.mjs
@@ -0,0 +1,54 @@
+#!/usr/bin/env node
+import fs from 'fs/promises';
+import svgToDataUri from 'mini-svg-data-uri';
+import path from 'path';
+import SVGO from 'svgo';
+
+const svgo = new SVGO();
+
+const config = {
+  inputDir: new URL('../icons/export/', import.meta.url).pathname,
+  iconsCssPath: new URL('../src/icons.css', import.meta.url).pathname,
+};
+
+async function loadIcons() {
+  const result = [];
+  const files = await fs.readdir(config.inputDir);
+
+  for (const file of files) {
+    if (!file.endsWith('.svg')) {
+      continue;
+    }
+    const name = path.basename(file, '.svg');
+    const svg = await fs.readFile(path.join(config.inputDir, file), { encoding: 'utf8' });
+    result.push({ name, svg });
+  }
+  return result;
+}
+
+async function optimizeSvg(svg) {
+  return (await svgo.optimize(svg)).data;
+}
+
+async function writeToCss(icons) {
+  let result = '';
+  for (const { name, svg } of icons) {
+    const optimizedSvg = await optimizeSvg(svg);
+    console.log(`> ${name} ${optimizedSvg.length}b (original ${svg.length}b)`);
+    if (result) result += '\n';
+    result += `.dd.icon.${name} {\n`;
+    result += `  background-image: url("${svgToDataUri(optimizedSvg)}");\n`;
+    result += '}\n';
+  }
+  await fs.writeFile(config.iconsCssPath, result, 'utf8');
+}
+
+async function main() {
+  const icons = await loadIcons();
+  await writeToCss(icons);
+}
+
+main().catch((error) => {
+  console.warn(error);
+  process.exitCode = 1;
+});
