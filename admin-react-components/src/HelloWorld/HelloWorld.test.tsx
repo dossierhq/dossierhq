aebb958daeb730c8b6247bd28dc43ceef09b724d
Jonas Bengtsson <jonas.b@gmail.com>
2021-01-31 16:30:46 +0100

    arc: Remove HelloWorld

--- admin-react-components/src/HelloWorld/HelloWorld.test.tsx
+++
@@ -1,8 +0,0 @@
-import * as React from 'react';
-import * as renderer from 'react-test-renderer';
-import { Normal } from './HelloWorld.stories';
-
-test('Normal', () => {
-  const tree = renderer.create(<Normal />).toJSON();
-  expect(tree).toMatchSnapshot();
-});
