75dcf79b28185e83760e70eb3bb1cf57f6f77803
Jonas Bengtsson <jonas.b@gmail.com>
2021-01-31 19:30:57 +0100

    arc: Enable using entity picker in storyboard

--- admin-react-components/src/StoryboardSchema.ts
+++
@@ -1,8 +0,0 @@
-import { FieldType, Schema } from '@datadata/core';
-
-const schema = new Schema({
-  entityTypes: [{ name: 'Foo', fields: [{ name: 'title', type: FieldType.String, isName: true }] }],
-  valueTypes: [],
-});
-
-export default schema;
