a5ad944b4459f188b1fb94b1992af6af1cf35707
Jonas Bengtsson <jonas.b@gmail.com>
2021-02-12 21:26:44 +0100

    next-web: Enable creating new entity

---
+++ examples/next-web/pages/api/entities/index.ts
@@ -0,0 +1,29 @@
+import { EntityAdmin } from '@datadata/server';
+import type { NextApiRequest, NextApiResponse } from 'next';
+import type { EntityCreateRequest } from '../../../types/RequestTypes';
+import type { EntityResponse } from '../../../types/ResponseTypes';
+import * as BackendUtils from '../../../utils/BackendUtils';
+import { errorResultToBoom } from '../../../utils/ErrorUtils';
+import { handlePostAsync } from '../../../utils/HandlerUtils';
+import { getServerConnection, getSessionContextForRequest } from '../../../utils/ServerUtils';
+
+export default async (req: NextApiRequest, res: NextApiResponse<EntityResponse>): Promise<void> => {
+  await handlePostAsync(req, res, async (body: EntityCreateRequest) => {
+    const { authContext, server } = await getServerConnection();
+    const authResult = await getSessionContextForRequest(server, authContext, req);
+    if (authResult.isError()) {
+      throw errorResultToBoom(authResult);
+    }
+    const context = authResult.value;
+
+    const result = await EntityAdmin.createEntity(context, body.item, { publish: false });
+
+    if (result.isError()) {
+      throw errorResultToBoom(result);
+    }
+    return {
+      location: BackendUtils.urls.getEntity(result.value.id, {}),
+      body: { item: result.value },
+    };
+  });
+};
--- examples/next-web/pages/entities/[id].tsx
+++ examples/next-web/pages/entities/[id].tsx
@@ -8,16 +8,25 @@ import type { EntityResponse } from '../../types/ResponseTypes';
 import { fetchJsonAsync, urls } from '../../utils/BackendUtils';
 import { validateQuery } from '../../utils/PageUtils';
 
-interface RouterQuery {
-  id: string;
+interface NewEntity {
+  _type: string;
 }
-const routerSchema = Joi.object<RouterQuery>({ id: Joi.string().required() });
+
+interface RouterQuery {
+  id: 'new' | string;
+  type?: string;
+}
+const routerSchema = Joi.object<RouterQuery>({ id: Joi.string().required(), type: Joi.string() });
 
 function PageContent({ query }: { query: RouterQuery }) {
   const { contextValue } = useInitializeContext();
-  const [entity, setEntity] = useState<AdminEntity | null>(null);
+  const [entity, setEntity] = useState<NewEntity | AdminEntity | null>(null);
 
   useEffect(() => {
+    if (query.id === 'new' && query.type) {
+      setEntity({ _type: query.type });
+      return;
+    }
     (async () => {
       try {
         const entityResponse = await fetchJsonAsync<EntityResponse>(urls.getEntity(query.id, {}));
@@ -26,21 +35,30 @@ function PageContent({ query }: { query: RouterQuery }) {
         console.warn(error);
       }
     })();
-  }, [query.id]);
+  }, [query.id, query.type]);
 
   const handleSubmit = useCallback(
     (entity: AdminEntityCreate | AdminEntityUpdate) => {
       (async () => {
         try {
-          //TODO handle create
           //TODO entity includes id on update, so always POST?
           //TODO version
           //TODO use context
-          await fetchJsonAsync<EntityResponse>(urls.getEntity(query.id, {}), {
-            method: 'PUT',
-            headers: { 'content-type': 'application/json' },
-            body: JSON.stringify({ item: entity }),
-          });
+          if (query.id === 'new') {
+            const response = await fetchJsonAsync<EntityResponse>(urls.createEntity, {
+              method: 'POST',
+              headers: { 'content-type': 'application/json' },
+              body: JSON.stringify({ item: entity }),
+            });
+            setEntity(response.item);
+          } else {
+            const response = await fetchJsonAsync<EntityResponse>(urls.getEntity(query.id, {}), {
+              method: 'PUT',
+              headers: { 'content-type': 'application/json' },
+              body: JSON.stringify({ item: entity }),
+            });
+            setEntity(response.item);
+          }
         } catch (error) {
           console.warn(error);
         }
