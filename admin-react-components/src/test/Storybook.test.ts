1aa28ffe5e6f55df272463eef911e1b0ca8241eb
Jonas Bengtsson <jonas.b@gmail.com>
2021-02-15 17:18:42 +0100

    arc: Change how storyshots are stored, to prepare for async components

--- admin-react-components/src/test/Storybook.test.ts
+++ admin-react-components/src/test/Storybook.test.ts
@@ -1,2 +1,30 @@
-import initStoryshots from '@storybook/addon-storyshots';
-initStoryshots();
+import initStoryshots, { Stories2SnapsConverter } from '@storybook/addon-storyshots';
+import { act, create, ReactTestRenderer } from 'react-test-renderer';
+
+const converter = new Stories2SnapsConverter();
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const runTest = async (story: any, context: any) => {
+  const filename = converter.getSnapshotFileName(context);
+
+  if (!filename) {
+    return;
+  }
+
+  const storyElement = story.render();
+  let tree: ReactTestRenderer | undefined;
+  await act(async () => {
+    tree = create(storyElement);
+  });
+
+  expect(tree?.toJSON()).toMatchSpecificSnapshot(filename);
+
+  tree?.unmount();
+};
+
+initStoryshots({
+  asyncJest: true,
+  test: ({ story, context, done }) => {
+    runTest(story, context).then(done);
+  },
+});
