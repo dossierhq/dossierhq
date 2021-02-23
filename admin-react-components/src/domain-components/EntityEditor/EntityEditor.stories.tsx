955b6c7ca57d285e835e90f13e2d29efccef1cad
Jonas Bengtsson <jonas.b@gmail.com>
2021-02-23 10:25:26 +0100

    arc: Extract SlowTestContextValue

--- admin-react-components/src/domain-components/EntityEditor/EntityEditor.stories.tsx
+++ admin-react-components/src/domain-components/EntityEditor/EntityEditor.stories.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import type { DataDataContextValue, EntityEditorProps } from '../..';
 import { DataDataContext, EntityEditor } from '../..';
 import { foo1Id, fooDeletedId } from '../../test/EntityFixtures';
+import { SlowTestContextValue } from '../../test/SlowTestContextValue';
 import TestContextValue from '../../test/TestContextValue';
 
 export default {
@@ -19,34 +20,6 @@ const Template: Story<EntityEditorProps & { contextValue?: DataDataContextValue 
   );
 };
 
-class SlowTestContextValue implements DataDataContextValue {
-  #inner = new TestContextValue();
-
-  schema = this.#inner.schema;
-
-  delay() {
-    return new Promise((resolve) => setTimeout(resolve, 1000));
-  }
-
-  useEntity: DataDataContextValue['useEntity'] = (id, options) => {
-    return this.#inner.useEntity(id, options);
-  };
-
-  useSearchEntities: DataDataContextValue['useSearchEntities'] = (query, paging) => {
-    return this.#inner.useSearchEntities(query, paging);
-  };
-
-  createEntity: DataDataContextValue['createEntity'] = async (entity, options) => {
-    await this.delay();
-    return await this.#inner.createEntity(entity, options);
-  };
-
-  updateEntity: DataDataContextValue['updateEntity'] = async (entity, options) => {
-    await this.delay();
-    return await this.#inner.updateEntity(entity, options);
-  };
-}
-
 export const NewFoo = Template.bind({});
 NewFoo.args = { entity: { type: 'Foo', isNew: true }, idPrefix: 'new-entity-123' };
 
@@ -57,7 +30,10 @@ export const DeletedFoo = Template.bind({});
 DeletedFoo.args = { entity: { id: fooDeletedId } };
 
 export const SlowFullFoo = Template.bind({});
-SlowFullFoo.args = { entity: { id: foo1Id }, contextValue: new SlowTestContextValue() };
+SlowFullFoo.args = {
+  entity: { id: foo1Id },
+  contextValue: new SlowTestContextValue(new TestContextValue()),
+};
 
 export const NotFound = Template.bind({});
 NotFound.args = { entity: { id: 'c6f97fae-1213-4be0-996f-4f20c7da7e65' } };
---
+++ admin-react-components/src/test/SlowTestContextValue.ts
@@ -0,0 +1,34 @@
+import type { Schema } from '@datadata/core';
+import type { DataDataContextValue } from '..';
+
+export class SlowTestContextValue implements DataDataContextValue {
+  schema: Schema;
+  #inner: DataDataContextValue;
+
+  constructor(inner: DataDataContextValue) {
+    this.#inner = inner;
+    this.schema = inner.schema;
+  }
+
+  private delay() {
+    return new Promise((resolve) => setTimeout(resolve, 1000));
+  }
+
+  useEntity: DataDataContextValue['useEntity'] = (id, options) => {
+    return this.#inner.useEntity(id, options);
+  };
+
+  useSearchEntities: DataDataContextValue['useSearchEntities'] = (query, paging) => {
+    return this.#inner.useSearchEntities(query, paging);
+  };
+
+  createEntity: DataDataContextValue['createEntity'] = async (entity, options) => {
+    await this.delay();
+    return await this.#inner.createEntity(entity, options);
+  };
+
+  updateEntity: DataDataContextValue['updateEntity'] = async (entity, options) => {
+    await this.delay();
+    return await this.#inner.updateEntity(entity, options);
+  };
+}
