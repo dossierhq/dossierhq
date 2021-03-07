2d875173b67e7e632e45d32fbe2fdf2767dc4bc3
Jonas Bengtsson <jonas.b@gmail.com>
2021-03-07 21:43:17 +0100

    testing-utils: Refactor InMemoryServer

---
+++ testing-utils/src/InMemoryAdmin.ts
@@ -0,0 +1,120 @@
+import type {
+  AdminEntity,
+  AdminEntityCreate,
+  AdminEntityHistory,
+  AdminEntityUpdate,
+  AdminQuery,
+  Connection,
+  Edge,
+  ErrorType,
+  Location,
+  Paging,
+  PromiseResult,
+} from '@datadata/core';
+import { isLocationItemField, notOk, ok, visitFieldsRecursively } from '@datadata/core';
+import { v4 as uuidv4 } from 'uuid';
+import type { InMemorySessionContext } from '.';
+
+export const InMemoryAdmin = {
+  getEntity: async (
+    context: InMemorySessionContext,
+    id: string,
+    options: { version?: number | null }
+  ): PromiseResult<{ item: AdminEntity }, ErrorType.NotFound> => {
+    const item = context.server.getEntity(id, options.version);
+    if (item) {
+      return ok({ item });
+    }
+    return notOk.NotFound('No such entity or version');
+  },
+
+  getEntityHistory: async (
+    context: InMemorySessionContext,
+    id: string
+  ): PromiseResult<AdminEntityHistory, ErrorType.NotFound> => {
+    const history = context.server.getEntityHistory(id);
+    if (!history) {
+      return notOk.NotFound('No such entity');
+    }
+    return ok(history);
+  },
+
+  searchEntities: async (
+    context: InMemorySessionContext,
+    query?: AdminQuery,
+    paging?: Paging
+  ): PromiseResult<Connection<Edge<AdminEntity, ErrorType>> | null, ErrorType.BadRequest> => {
+    const entities = context.server.getLatestEntities().filter((entity) => {
+      if (
+        query?.entityTypes?.length &&
+        query.entityTypes.length > 0 &&
+        query.entityTypes.indexOf(entity._type) < 0
+      ) {
+        return false;
+      }
+      if (query?.boundingBox) {
+        const { minLat, maxLat, minLng, maxLng } = query.boundingBox;
+        const locations: Location[] = [];
+        visitFieldsRecursively({
+          schema: context.server.schema,
+          entity,
+          visitField: (path, fieldSpec, data, unusedVisitContext) => {
+            if (isLocationItemField(fieldSpec, data) && data) {
+              locations.push(data);
+            }
+          },
+          initialVisitContext: undefined,
+        });
+
+        if (
+          !locations.find(
+            ({ lat, lng }) => lat >= minLat && lat <= maxLat && lng >= minLng && lng <= maxLng
+          )
+        ) {
+          return false;
+        }
+      }
+      return true;
+    });
+    if (entities.length === 0) {
+      return ok(null);
+    }
+    return ok({
+      pageInfo: {
+        hasPreviousPage: false,
+        hasNextPage: false,
+        startCursor: entities[0].id,
+        endCursor: entities[entities.length - 1].id,
+      },
+      edges: entities.map((entity) => ({ cursor: entity.id, node: ok(entity) })),
+    });
+  },
+
+  createEntity: async (
+    context: InMemorySessionContext,
+    entity: AdminEntityCreate,
+    options: { publish: boolean }
+  ): PromiseResult<AdminEntity, ErrorType.BadRequest> => {
+    const newEntity = { ...entity, id: uuidv4(), _version: 0 };
+    context.server.addNewEntity(newEntity, context.userId);
+    return ok(newEntity);
+  },
+
+  updateEntity: async (
+    context: InMemorySessionContext,
+    entity: AdminEntityUpdate,
+    options: { publish: boolean }
+  ): PromiseResult<AdminEntity, ErrorType.BadRequest | ErrorType.NotFound> => {
+    const previousVersion = context.server.getEntity(entity.id);
+    if (!previousVersion) {
+      return notOk.NotFound('No such entity');
+    }
+    const newEntity = {
+      ...previousVersion,
+      ...entity,
+      _version: previousVersion._version + 1,
+    };
+    context.server.addUpdatedEntity(newEntity, context.userId);
+    return ok(newEntity);
+  },
+};
