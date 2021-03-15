9c58f733c2a32cdaaa4c8ae8e67770433e151d6a
Jonas Bengtsson <jonas.b@gmail.com>
2021-03-15 18:37:03 +0100

    arc: Update snapshot

--- admin-react-components/src/domain-components/EntityEditor/EntityEditor.test.tsx
+++ admin-react-components/src/domain-components/EntityEditor/EntityEditor.test.tsx
@@ -103,7 +103,6 @@ describe('NewFoo', () => {
           Object {
             "_name": "New name",
             "_type": "Foo",
-            "_version": 0,
           },
         ],
       ]
@@ -130,7 +129,6 @@ describe('NewFoo', () => {
           Object {
             "_name": "New name",
             "_type": "Foo",
-            "_version": 0,
             "title": "New title",
           },
         ],
