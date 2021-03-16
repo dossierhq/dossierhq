e402c781a0b653699740f09450e054758e4860fd
Jonas Bengtsson <jonas.b@gmail.com>
2021-03-16 23:00:55 +0100

    next-web: Tweak eslint settings

--- examples/next-web/.eslintrc.js
+++ examples/next-web/.eslintrc.js
@@ -6,8 +6,9 @@ module.exports = {
   },
   extends: [
     'eslint:recommended',
-    'plugin:react/recommended',
     'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:react/recommended',
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
