900db13c41762eefc93417f42c6d4c4894dbafc9
Jonas Bengtsson <jonas.b@gmail.com>
2021-03-18 23:37:51 +0100

    arc: Add margin, padding, gap

---
+++ admin-react-components/src/stories/MarginSwatch.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { joinClassNames } from '../utils/ClassNameUtils';
+
+interface MarginSwatchProps {
+  className: string;
+}
+
+export function MarginSwatch({ className }: MarginSwatchProps): JSX.Element {
+  return (
+    <div style={{ border: 'solid 1px black' }}>
+      <p
+        className={joinClassNames('dd text-caption', className)}
+        style={{
+          width: '30px',
+          height: '30px',
+          backgroundColor: 'green',
+          textAlign: 'center',
+          padding: '0.5rem',
+        }}
+      >
+        {className}
+      </p>
+    </div>
+  );
+}
+
+interface MarginSwatchGroupProps {
+  size: SpacingSize;
+}
+
+export function MarginSwatchGroup({ size }: MarginSwatchGroupProps): JSX.Element {
+  return (
+    <div className="dd flex-row g-2" style={{ alignItems: 'center' }}>
+      <MarginSwatch className={`m-${size}`} />
+      <MarginSwatch className={`mx-${size}`} />
+      <MarginSwatch className={`my-${size}`} />
+      <MarginSwatch className={`ml-${size}`} />
+      <MarginSwatch className={`mt-${size}`} />
+      <MarginSwatch className={`mr-${size}`} />
+      <MarginSwatch className={`mb-${size}`} />
+    </div>
+  );
+}
---
+++ admin-react-components/src/stories/PaddingSwatch.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { joinClassNames } from '../utils/ClassNameUtils';
+
+interface PaddingSwatchProps {
+  className: string;
+}
+
+export function PaddingSwatch({ className }: PaddingSwatchProps): JSX.Element {
+  return (
+    <div className={joinClassNames('dd', className)} style={{ border: 'solid 1px black' }}>
+      <p
+        className={'dd text-caption'}
+        style={{
+          width: '30px',
+          height: '30px',
+          backgroundColor: 'hotpink',
+          textAlign: 'center',
+          padding: '0.5rem',
+        }}
+      >
+        {className}
+      </p>
+    </div>
+  );
+}
+
+interface PaddingSwatchGroupProps {
+  size: SpacingSize;
+}
+
+export function PaddingSwatchGroup({ size }: PaddingSwatchGroupProps): JSX.Element {
+  return (
+    <div className="dd flex-row g-2" style={{ alignItems: 'center' }}>
+      <PaddingSwatch className={`p-${size}`} />
+      <PaddingSwatch className={`px-${size}`} />
+      <PaddingSwatch className={`py-${size}`} />
+      <PaddingSwatch className={`pl-${size}`} />
+      <PaddingSwatch className={`pt-${size}`} />
+      <PaddingSwatch className={`pr-${size}`} />
+      <PaddingSwatch className={`pb-${size}`} />
+    </div>
+  );
+}
