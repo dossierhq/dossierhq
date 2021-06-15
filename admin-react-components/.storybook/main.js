7c8e97a4f6a3b19557d13e864f9a0dac0adb068f
Jonas Bengtsson <jonas.b@gmail.com>
2021-06-15 16:35:35 +0200

    arc: Silence postcss warning

--- admin-react-components/.storybook/main.js
+++ admin-react-components/.storybook/main.js
@@ -1,4 +1,5 @@
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  features: { postcss: false },
 };
