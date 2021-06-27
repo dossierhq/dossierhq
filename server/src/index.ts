051bbae8c096cfb4d963816f36933ae7638dfb9b
Jonas Bengtsson <jonas.b@gmail.com>
2021-06-27 13:05:19 +0200

    server: Remove PublishedEntity export

--- server/src/index.ts
+++ server/src/index.ts
@@ -5,6 +5,5 @@ export { createServerAdminClient } from './AdminClient';
 export { default as Auth } from './Auth';
 export { isPagingForwards } from './Paging';
 export { createServerPublishedClient } from './PublishedClient';
-export * as PublishedEntity from './PublishedEntity';
 export { default as Server } from './Server';
 export * as ServerTestUtils from './ServerTestUtils';
