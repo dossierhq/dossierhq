1c007bfa03d3e23239e1cc10068d2fb1cfbfbc69
Jonas Bengtsson <jonas.b@gmail.com>
2021-06-27 14:35:24 +0200

    gql: Stop using require in test setup()

--- graphql/test/setup.ts
+++ graphql/test/setup.ts
@@ -1,5 +1,7 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-require('dotenv').config({ path: require('path').resolve(process.cwd(), '.env') });
+import dotenv from 'dotenv';
+import path from 'path';
+
+dotenv.config({ path: path.resolve(process.cwd(), '.env') });
 
 // eslint-disable-next-line @typescript-eslint/no-empty-function
 export default function (): void {}
