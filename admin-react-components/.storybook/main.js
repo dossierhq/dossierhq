6dc77946a5f5c1d8cf8729f0df62aba05a8a727e
Jonas Bengtsson <jonas.b@gmail.com>
2021-07-03 13:46:19 +0200

    global: Upgrade to typescript 4.3.5

--- admin-react-components/.storybook/main.js
+++ admin-react-components/.storybook/main.js
@@ -2,4 +2,8 @@ module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
   features: { postcss: false },
+  typescript: {
+    //TODO reactivate docgen: https://github.com/styleguidist/react-docgen-typescript/issues/356
+    reactDocgen: 'none',
+  },
 };
