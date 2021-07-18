10d4d5343c71a05ae8304827146e32ad26659533
Jonas Bengtsson <jonas.b@gmail.com>
2021-07-18 21:09:10 +0200

    gql: Fix test setup

--- graphql/src/test/setup.ts
+++ graphql/src/test/setup.ts
@@ -1,7 +1,7 @@
 import dotenv from 'dotenv';
 import path from 'path';
 
-dotenv.config({ path: path.resolve(__dirname, '..', '.env') });
+dotenv.config({ path: path.resolve(__dirname, '..', '..', '.env') });
 
 // eslint-disable-next-line @typescript-eslint/no-empty-function
 export default function (): void {}
