c7f99c82b1dfe0ce21a6c9dbe877a51b461f11cd
Jonas Bengtsson <jonas.b@gmail.com>
2021-07-26 16:41:33 +0200

    next-web: Cleanup unused types and rename handlers

--- examples/next-web/types/RequestTypes.ts
+++ examples/next-web/types/RequestTypes.ts
@@ -1,28 +1,3 @@
-import type {
-  AdminClientJsonOperation,
-  AdminEntityCreate,
-  AdminEntityUpdate,
-  EntityVersionReference,
-} from '@jonasb/datadata-core';
-
-export interface EntityCreateRequest {
-  item: AdminEntityCreate;
-}
-
-export interface EntityUpdateRequest {
-  item: AdminEntityUpdate;
-}
-
-export interface EntityPublishRequest {
-  items: EntityVersionReference[];
-}
-
-export interface EntityUnpublishRequest {
-  items: string[];
-}
-
-export type EntityArchiveRequest = Record<never, never>;
-
-export type EntityUnarchiveRequest = Record<never, never>;
+import type { AdminClientJsonOperation } from '@jonasb/datadata-core';
 
 export type AdminOperationRequest = AdminClientJsonOperation;
