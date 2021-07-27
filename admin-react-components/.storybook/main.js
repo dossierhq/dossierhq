58c9dc7b05021d8a779cb239cb004a634566f282
Jonas Bengtsson <jonas.b@gmail.com>
2021-07-27 23:06:30 +0200

    arc: Configure storybook with strict mode

--- admin-react-components/.storybook/main.js
+++ admin-react-components/.storybook/main.js
@@ -2,6 +2,9 @@ module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
   features: { postcss: false },
+  reactOptions: {
+    strictMode: true,
+  },
   typescript: {
     //TODO reactivate docgen: https://github.com/styleguidist/react-docgen-typescript/issues/356
     reactDocgen: 'none',
