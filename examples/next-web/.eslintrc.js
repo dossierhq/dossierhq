6aab838aab37f603fa9f7d00969fb1ff95ee5b70
Jonas Bengtsson <jonas.b@gmail.com>
2021-08-13 20:52:34 +0200

    next-web: Upgrade to Next 11.1

--- examples/next-web/.eslintrc.js
+++ examples/next-web/.eslintrc.js
@@ -1,36 +1,7 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-    node: true,
-  },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-    'plugin:react/recommended',
-  ],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaFeatures: { jsx: true },
-    ecmaVersion: 12,
-    sourceType: 'module',
-  },
-  plugins: ['react', '@typescript-eslint'],
-  reportUnusedDisableDirectives: true,
+  extends: ['next', 'next/core-web-vitals'],
+  plugins: ['@typescript-eslint'],
   rules: {
     '@typescript-eslint/consistent-type-imports': ['warn'],
-    '@typescript-eslint/no-unused-vars': [
-      'warn',
-      {
-        vars: 'local',
-        args: 'after-used',
-        ignoreRestSiblings: true,
-        varsIgnorePattern: '^_',
-        argsIgnorePattern: '^_',
-      },
-    ],
-    'react/react-in-jsx-scope': 'off',
   },
-  settings: { react: { version: 'detect' } },
 };
--- examples/next-web/next.config.js
+++ examples/next-web/next.config.js
@@ -1,19 +1,9 @@
 const path = require('path');
 
-const withTranspileModules = require('next-transpile-modules')([
-  '@jonasb/datadata-admin-react-components',
-  '@jonasb/datadata-core',
-  '@jonasb/datadata-graphql',
-  '@jonasb/datadata-server',
-]);
-
-module.exports = withTranspileModules({
-  future: {
-    webpack5: true,
-  },
+module.exports = {
   reactStrictMode: true,
   webpack: (config) => {
     config.resolve.alias['react'] = path.resolve(__dirname, '.', 'node_modules', 'react');
     return config;
   },
-});
+};
