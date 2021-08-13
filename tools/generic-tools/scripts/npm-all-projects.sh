aca9c7dd36c8df532dabef6a83166e44097f10b3
Jonas Bengtsson <jonas.b@gmail.com>
2021-08-13 21:24:17 +0200

    generic-tools: Fix npm-all-projects.sh

--- tools/generic-tools/scripts/npm-all-projects.sh
+++ tools/generic-tools/scripts/npm-all-projects.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 set -e -u
 
-# find all package.json files (including root)
-PACKAGE_FILES="$(git ls-files | grep '\(^\|.*/\)package\.json$')"
+THIS_DIR="$(cd "$(dirname "$0")" > /dev/null; pwd)"
 
-echo "$PACKAGE_FILES" | while read -r PACKAGE_FILE; do
-  PROJECT_DIR="$(dirname "${PACKAGE_FILE}")"
-  (
-    set -x
-    cd "${PROJECT_DIR}"
-    npm "$@"
-  )
-done
+(
+  cd "$THIS_DIR/../../../"
+
+  # find all package.json files (including root)
+  PACKAGE_FILES="$(git ls-files | grep '\(^\|.*/\)package\.json$')"
+
+  echo "$PACKAGE_FILES" | while read -r PACKAGE_FILE; do
+    PROJECT_DIR="$(dirname "${PACKAGE_FILE}")"
+    (
+      set -x
+      cd "${PROJECT_DIR}"
+      npm "$@"
+    )
+  done
+
+)
