3561e5f421f2ff6caa1a1a0ee38cda96a726b76f
Jonas Bengtsson <jonas.b@gmail.com>
2021-08-13 23:38:12 +0200

    arc: Use webpack5 and upgrade leaflet

--- libraries/admin-react-components/.storybook/main.js
+++ libraries/admin-react-components/.storybook/main.js
@@ -1,6 +1,9 @@
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  core: {
+    builder: 'webpack5',
+  },
   features: { postcss: false },
   reactOptions: {
     strictMode: true,
