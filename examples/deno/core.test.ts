9f7f3d1d2c34fd5af5aa2e5f1a027e367d686cdb
Jonas Bengtsson <jonas.b@gmail.com>
2021-08-25 23:10:38 +0200

    core: Build for deno, add deno sample

---
+++ examples/deno/core.test.ts
@@ -0,0 +1,25 @@
+import {
+  AdminEntity,
+  copyEntity,
+  EntityPublishState,
+} from "@jonasb/datadata-core";
+import { Temporal } from "@js-temporal/polyfill";
+import { assertEquals } from "std/testing/asserts.ts";
+
+Deno.test("copyEntity()", () => {
+  const original: AdminEntity = {
+    id: "123",
+    info: {
+      type: "Foo",
+      name: "Hello",
+      version: 0,
+      publishingState: EntityPublishState.Draft,
+      createdAt: Temporal.Now.instant(),
+      updatedAt: Temporal.Now.instant(),
+    },
+    fields: { title: "message" },
+  };
+
+  const copy = copyEntity(original, { fields: { title: "hello" } });
+  assertEquals(copy, { ...original, fields: { title: "hello" } });
+});
