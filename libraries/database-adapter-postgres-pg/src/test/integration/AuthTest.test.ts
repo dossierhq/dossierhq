bd83ce864a0c0674639dbf2043652b4acbc245c6
Jonas Bengtsson <jonas.b@gmail.com>
2021-08-30 19:50:34 +0200

    test-integ: Rename package

--- libraries/database-adapter-postgres-pg/src/test/integration/AuthTest.test.ts
+++ libraries/database-adapter-postgres-pg/src/test/integration/AuthTest.test.ts
@@ -1,6 +1,6 @@
-import { createAuthTestSuite } from '@jonasb/datadata-database-adapter-test';
+import { createAuthTestSuite } from '@jonasb/datadata-database-adapter-test-integration';
 import { AuthContext, Server } from '@jonasb/datadata-server';
-import { createPostgresTestAdapter, registerTestSuite } from './TestUtils';
+import { createPostgresTestAdapter, registerTestSuite } from '../TestUtils';
 
 let server: Server | null = new Server({ databaseAdapter: createPostgresTestAdapter() });
 let authContext: AuthContext | null = server.createAuthContext();
