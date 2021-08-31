72d73f92ef6a183bffcef6a99a517adba4f9dbcb
Jonas Bengtsson <jonas.b@gmail.com>
2021-08-31 12:08:26 +0200

    postgres: Catch all exceptions from queries

--- libraries/database-adapter-postgres-core/src/QueryFunctions.ts
+++ libraries/database-adapter-postgres-core/src/QueryFunctions.ts
@@ -1,28 +1,75 @@
-import { ErrorType, notOk, ok, PromiseResult } from '@jonasb/datadata-core';
+import { ErrorType, notOk, ok, PromiseResult, Result } from '@jonasb/datadata-core';
 import type { Context } from '@jonasb/datadata-server';
 import type { PostgresDatabaseAdapter, PostgresTransaction } from '.';
 
-//TODO handle all exceptions (need some extra work for unique)
-export async function queryNone(
+interface ErrorConverter<TRow, TError extends ErrorType> {
+  (error: unknown): Result<TRow[], TError | ErrorType.Generic>;
+}
+
+async function queryCommon<TRow, TError extends ErrorType>(
   context: Context,
   adapter: PostgresDatabaseAdapter,
   query: string,
-  values?: unknown[]
-): PromiseResult<void, ErrorType.Generic> {
-  const rows = await adapter.query(context.transaction as PostgresTransaction, query, values);
+  values: unknown[] | undefined,
+  errorConverter: ErrorConverter<TRow, TError> | undefined
+): PromiseResult<TRow[], TError | ErrorType.Generic> {
+  try {
+    const rows = await adapter.query<TRow>(
+      context.transaction as PostgresTransaction,
+      query,
+      values
+    );
+    return ok(rows);
+  } catch (error) {
+    if (errorConverter) {
+      return errorConverter(error);
+    }
+    return notOk.GenericUnexpectedException(error);
+  }
+}
+
+export async function queryNone<TError extends ErrorType | ErrorType.Generic = ErrorType.Generic>(
+  context: Context,
+  adapter: PostgresDatabaseAdapter,
+  query: string,
+  values?: unknown[],
+  errorConverter?: ErrorConverter<unknown, TError | ErrorType.Generic>
+): PromiseResult<void, TError | ErrorType.Generic> {
+  const result = await queryCommon<unknown, TError>(
+    context,
+    adapter,
+    query,
+    values,
+    errorConverter as ErrorConverter<unknown, TError>
+  );
+  if (result.isError()) {
+    return result;
+  }
+  const rows = result.value;
   if (rows.length !== 0) {
     return notOk.Generic(`Expected 0 rows, got ${rows.length}`);
   }
   return ok(undefined);
 }
 
-export async function queryOne<R>(
+export async function queryOne<TRow, TError extends ErrorType = ErrorType.Generic>(
   context: Context,
   adapter: PostgresDatabaseAdapter,
   query: string,
-  values?: unknown[]
-): PromiseResult<R, ErrorType.Generic> {
-  const rows = await adapter.query<R>(context.transaction as PostgresTransaction, query, values);
+  values?: unknown[],
+  errorConverter?: ErrorConverter<TRow, TError | ErrorType.Generic>
+): PromiseResult<TRow, TError | ErrorType.Generic> {
+  const result = await queryCommon<TRow, TError>(
+    context,
+    adapter,
+    query,
+    values,
+    errorConverter as ErrorConverter<TRow, TError>
+  );
+  if (result.isError()) {
+    return result;
+  }
+  const rows = result.value;
   if (rows.length !== 1) {
     return notOk.Generic(`Expected 1 row, got ${rows.length}`);
   }
