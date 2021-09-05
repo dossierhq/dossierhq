81d7c24074a1749ae23340812debf887ea0311e3
Jonas Bengtsson <jonas.b@gmail.com>
2021-09-06 01:14:38 +0200

    deno: Only cache external dependencies

---
+++ examples/deno/external-dependencies.ts
@@ -0,0 +1,6 @@
+import type {} from "@js-temporal/polyfill";
+import type {} from "dotenv";
+import type {} from "postgres";
+import type {} from "std/testing/asserts.ts";
+
+// This file is used to cache external dependencies for postinstall
