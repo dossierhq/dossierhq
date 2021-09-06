684ba249d7df2930b2ca75a9aaaad6c16631ebbe
Jonas Bengtsson <jonas.b@gmail.com>
2021-09-06 20:36:15 +0200

    integ: Remove createPrincipal tests

--- libraries/database-adapter-test-integration/src/auth/AuthTestSuite.ts
+++ libraries/database-adapter-test-integration/src/auth/AuthTestSuite.ts
@@ -1,22 +1,16 @@
 import { CoreTestUtils, ErrorType } from '@jonasb/datadata-core';
-import type { AuthContext, Server2 } from '@jonasb/datadata-server';
-import { Auth } from '@jonasb/datadata-server';
-import { validate as validateUuid } from 'uuid';
+import type { Server2 } from '@jonasb/datadata-server';
 import type { TestFunctionInitializer, TestSuite } from '..';
-import { assertSame, assertTruthy } from '../Asserts';
+import { assertSame } from '../Asserts';
 import { buildSuite } from '../Builder';
 
 const { expectErrorResult, expectOkResult } = CoreTestUtils;
 
 export function createAuthTestSuite<TCleanup>(
-  initializer: TestFunctionInitializer<{ authContext: AuthContext; server: Server2 }, TCleanup>
+  initializer: TestFunctionInitializer<{ server: Server2 }, TCleanup>
 ): TestSuite {
   return buildSuite(
     initializer,
-    createPrincipal_create_new_identifier,
-    createPrincipal_error_create_duplicate_fails,
-    createPrincipal_error_create_missing_provider,
-    createPrincipal_error_create_missing_identifier,
     createSession_create_new_identifier,
     createSession_create_existing_identifier,
     createSession_error_missing_provider,
@@ -26,49 +20,6 @@ export function createAuthTestSuite<TCleanup>(
 
 function randomIdentifier() {
   return Math.random().toString();
-}
-
-async function createPrincipal_create_new_identifier({
-  authContext,
-}: {
-  authContext: AuthContext;
-}) {
-  const result = await Auth.createPrincipal(authContext, 'test', randomIdentifier());
-  if (expectOkResult(result)) {
-    assertTruthy(validateUuid(result.value));
-  }
-}
-
-async function createPrincipal_error_create_duplicate_fails({
-  authContext,
-}: {
-  authContext: AuthContext;
-}) {
-  const identifier = randomIdentifier();
-  const firstResult = await Auth.createPrincipal(authContext, 'test', identifier);
-  expectOkResult(firstResult);
-
-  const secondResult = await Auth.createPrincipal(authContext, 'test', identifier);
-  expectErrorResult(secondResult, ErrorType.Conflict, 'Principal already exist');
-}
-
-async function createPrincipal_error_create_missing_provider({
-  authContext,
-}: {
-  authContext: AuthContext;
-}) {
-  const identifier = randomIdentifier();
-  const result = await Auth.createPrincipal(authContext, '', identifier);
-  expectErrorResult(result, ErrorType.BadRequest, 'Missing provider');
-}
-
-async function createPrincipal_error_create_missing_identifier({
-  authContext,
-}: {
-  authContext: AuthContext;
-}) {
-  const result = await Auth.createPrincipal(authContext, 'test', '');
-  expectErrorResult(result, ErrorType.BadRequest, 'Missing identifier');
 }
 
 async function createSession_create_new_identifier({ server }: { server: Server2 }) {
