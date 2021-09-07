7c546a5f0b9029d551987c3f2c1efb84d8d692e6
Jonas Bengtsson <jonas.b@gmail.com>
2021-09-07 20:23:47 +0200

    sqlite: Remove old code from integration test

--- libraries/database-adapter-sqlite/src/test/sqljs/integration/AuthTest.test.ts
+++ libraries/database-adapter-sqlite/src/test/sqljs/integration/AuthTest.test.ts
@@ -1,49 +1,37 @@
 import { assertIsDefined } from '@jonasb/datadata-core';
 import { createAuthTestSuite } from '@jonasb/datadata-database-adapter-test-integration';
-import type { AuthContext, Server2 } from '@jonasb/datadata-server';
-import { createServer, Server } from '@jonasb/datadata-server';
+import type { Server2 } from '@jonasb/datadata-server';
+import { createServer } from '@jonasb/datadata-server';
 import { createDummyLogger, createSqlJsTestAdapter, registerTestSuite } from '../../TestUtils';
 
-let server: Server | null = null;
-let authContext: AuthContext | null;
-let server2: Server2 | null = null;
+let server: Server2 | null = null;
 
 beforeAll(async () => {
   const databaseAdapterResult = await createSqlJsTestAdapter();
   if (databaseAdapterResult.isError()) {
     throw databaseAdapterResult.toError();
   }
-  server = new Server({ databaseAdapter: databaseAdapterResult.value });
-  authContext = server.createAuthContext();
 
-  const databaseAdapter2Result = await createSqlJsTestAdapter();
-  if (databaseAdapter2Result.isError()) {
-    throw databaseAdapter2Result.toError();
-  }
-
-  const createServer2Result = await createServer({
-    databaseAdapter: databaseAdapter2Result.value,
+  const createServerResult = await createServer({
+    databaseAdapter: databaseAdapterResult.value,
     logger: createDummyLogger(),
   });
-  if (createServer2Result.isError()) {
-    return createServer2Result;
+  if (createServerResult.isError()) {
+    return createServerResult;
   }
-  server2 = createServer2Result.value;
+  server = createServerResult.value;
 });
 afterAll(async () => {
   if (server) {
     await server.shutdown();
   }
-  server = null;
-  authContext = null;
 });
 
 registerTestSuite(
   createAuthTestSuite({
     before: async () => {
-      assertIsDefined(authContext);
-      assertIsDefined(server2);
-      return [{ authContext, server: server2 }, undefined];
+      assertIsDefined(server);
+      return [{ server }, undefined];
     },
     after: async () => {
       //empty
