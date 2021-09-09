d39eb6fe5612cba68363f89c6a44be7c7dd39a76
Jonas Bengtsson <jonas.b@gmail.com>
2021-09-10 01:12:49 +0200

    server: Use/store schema in server

--- libraries/server/src/Schema.ts
+++ libraries/server/src/Schema.ts
@@ -6,8 +6,7 @@ import type {
   SchemaSpecificationUpdatePayload,
 } from '@jonasb/datadata-core';
 import { ok, Schema } from '@jonasb/datadata-core';
-import type { DatabaseAdapter, SessionContext, TransactionContext } from '.';
-import * as Db from './Database';
+import type { DatabaseAdapter, TransactionContext } from '.';
 
 export async function getSchemaSpecification(
   databaseAdapter: DatabaseAdapter,
@@ -31,21 +30,6 @@ export async function getSchemaSpecification(
     specification.valueTypes.length
   );
   return ok(specification);
-}
-
-export async function setSchema(
-  context: SessionContext,
-  schema: Schema
-): PromiseResult<void, ErrorType.BadRequest> {
-  const validation = schema.validate();
-  if (validation.isError()) {
-    return validation;
-  }
-  // TODO check if different
-  await Db.queryNone(context, 'INSERT INTO schema_versions (specification) VALUES ($1)', [
-    schema.spec,
-  ]);
-  return ok(undefined);
 }
 
 export async function updateSchemaSpecification(
