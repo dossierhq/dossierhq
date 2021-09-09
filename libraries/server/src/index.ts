7f68b3fbbbeb039853dc162517ed2ad4dea100b3
Jonas Bengtsson <jonas.b@gmail.com>
2021-09-09 23:32:57 +0200

    server: Remove AuthContext

--- libraries/server/src/index.ts
+++ libraries/server/src/index.ts
@@ -1,5 +1,5 @@
 export type { Session } from './Auth'; // TODO move to DatabaseAdapter
-export type { AuthContext, Context, Context2, SessionContext, TransactionContext } from './Context'; //TODO remove AuthContext
+export type { Context, Context2, SessionContext, TransactionContext } from './Context';
 export type { AuthCreateSessionPayload, DatabaseAdapter, Transaction } from './DatabaseAdapter';
 export * as DatabaseTables from './DatabaseTables'; //TODO move to postgres-core
 export { createServer } from './Server';
