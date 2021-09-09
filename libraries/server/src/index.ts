04707c66206dced5a50349a8237bf2138bcee690
Jonas Bengtsson <jonas.b@gmail.com>
2021-09-09 23:29:24 +0200

    server: Remove createServerPublishedClient()

--- libraries/server/src/index.ts
+++ libraries/server/src/index.ts
@@ -2,7 +2,6 @@ export type { Session } from './Auth'; // TODO move to DatabaseAdapter
 export type { AuthContext, Context, Context2, SessionContext, TransactionContext } from './Context'; //TODO remove AuthContext
 export type { AuthCreateSessionPayload, DatabaseAdapter, Transaction } from './DatabaseAdapter';
 export * as DatabaseTables from './DatabaseTables'; //TODO move to postgres-core
-export { createServerPublishedClient } from './PublishedClient'; //TODO move to Server2
 export { createServer } from './Server';
 export type { CreateSessionPayload, Server2 } from './Server';
 export * as ServerTestUtils from './ServerTestUtils';
