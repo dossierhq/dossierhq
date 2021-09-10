961acc02a3e128dcbe8e2b6cdee0b9c674ae2b38
Jonas Bengtsson <jonas.b@gmail.com>
2021-09-11 01:47:42 +0200

    adapters: Import createSession tests

--- libraries/database-adapter-sqlite/src/auth/createSession.test.ts
+++ libraries/database-adapter-sqlite/src/auth/createSession.test.ts
@@ -50,4 +50,37 @@ describe('authCreateSession', () => {
       ]);
     }
   });
+
+  test('Existing principal', async () => {
+    const adapter = createMockAdapter();
+    const contextResult = await createMockContext(adapter);
+    if (contextResult.isError()) throw contextResult.toError();
+    const context = contextResult.value;
+
+    adapter.query.mockClear();
+    adapter.query.mockImplementation(async (query, _values) => {
+      if (query.startsWith('SELECT s.id, s.uuid FROM')) return [{ id: 123 }];
+      return [];
+    });
+
+    const result = await authCreateSession(adapter, context, 'test', 'hello');
+    if (expectOkResult(result)) {
+      const {
+        session: { subjectId },
+      } = result.value;
+      expectResultValue(result, {
+        principalEffect: 'none',
+        session: { subjectInternalId: 123, subjectId },
+      });
+
+      expect(getQueryCalls(adapter)).toEqual([
+        [
+          `SELECT s.id, s.uuid FROM subjects s, principals p
+    WHERE p.provider = $1 AND p.identifier = $2 AND p.subjects_id = s.id`,
+          'test',
+          'hello',
+        ],
+      ]);
+    }
+  });
 });
