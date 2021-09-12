e29b8cb30e16d32bcd4088e7b3f7d2236a525fee
Jonas Bengtsson <jonas.b@gmail.com>
2021-09-12 22:37:25 +0200

    design: Tweak table

--- libraries/design/src/components/IconImage.tsx
+++ libraries/design/src/components/IconImage.tsx
@@ -2,18 +2,24 @@ import {
   faChevronDown,
   faChevronUp,
   faMapMarkedAlt,
+  faPlusSquare,
   faSearch,
+  faSortDown,
+  faSortUp,
   faThList,
 } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 
 const icons = {
+  add: faPlusSquare,
   chevronDown: faChevronDown,
   chevronUp: faChevronUp,
   list: faThList,
   map: faMapMarkedAlt,
   search: faSearch,
+  orderDown: faSortDown,
+  orderUp: faSortUp,
 };
 
 interface Props {
--- libraries/design/src/stories/screens/EntityListScreen.stories.tsx
+++ libraries/design/src/stories/screens/EntityListScreen.stories.tsx
@@ -22,7 +22,12 @@ import type {
   EntityTypeSelectorState,
 } from './EntityTypeSelector';
 import { EntityTypeSelector, initializeEntityTypeSelectorState } from './EntityTypeSelector';
-import type { StatusSelectorInitArgs, StatusSelectorReducer } from './StatusSelector';
+import type {
+  StatusSelectorDispatch,
+  StatusSelectorInitArgs,
+  StatusSelectorReducer,
+  StatusSelectorState,
+} from './StatusSelector';
 import { initializeStatusSelectorState, StatusSelector } from './StatusSelector';
 
 interface ScreenProps {
@@ -72,61 +77,76 @@ function Wrapper() {
         </Navbar.Brand>
         <Navbar.Item>Entities</Navbar.Item>
       </Navbar>
+      <Container className="is-flex is-flex-grow-0 pt-2" flexDirection="column">
+        <Columns gap={1 as unknown as undefined}>
+          <Columns.Column>
+            <p className="control has-icons-left">
+              <input className="input" type="text" placeholder="Search" />
+              <Icon size="small" align="left">
+                <IconImage icon="search" />
+              </Icon>
+            </p>
+          </Columns.Column>
+          <Columns.Column narrow>
+            <EntityTypeSelector
+              label="Entity type"
+              state={entityTypeFilterState}
+              dispatch={entityTypeFilterDispatch}
+            />
+          </Columns.Column>
+          <Columns.Column narrow>
+            <StatusSelector
+              label="Status"
+              state={statusFilterState}
+              dispatch={statusFilterDispatch}
+            />
+          </Columns.Column>
+          <Columns.Column narrow>
+            <Button>
+              <Icon size="small" align="left">
+                <IconImage icon="map" />
+              </Icon>
+            </Button>
+          </Columns.Column>
+          <Columns.Column narrow>
+            <Button>
+              <Icon size="small">
+                <IconImage icon="add" />
+              </Icon>
+              <span>Create</span>
+              <Icon size="small">
+                <IconImage icon="chevronDown" />
+              </Icon>
+            </Button>
+          </Columns.Column>
+        </Columns>
+      </Container>
       <div style={{ overflowY: 'scroll', flexGrow: 1, height: '0' }}>
-        <Container className="is-flex is-flex-grow-1 pt-2" flexDirection="column">
-          <Columns gap={1 as unknown as undefined}>
-            <Columns.Column>
-              <p className="control has-icons-left">
-                <input className="input" type="text" placeholder="Search" />
-                <Icon size="small" align="left">
-                  <IconImage icon="search" />
-                </Icon>
-              </p>
-            </Columns.Column>
-            <Columns.Column narrow>
-              <EntityTypeSelector
-                label="Entity type"
-                state={entityTypeFilterState}
-                dispatch={entityTypeFilterDispatch}
-              />
-            </Columns.Column>
-            <Columns.Column narrow>
-              <StatusSelector
-                label="Status"
-                state={statusFilterState}
-                dispatch={statusFilterDispatch}
-              />
-            </Columns.Column>
-            <Columns.Column narrow>
-              <Button>
-                <Icon size="small" align="left">
-                  <IconImage icon="map" />
-                </Icon>
-              </Button>
-            </Columns.Column>
-          </Columns>
-
+        <Container className="is-flex" flexDirection="column">
           <EntityTypesList state={entityTypeFilterState} dispatch={entityTypeFilterDispatch} />
-          <Table size="fullwidth">
+          <StatusTagList state={statusFilterState} dispatch={statusFilterDispatch} />
+          <Table size="fullwidth" hoverable>
             <thead>
-              <tr>
-                <th>Name</th>
-                <th>Entity type</th>
-                <th className="narrow-cell">Status</th>
-                <th className="narrow-cell">Created</th>
-                <th className="narrow-cell">Updated</th>
+              <tr className="sticky-row has-background-white">
+                <OrderByHeader active="down">Name</OrderByHeader>
+                <OrderByHeader active="up">Entity type</OrderByHeader>
+                <th className="is-narrow">Status</th>
+                <OrderByHeader className="is-narrow" active="up">
+                  Created
+                </OrderByHeader>
+                <OrderByHeader className="is-narrow">Updated</OrderByHeader>
               </tr>
             </thead>
             <tbody>
               {[...Array(50).keys()].map((_, index) => (
-                <tr key={index}>
+                <tr key={index} className="is-clickable">
                   <td>Hello</td>
                   <td>BlogPost</td>
-                  <td className="narrow-cell">
+                  <td className="is-narrow">
                     <Tag color="success">Published</Tag>
                   </td>
-                  <td className="narrow-cell">Today</td>
-                  <td className="narrow-cell">Today</td>
+                  <td className="is-narrow">Today</td>
+                  <td className="is-narrow">Today</td>
                 </tr>
               ))}
             </tbody>
@@ -137,6 +157,27 @@ function Wrapper() {
         <Pagination current={3} showFirstLast total={5} />
       </Container>
     </div>
+  );
+}
+
+function OrderByHeader({
+  className,
+  active,
+  children,
+}: {
+  className?: string;
+  active?: 'up' | 'down';
+  children: React.ReactNode;
+}) {
+  return (
+    <th className={`is-clickable order-header ${className ?? ''}`}>
+      {children}
+      <span className="icon-text">
+        <Icon>
+          {active ? <IconImage icon={active === 'down' ? 'orderDown' : 'orderUp'} /> : null}
+        </Icon>
+      </span>
+    </th>
   );
 }
 
@@ -161,6 +202,49 @@ function EntityTypesList({
           <div key={id} className="control">
             <Tag.Group hasAddons>
               <Tag>{name}</Tag>
+              <Tag
+                className="is-clickable"
+                remove
+                onClick={() => dispatch(new MultipleSelectorStateActions.ToggleItem(id))}
+              />
+            </Tag.Group>
+          </div>
+        ))}
+        <Tag.Group>
+          <Tag
+            className="is-clickable"
+            color="white"
+            onClick={() => dispatch(new MultipleSelectorStateActions.ClearSelection())}
+          >
+            Clear
+          </Tag>
+        </Tag.Group>
+      </div>
+    </>
+  );
+}
+
+function StatusTagList({
+  state,
+  dispatch,
+}: {
+  state: StatusSelectorState;
+  dispatch: StatusSelectorDispatch;
+}) {
+  const { items, selectedIds } = state;
+  if (selectedIds.length === 0) {
+    return null;
+  }
+
+  const statuses = items.filter(({ id }) => selectedIds.includes(id));
+  return (
+    <>
+      <p className="is-size-7 has-text-weight-semibold">Show entities with status:</p>
+      <div className="field is-grouped">
+        {statuses.map(({ id, name, color }) => (
+          <div key={id} className="control">
+            <Tag.Group hasAddons>
+              <Tag color={color}>{name}</Tag>
               <Tag
                 className="is-clickable"
                 remove
