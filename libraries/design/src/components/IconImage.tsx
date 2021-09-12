024ce95bdac89fa6a3bdb358e558fd837fef1873
Jonas Bengtsson <jonas.b@gmail.com>
2021-09-12 14:10:51 +0200

    design: Add react-bulma-components, sketch some UI

---
+++ libraries/design/src/components/IconImage.tsx
@@ -0,0 +1,27 @@
+import {
+  faChevronDown,
+  faChevronUp,
+  faMapMarkedAlt,
+  faSearch,
+  faThList,
+} from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React from 'react';
+
+const icons = {
+  chevronDown: faChevronDown,
+  chevronUp: faChevronUp,
+  list: faThList,
+  map: faMapMarkedAlt,
+  search: faSearch,
+};
+
+interface Props {
+  className?: string;
+  icon: keyof typeof icons;
+}
+
+export function IconImage({ className, icon }: Props): JSX.Element {
+  const i = icons[icon];
+  return <FontAwesomeIcon className={className} icon={i} />;
+}
---
+++ libraries/design/src/components/index.ts
@@ -0,0 +1,1 @@
+export { IconImage } from './IconImage';
