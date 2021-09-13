00757fda764cb51c55f79d7a9bbd8ea96b56a689
Jonas Bengtsson <jonas.b@gmail.com>
2021-09-14 00:48:45 +0200

    design: Add Table and text to Icon

--- libraries/design/src/components/Icon/Icon.stories.tsx
+++ libraries/design/src/components/Icon/Icon.stories.tsx
@@ -3,6 +3,11 @@ import React from 'react';
 import type { IconProps } from './Icon';
 import { Icon } from './Icon';
 
+interface StoryProps extends IconProps {
+  prefixText?: string;
+  suffixText?: string;
+}
+
 const meta: Meta<IconProps> = {
   title: 'Components/Icon',
   component: Icon,
@@ -10,7 +15,16 @@ const meta: Meta<IconProps> = {
 };
 export default meta;
 
-const Template: Story<IconProps> = (args) => {
+const Template: Story<StoryProps> = ({ prefixText, suffixText, ...args }: StoryProps) => {
+  if (prefixText || suffixText) {
+    return (
+      <p>
+        {prefixText}
+        <Icon {...args} />
+        {suffixText}
+      </p>
+    );
+  }
   return <Icon {...args} />;
 };
 
@@ -19,3 +33,6 @@ Normal.args = { icon: 'chevronDown' };
 
 export const Empty = Template.bind({});
 Empty.args = { icon: null };
+
+export const TextIcon = Template.bind({});
+TextIcon.args = { icon: 'map', text: true, prefixText: 'Icon in', suffixText: 'text' };
---
+++ libraries/design/src/components/Table/Table.stories.tsx
@@ -0,0 +1,99 @@
+import type { Meta, Story } from '@storybook/react/types-6-0';
+import React, { useState } from 'react';
+import type { TableProps } from './Table';
+import { Table } from './Table';
+
+type ColumnName = 'one' | 'two' | 'three';
+export interface StoryProps extends TableProps {
+  rowCount: number;
+  stickyHeader: boolean;
+  orderableHeaders: ColumnName[];
+  onHeaderClick: (event: MouseEvent) => void;
+  onRowClick: (event: MouseEvent) => void;
+}
+
+const meta: Meta<StoryProps> = {
+  title: 'Components/Table',
+  component: Table,
+  args: { orderableHeaders: [], rowCount: 50 },
+  argTypes: { onHeaderClick: { action: 'clicked' }, onRowClick: { action: 'clicked' } },
+};
+export default meta;
+
+const Template: Story<StoryProps> = (args) => {
+  return <Wrapper {...args} />;
+};
+
+function Wrapper({
+  orderableHeaders,
+  rowCount,
+  stickyHeader,
+  onHeaderClick,
+  onRowClick,
+}: StoryProps) {
+  const [activeOrder, setActiveOrder] = useState(orderableHeaders[0] ?? null);
+  const [orderDirection, setOrderDirection] = useState<'asc' | 'desc'>('asc');
+
+  const orderForColumn = (column: ColumnName) => {
+    const isOrderable = orderableHeaders.includes(column);
+    if (!isOrderable) return undefined;
+    return column === activeOrder ? orderDirection : '';
+  };
+
+  const onClickHandlerForColumn = (column: ColumnName) => {
+    const isOrderable = orderableHeaders.includes(column);
+    if (isOrderable) {
+      return (event: MouseEvent) => {
+        if (activeOrder !== column) {
+          setActiveOrder(column);
+        } else {
+          setOrderDirection(orderDirection === 'asc' ? 'desc' : 'asc');
+        }
+        onHeaderClick(event);
+      };
+    }
+    return onHeaderClick;
+  };
+
+  return (
+    <Table>
+      <Table.Head>
+        <Table.Row sticky={stickyHeader}>
+          <Table.Header order={orderForColumn('one')} onClick={onClickHandlerForColumn('one')}>
+            One
+          </Table.Header>
+          <Table.Header order={orderForColumn('two')} onClick={onClickHandlerForColumn('two')}>
+            Two
+          </Table.Header>
+          <Table.Header
+            narrow
+            order={orderForColumn('three')}
+            onClick={onClickHandlerForColumn('three')}
+          >
+            Three
+          </Table.Header>
+        </Table.Row>
+      </Table.Head>
+      <Table.Body>
+        {[...Array(rowCount).keys()].map((_, index) => (
+          <Table.Row key={index} clickable onClick={onRowClick}>
+            <Table.Cell>Row {index + 1}</Table.Cell>
+            <Table.Cell>Two</Table.Cell>
+            <Table.Cell narrow>Three</Table.Cell>
+          </Table.Row>
+        ))}
+      </Table.Body>
+    </Table>
+  );
+}
+
+export const Normal = Template.bind({});
+Normal.args = {};
+
+export const StickyHeader = Template.bind({});
+StickyHeader.args = { stickyHeader: true };
+
+export const Orderable = Template.bind({});
+Orderable.args = {
+  orderableHeaders: ['one', 'two'],
+};
