278f84f18aa4202d2d0ca83065f666f49b2f675f
Jonas Bengtsson <jonas.b@gmail.com>
2021-09-25 20:03:48 +0200

    core: Update comments for logging

--- libraries/core/src/Logger.ts
+++ libraries/core/src/Logger.ts
@@ -1,21 +1,25 @@
+// Formatting placeholders compatible with node and pino: %s %d %O
+// https://nodejs.org/api/util.html#util_util_format_format_args
+// https://github.com/pinojs/pino/blob/master/docs/api.md#message
+
 export interface Logger {
   /**
-   * @param message Message with formatting (e.g. %s, %d, %O)
+   * @param message Message with printf formatting (%s, %d, %O)
    * @param args Arguments to formatting
    */
   error(message: string, ...args: unknown[]): void;
   /**
-   * @param message Message with formatting (e.g. %s, %d, %O)
+   * @param message Message with printf formatting (%s, %d, %O)
    * @param args Arguments to formatting
    */
   warn(message: string, ...args: unknown[]): void;
   /**
-   * @param message Message with formatting (e.g. %s, %d, %O)
+   * @param message Message with printf formatting (%s, %d, %O)
    * @param args Arguments to formatting
    */
   info(message: string, ...args: unknown[]): void;
   /**
-   * @param message Message with formatting (e.g. %s, %d, %O)
+   * @param message Message with printf formatting (%s, %d, %O)
    * @param args Arguments to formatting
    */
   debug(message: string, ...args: unknown[]): void;
