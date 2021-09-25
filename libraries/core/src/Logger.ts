7e5f5e810ffab18cd6a4006f77f0afa3bfb800ed
Jonas Bengtsson <jonas.b@gmail.com>
2021-09-25 19:57:28 +0200

    core: Tweak logging middleware

--- libraries/core/src/Logger.ts
+++ libraries/core/src/Logger.ts
@@ -1,6 +1,22 @@
 export interface Logger {
-  error: (message: string, ...args: unknown[]) => void;
-  warn: (message: string, ...args: unknown[]) => void;
-  info: (message: string, ...args: unknown[]) => void;
-  debug: (message: string, ...args: unknown[]) => void;
+  /**
+   * @param message Message with formatting (e.g. %s, %d, %O)
+   * @param args Arguments to formatting
+   */
+  error(message: string, ...args: unknown[]): void;
+  /**
+   * @param message Message with formatting (e.g. %s, %d, %O)
+   * @param args Arguments to formatting
+   */
+  warn(message: string, ...args: unknown[]): void;
+  /**
+   * @param message Message with formatting (e.g. %s, %d, %O)
+   * @param args Arguments to formatting
+   */
+  info(message: string, ...args: unknown[]): void;
+  /**
+   * @param message Message with formatting (e.g. %s, %d, %O)
+   * @param args Arguments to formatting
+   */
+  debug(message: string, ...args: unknown[]): void;
 }
