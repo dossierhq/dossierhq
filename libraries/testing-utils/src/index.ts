d5298b05cb5dd7b4cec651e1e9a53a4e25f9bea3
Jonas Bengtsson <jonas.b@gmail.com>
2021-09-26 10:59:33 +0200

    testing-utils: Remove

--- libraries/testing-utils/src/index.ts
+++
@@ -1,4 +0,0 @@
-export type { InMemorySessionContext, JsonInMemoryEntity } from './InMemoryServer';
-export { InMemoryServer } from './InMemoryServer';
-export { createInMemoryAdminClient } from './InMemoryAdminClient';
-export { createInMemoryPublishedClient } from './InMemoryPublishedClient';
