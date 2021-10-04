aaa97f06ab6e2b030b6a91c79cd4a143b1cb6ed4
Jonas Bengtsson <jonas.b@gmail.com>
2021-10-04 23:56:45 +0200

    arc/design: Fix storybook for esm

--- libraries/design/.storybook/main.cjs
+++ libraries/design/.storybook/main.cjs
@@ -1,16 +1,11 @@
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/preset-scss'],
   core: {
     builder: 'webpack5',
   },
-  features: { postcss: false },
   reactOptions: {
     strictMode: true,
-  },
-  typescript: {
-    //TODO reactivate docgen: https://github.com/styleguidist/react-docgen-typescript/issues/356
-    reactDocgen: 'none',
   },
   webpackFinal: (config) => {
     config.resolve.plugins = [...(config.resolve.plugins || []), new ResolveJsToTsPlugin()];
