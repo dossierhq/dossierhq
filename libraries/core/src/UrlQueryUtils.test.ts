4c1442624ab7712b770724e4f6a2776a1b16fe8b
Jonas Bengtsson <jonas.b@gmail.com>
2021-10-07 00:07:50 +0200

    core/next: Add url query utils

---
+++ libraries/core/src/UrlQueryUtils.test.ts
@@ -0,0 +1,54 @@
+import { buildUrlWithUrlQuery, decodeUrlQueryStringifiedParam, stringifyUrlQueryParams } from '.';
+
+describe('stringifyUrlQueryParams', () => {
+  test('empty', () => {
+    expect(stringifyUrlQueryParams({})).toEqual({});
+  });
+
+  test('null value', () => {
+    expect(stringifyUrlQueryParams({ foo: null })).toEqual({});
+  });
+
+  test('undefined value', () => {
+    expect(stringifyUrlQueryParams({ foo: undefined })).toEqual({});
+  });
+
+  test('empty object value', () => {
+    expect(stringifyUrlQueryParams({ foo: {} })).toEqual({});
+  });
+
+  test('empty object value with keepEmptyObjects', () => {
+    expect(stringifyUrlQueryParams({ foo: {} }, { keepEmptyObjects: true })).toEqual({ foo: '{}' });
+  });
+
+  test('deep value', () => {
+    const actual = stringifyUrlQueryParams({ foo: { bar: { baz: 'hello' } } });
+    expect(actual).toEqual({ foo: '{"bar":{"baz":"hello"}}' });
+  });
+});
+
+describe('buildUrlWithUrlQuery', () => {
+  test('no params', () => {
+    expect(buildUrlWithUrlQuery('http://example.com', {})).toBe('http://example.com');
+  });
+
+  test('deep value', () => {
+    expect(
+      buildUrlWithUrlQuery(
+        'http://example.com',
+        stringifyUrlQueryParams({ foo: { bar: { baz: 'hello' } } })
+      )
+    ).toBe('http://example.com?foo=%7B%22bar%22%3A%7B%22baz%22%3A%22hello%22%7D%7D');
+  });
+});
+
+describe('decodeUrlQueryStringifiedParam', () => {
+  test('deep value', () => {
+    const actual: { bar: { baz: string } } | undefined = decodeUrlQueryStringifiedParam('foo', {
+      foo: '{"bar":{"baz":"hello"}}',
+    });
+    expect(actual).toEqual({
+      bar: { baz: 'hello' },
+    });
+  });
+});
