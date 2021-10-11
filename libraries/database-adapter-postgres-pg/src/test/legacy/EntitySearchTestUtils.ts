627af6bcad37852c01e8745aa2dcd3daf076b1ee
Jonas Bengtsson <jonas.b@gmail.com>
2021-10-11 16:33:10 +0200

    pg: Add tests for published searchEntities() with boundingBox

--- libraries/database-adapter-postgres-pg/src/test/legacy/EntitySearchTestUtils.ts
+++ libraries/database-adapter-postgres-pg/src/test/legacy/EntitySearchTestUtils.ts
@@ -2,6 +2,7 @@ import type {
   AdminClient,
   AdminEntity,
   AdminQuery,
+  BoundingBox,
   Connection,
   Edge,
   Entity,
@@ -88,4 +89,17 @@ export function expectConnectionToMatchSlice<TEntity extends Entity | AdminEntit
   const expectedIds = expectedEntities.slice(sliceStart, sliceEnd).map((x) => ({ id: x.id }));
 
   expect(actualIds).toEqual(expectedIds);
+}
+
+/** Random bounding box (which doesn't wrap 180/-180 longitude) */
+export function randomBoundingBox(heightLat = 1.0, widthLng = 1.0): BoundingBox {
+  function randomInRange(min: number, max: number) {
+    return min + Math.random() * (max - min);
+  }
+
+  const minLat = randomInRange(-90, 90 - heightLat);
+  const minLng = randomInRange(-180, 180 - widthLng);
+  const maxLat = minLat + heightLat;
+  const maxLng = minLng + widthLng;
+  return { minLat, maxLat, minLng, maxLng };
 }
