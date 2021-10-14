522500ec348d1dbc79902653217b0bac32a7c8e6
Jonas Bengtsson <jonas.b@gmail.com>
2021-10-14 23:55:33 +0200

    arc: Extract EntityTypeTagSelector

---
+++ libraries/admin-react-components/src/shared/components/EntityTypeTagSelector/EntityTypeTagSelector.tsx
@@ -0,0 +1,28 @@
+import { Field, TagSelector } from '@jonasb/datadata-design';
+import React from 'react';
+import type { EntityTypeSelectorDispatch, EntityTypeSelectorState } from '../../index.js';
+
+interface Props {
+  state: EntityTypeSelectorState;
+  dispatch: EntityTypeSelectorDispatch;
+}
+
+export function EntityTypeTagSelector({ state, dispatch }: Props) {
+  if (state.selectedIds.length === 0) {
+    return null;
+  }
+
+  return (
+    <Field>
+      <Field.Label size="small">Show entity types</Field.Label>
+      <Field.Control>
+        <TagSelector
+          clearLabel="Clear"
+          itemTag={(item) => ({ tag: item.name })}
+          state={state}
+          dispatch={dispatch}
+        />
+      </Field.Control>
+    </Field>
+  );
+}
