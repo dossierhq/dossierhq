0f37af879a8929acd707dea420c5c817141fc658
Jonas Bengtsson <jonas.b@gmail.com>
2021-10-15 23:03:28 +0200

    arc: Add rudamentary published detail screen

---
+++ libraries/admin-react-components/src/published/screens/EntityDetailScreen/EntityDetailScreen.stories.tsx
@@ -0,0 +1,37 @@
+import type { Meta, Story } from '@storybook/react/types-6-0.js';
+import React from 'react';
+import { foo1Id } from '../../../test/EntityFixtures.js';
+import { LoadContextProvider } from '../../test/LoadContextProvider.js';
+import type { EntityDetailScreenProps } from './EntityDetailScreen.js';
+import { EntityDetailScreen } from './EntityDetailScreen.js';
+
+type StoryProps = EntityDetailScreenProps;
+
+const meta: Meta<EntityDetailScreenProps> = {
+  title: 'Published/Screens/EntityDetailScreen',
+  component: EntityDetailScreen,
+  argTypes: {},
+  args: { reference: { id: foo1Id } },
+  parameters: { layout: 'fullscreen' },
+};
+export default meta;
+
+const Template: Story<StoryProps> = (args) => {
+  return Wrapper(args);
+};
+
+function Wrapper(props: StoryProps) {
+  return (
+    <LoadContextProvider>
+      <EntityDetailScreen {...props} />
+    </LoadContextProvider>
+  );
+}
+
+export const Normal = Template.bind({});
+
+export const HeaderFooter = Template.bind({});
+HeaderFooter.args = {
+  header: <div style={{ height: 50, backgroundColor: 'papayawhip' }} />,
+  footer: <div style={{ height: 50, backgroundColor: 'papayawhip' }} />,
+};
