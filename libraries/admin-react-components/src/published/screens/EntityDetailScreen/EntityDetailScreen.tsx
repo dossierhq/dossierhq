6b9ab07d8e792a6021ef100456109ee3d0fcf38f
Jonas Bengtsson <jonas.b@gmail.com>
2021-10-16 01:46:23 +0200

    arc: Start rendering fields of published entity

--- libraries/admin-react-components/src/published/screens/EntityDetailScreen/EntityDetailScreen.tsx
+++ libraries/admin-react-components/src/published/screens/EntityDetailScreen/EntityDetailScreen.tsx
@@ -1,5 +1,5 @@
-import type { EntityReference } from '@jonasb/datadata-core';
-import { FullscreenContainer, Text } from '@jonasb/datadata-design';
+import type { Entity, EntityReference, EntityTypeSpecification } from '@jonasb/datadata-core';
+import { Field, FullscreenContainer, Text } from '@jonasb/datadata-design';
 import React, { useContext, useEffect } from 'react';
 import { PublishedDataDataContext, useEntity } from '../../index.js';
 
@@ -16,8 +16,10 @@ export function EntityDetailScreen({
   reference,
   onTitleChange,
 }: EntityDetailScreenProps): JSX.Element | null {
-  const { publishedClient, schema: _1 } = useContext(PublishedDataDataContext);
+  const { publishedClient, schema } = useContext(PublishedDataDataContext);
   const { entity, entityError: _2 } = useEntity(publishedClient, reference);
+
+  const typeSpec = schema && entity ? schema.getEntityTypeSpecification(entity.info.type) : null;
 
   useEffect(() => {
     if (entity && onTitleChange) onTitleChange(entity.info.name);
@@ -26,17 +28,59 @@ export function EntityDetailScreen({
   return (
     <FullscreenContainer>
       {header ? <FullscreenContainer.Row fullWidth>{header}</FullscreenContainer.Row> : null}
-      <FullscreenContainer.Row center flexDirection="row" gap={2} paddingVertical={2}>
-        <Text as="h1" textStyle="headline4">
-          {entity?.info.name}
-        </Text>
+      <FullscreenContainer.Row flexDirection="row" gap={2} paddingVertical={2}>
+        {entity ? (
+          <Text as="h1" textStyle="headline4">
+            {entity.info.name}{' '}
+            <Text as="span" textStyle="headline6">
+              {entity.info.type}
+            </Text>
+          </Text>
+        ) : null}
       </FullscreenContainer.Row>
       <FullscreenContainer.ScrollableRow>
         <FullscreenContainer.Row>
-          <pre>{JSON.stringify(entity, null, 2)}</pre>
+          {entity && typeSpec ? (
+            <>
+              <EntityMetadata entity={entity} />
+              <EntityFields entity={entity} typeSpec={typeSpec} />
+            </>
+          ) : null}
         </FullscreenContainer.Row>
       </FullscreenContainer.ScrollableRow>
       {footer ? <FullscreenContainer.Row fullWidth>{footer}</FullscreenContainer.Row> : null}
     </FullscreenContainer>
   );
+}
+
+function EntityMetadata({ entity }: { entity: Entity }) {
+  return (
+    <Field>
+      <Field.Label>ID</Field.Label>
+      <Field.Control>{entity.id}</Field.Control>
+    </Field>
+  );
+}
+
+function EntityFields({
+  entity,
+  typeSpec,
+}: {
+  entity: Entity;
+  typeSpec: EntityTypeSpecification;
+}): JSX.Element {
+  const fieldComponents = [];
+  for (const fieldSpec of typeSpec.fields) {
+    const { name } = fieldSpec;
+    const fieldValue = entity.fields[name];
+    if (fieldValue) {
+      fieldComponents.push(
+        <Field>
+          <Field.Label>{name}</Field.Label>
+          <Field.Control>{JSON.stringify(fieldValue)}</Field.Control>
+        </Field>
+      );
+    }
+  }
+  return <>{fieldComponents}</>;
 }
