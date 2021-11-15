54c034e4d53b29f44184c064660d9c8aaa31f8f4
Jonas Bengtsson <jonas.b@gmail.com>
2021-11-15 23:23:42 +0100

    feat(arc): Add status tag selector

---
+++ libraries/admin-react-components/src/components/StatusTagSelector/StatusTagSelector.tsx
@@ -0,0 +1,28 @@
+import { Field, TagSelector } from '@jonasb/datadata-design';
+import React from 'react';
+import type { StatusSelectorDispatch, StatusSelectorState } from '../../index.js';
+
+interface Props {
+  state: StatusSelectorState;
+  dispatch: StatusSelectorDispatch;
+}
+
+export function StatusTagSelector({ state, dispatch }: Props) {
+  if (state.selectedIds.length === 0) {
+    return null;
+  }
+
+  return (
+    <Field>
+      <Field.Label size="small">Show entities with status</Field.Label>
+      <Field.Control>
+        <TagSelector
+          clearLabel="Clear"
+          itemTag={(item) => ({ tag: item.id, color: item.id })}
+          state={state}
+          dispatch={dispatch}
+        />
+      </Field.Control>
+    </Field>
+  );
+}
