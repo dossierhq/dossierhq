aa9a15c19724f49c5d760fb8f78a1c2fa2a8d9f0
Jonas Bengtsson <jonas.b@gmail.com>
2021-11-29 20:38:35 +0100

    chore(server): Check authKey is provided for create/upsert

--- libraries/database-adapter-test-integration/src/TestAuthorizationAdapter.ts
+++ libraries/database-adapter-test-integration/src/TestAuthorizationAdapter.ts
@@ -1,12 +1,20 @@
-import { ok } from '@jonasb/datadata-core';
+import { notOk, ok } from '@jonasb/datadata-core';
 import type { AuthorizationAdapter, SessionContext } from '@jonasb/datadata-server';
 
 export function createTestAuthorizationAdapter(): AuthorizationAdapter {
   return {
-    async resolveAuthorizationKeys<T extends string>(_context: SessionContext, authKeys: T[]) {
+    async resolveAuthorizationKeys<T extends string>(context: SessionContext, authKeys: T[]) {
       const result = {} as Record<T, string>;
       for (const key of authKeys) {
-        result[key] = key;
+        let resolved: string = key;
+        if (key === 'subject') {
+          resolved = `subject:${context.session.subjectId}`;
+        } else if (key === 'unauthorized') {
+          return notOk.NotAuthorized(`User not authorized to use authKey ${key}`);
+        } else if (key === 'non-existing') {
+          return notOk.BadRequest(`The authKey ${key} doesn't exist`);
+        }
+        result[key] = resolved;
       }
       return ok(result);
     },
