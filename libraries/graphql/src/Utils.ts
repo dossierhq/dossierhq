d480f193aa496806b702bb9aa0b0ceec58a5b54e
Jonas Bengtsson <jonas.b@gmail.com>
2021-12-12 00:58:57 +0100

    fix(gql): Do not use instanceof for checking Instant scalar check

--- libraries/graphql/src/Utils.ts
+++ libraries/graphql/src/Utils.ts
@@ -1,4 +1,5 @@
 import type { AdminClient, PublishedClient } from '@jonasb/datadata-core';
+import type { Temporal } from '@js-temporal/polyfill';
 import type { SessionGraphQLContext } from '.';
 
 export function getAdminClient<TContext extends SessionGraphQLContext>(
@@ -17,4 +18,9 @@ export function getPublishedClient<TContext extends SessionGraphQLContext>(
     throw context.publishedClient.toError();
   }
   return context.publishedClient.value;
+}
+
+// Next.js messes up instanceof, so use do a "weak" instanceof check
+export function seemsLikeATemporalInstant(value: unknown): value is Temporal.Instant {
+  return !!(value && typeof value === 'object' && value.constructor.name === 'Instant');
 }
