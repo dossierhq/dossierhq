199241220d57a7b8802292b3561ec91f5499b27f
Jonas Bengtsson <jonas.b@gmail.com>
2021-12-14 18:12:11 +0100

    feat(postgres): Support DATABASE_CERTIFICATE_AUTHORITY for migration

--- libraries/postgres/datadata-pg-migrate.js
+++ libraries/postgres/datadata-pg-migrate.js
@@ -1,30 +1,23 @@
 #!/usr/bin/env node
 import Postgrator from 'postgrator';
-import urlParse from 'url-parse-lax';
+import pg from 'pg';
 
 const migrationDirectory = new URL('migrations', import.meta.url).pathname;
 
-function getConnectionDetailsFromUrl() {
-  const url = urlParse(process.env.DATABASE_URL);
-  const databaseName = url.path.slice(1);
-  const [username, password] = url.auth.split(':');
-  return {
-    host: url.hostname,
-    port: url.port,
-    database: databaseName,
-    username,
-    password,
-  };
-}
+async function main(connection, targetVersion) {
+  const client = new pg.Client({
+    connectionString: connection.connectionString,
+    ssl: connection.certificateAuthority ? { ca: connection.certificateAuthority } : undefined,
+  });
 
-async function main(targetVersion) {
-  const connectionDetails = getConnectionDetailsFromUrl();
+  await client.connect();
+
   const postgrator = new Postgrator({
     ...connectionDetails,
     migrationDirectory,
     driver: 'pg',
     schemaTable: 'schemaversion',
-    ssl: process.env.NODE_ENV === 'production',
+    execQuery: (query) => client.query(query),
   });
 
   try {
@@ -35,14 +28,23 @@ async function main(targetVersion) {
     console.log(error.appliedMigrations);
     process.exitCode = 1;
   }
+
+  await client.end();
 }
 
+const connection = {
+  connectionString: process.env.DATABASE_URL,
+  certificateAuthority: process.env.DATABASE_CERTIFICATE_AUTHORITY,
+};
+if (!connection.connectionString) {
+  throw new Error(`No DATABASE_URL specified`);
+}
 const args = process.argv.slice(2);
 if (args.length > 1) {
   throw new Error(`Expected 0 or 1 arguments, got ${args.length}: ${args}`);
 }
 const targetVersion = args[0]; // undefined => latest
-main(targetVersion).catch((error) => {
+main(connection, targetVersion).catch((error) => {
   console.warn(error);
   process.exitCode = 1;
 });
