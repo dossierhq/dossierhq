5389fd4f7340976dae08dd8ea2f4b6e2e8c3a2c9
Jonas Bengtsson <jonas.b@gmail.com>
2021-12-30 17:02:40 +0100

    chore(integ): Add more tests

--- libraries/database-adapter-test-integration/src/admin-entity/AdminEntityGetEntitySubSuite.ts
+++ libraries/database-adapter-test-integration/src/admin-entity/AdminEntityGetEntitySubSuite.ts
@@ -3,13 +3,28 @@ import type { UnboundTestFunction } from '../Builder';
 import type { AdminEntityTestContext } from './AdminEntityTestSuite';
 import { TITLE_ONLY_CREATE } from './Fixtures';
 
-const { expectErrorResult, expectOkResult } = CoreTestUtils;
+const { expectErrorResult, expectResultValue, expectOkResult } = CoreTestUtils;
 
 export const GetEntitySubSuite: UnboundTestFunction<AdminEntityTestContext>[] = [
+  getEntity_withSubjectAuthKey,
   getEntity_errorInvalidId,
   getEntity_errorInvalidVersion,
   getEntity_errorWrongAuthKey,
 ];
+
+async function getEntity_withSubjectAuthKey({ client }: AdminEntityTestContext) {
+  const createResult = await client.createEntity(
+    copyEntity(TITLE_ONLY_CREATE, { info: { authKey: 'subject' } })
+  );
+  if (expectOkResult(createResult)) {
+    const {
+      entity: { id },
+    } = createResult.value;
+
+    const getResult = await client.getEntity({ id, authKeys: ['subject'] });
+    expectResultValue(getResult, createResult.value.entity);
+  }
+}
 
 async function getEntity_errorInvalidId({ client }: AdminEntityTestContext) {
   const result = await client.getEntity({ id: '13e4c7da-616e-44a3-a039-24f96f9b17da' });
