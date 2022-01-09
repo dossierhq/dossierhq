23bea03375c64b1a8d0509a836d5e5e7b3fe8e2f
Jonas Bengtsson <jonas.b@gmail.com>
2022-01-09 11:50:55 +0100

    chore(integ): Create more readonly entities

--- libraries/database-adapter-test-integration/src/admin-entity/AdminEntitySearchEntitiesSubSuite.ts
+++ libraries/database-adapter-test-integration/src/admin-entity/AdminEntitySearchEntitiesSubSuite.ts
@@ -1,6 +1,6 @@
 import { assertOkResult, assertSame } from '../Asserts';
 import type { UnboundTestFunction } from '../Builder';
-import { getReadOnlyAdminEntities } from '../shared-entity/ReadOnlyUtils';
+import { getMainPrincipalReadOnlyAdminEntities } from '../shared-entity/ReadOnlyUtils';
 import { adminClientForMainPrincipal } from '../shared-entity/TestClients';
 import type { AdminEntityTestContext } from './AdminEntityTestSuite';
 
@@ -9,12 +9,14 @@ export const SearchEntitiesSubSuite: UnboundTestFunction<AdminEntityTestContext>
 ];
 
 async function searchEntities_minimal({ server }: AdminEntityTestContext) {
-  const setupResult = await getReadOnlyAdminEntities(server);
+  const setupResult = await getMainPrincipalReadOnlyAdminEntities(server);
   assertOkResult(setupResult);
-  const readOnlyEntities = setupResult.value;
-  const result = await adminClientForMainPrincipal(server).searchEntities({
-    entityTypes: ['ReadOnly'],
-  });
+  const expectedEntities = setupResult.value;
+
+  const result = await adminClientForMainPrincipal(server).searchEntities(
+    { entityTypes: ['ReadOnly'] },
+    { first: 100 }
+  );
   assertOkResult(result);
-  assertSame(result.value?.edges.length, readOnlyEntities.length);
+  assertSame(result.value?.edges.length, expectedEntities.length);
 }
