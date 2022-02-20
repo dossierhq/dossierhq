3a522c26bcbf62c42d9ad0aca216c47a04228f1a
Jonas Bengtsson <jonas.b@gmail.com>
2022-02-20 23:49:03 +0100

    chore(deno/ci): Try to split AdminEntity test suite into 3 to avoid coredumps

---
+++ examples/deno/integration-test/AdminEntity1.test.ts
@@ -0,0 +1,38 @@
+import type { Server } from "@jonasb/datadata-server";
+import {
+  createAdminEntityTestSuite,
+  createReadOnlyEntityRepository,
+} from "@jonasb/datadata-database-adapter-test-integration";
+import {
+  initializeIntegrationTestServer,
+  registerTestSuite,
+} from "./TestUtils.ts";
+
+const testSuite = createAdminEntityTestSuite({
+  before: async () => {
+    const serverResult = await initializeIntegrationTestServer();
+    if (serverResult.isError()) {
+      throw serverResult.toError();
+    }
+    const server = serverResult.value;
+    const client = server.createAdminClient(() =>
+      server.createSession({
+        provider: "test",
+        identifier: "id",
+        defaultAuthKeys: ["none"],
+      })
+    );
+
+    const readOnlyEntityRepository = await createReadOnlyEntityRepository(
+      server,
+    );
+
+    //TODO remove client
+    return [{ server, client, readOnlyEntityRepository }, { server }];
+  },
+  after: async ({ server }: { server: Server }) => {
+    await server.shutdown();
+  },
+});
+
+registerTestSuite(testSuite, [1, 3]);
