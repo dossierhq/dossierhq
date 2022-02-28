0d713379510c64b0df43d58ddefd74782df7a105
Jonas Bengtsson <jonas.b@gmail.com>
2022-02-28 19:23:17 +0100

    chore(gql): Refactor schema generator

--- libraries/graphql/src/GraphQLSchemaGenerator.ts
+++ libraries/graphql/src/GraphQLSchemaGenerator.ts
@@ -29,9 +29,6 @@ import type {
   GraphQLFieldConfig,
   GraphQLFieldConfigMap,
   GraphQLInputFieldConfigMap,
-  GraphQLInputType,
-  GraphQLNamedType,
-  GraphQLOutputType,
   GraphQLSchemaConfig,
 } from 'graphql';
 import {
@@ -48,10 +45,6 @@ import {
   GraphQLScalarType,
   GraphQLSchema,
   GraphQLString,
-  isEnumType,
-  isInputType,
-  isInterfaceType,
-  isOutputType,
   Kind,
 } from 'graphql';
 import { GraphQLJSON } from 'graphql-type-json';
@@ -68,47 +61,23 @@ import {
   loadVersionHistory,
 } from './DataLoaders';
 import * as Mutations from './Mutations';
+import {
+  toAdminCreateInputTypeName,
+  toAdminCreatePayloadTypeName,
+  toAdminTypeName,
+  toAdminUpdateInputTypeName,
+  toAdminUpdatePayloadTypeName,
+  toAdminUpsertInputTypeName,
+  toAdminUpsertPayloadTypeName,
+  toAdminValueInputTypeName,
+  toPublishedTypeName,
+} from './NameGenerator';
+import { TypeRepository } from './TypeRepository';
 import { seemsLikeATemporalInstant } from './Utils';
 
 export interface SessionGraphQLContext {
   adminClient: Result<AdminClient, ErrorType.NotAuthenticated>;
   publishedClient: Result<PublishedClient, ErrorType.NotAuthenticated>;
-}
-
-function toAdminTypeName(name: string, isAdmin = true) {
-  return isAdmin ? `Admin${name}` : toPublishedTypeName(name);
-}
-
-function toPublishedTypeName(name: string) {
-  return `Published${name}`;
-}
-
-function toAdminCreateInputTypeName(name: string) {
-  return `Admin${name}CreateInput`;
-}
-
-function toAdminCreatePayloadTypeName(name: string) {
-  return `Admin${name}CreatePayload`;
-}
-
-function toAdminUpdateInputTypeName(name: string) {
-  return `Admin${name}UpdateInput`;
-}
-
-function toAdminUpdatePayloadTypeName(name: string) {
-  return `Admin${name}UpdatePayload`;
-}
-
-function toAdminUpsertInputTypeName(name: string) {
-  return `Admin${name}UpsertInput`;
-}
-
-function toAdminUpsertPayloadTypeName(name: string) {
-  return `Admin${name}UpsertPayload`;
-}
-
-function toAdminValueInputTypeName(name: string) {
-  return `Admin${name}Input`;
 }
 
 function fieldConfigWithArgs<TSource, TContext, TArgs>(
@@ -117,8 +86,7 @@ function fieldConfigWithArgs<TSource, TContext, TArgs>(
   return config as GraphQLFieldConfig<TSource, TContext>;
 }
 
-export class GraphQLSchemaGenerator<TContext extends SessionGraphQLContext> {
-  #types: GraphQLNamedType[] = [];
+export class GraphQLSchemaGenerator<TContext extends SessionGraphQLContext> extends TypeRepository {
   private readonly adminSchema: AdminSchema | null;
   private readonly publishedSchema: PublishedSchema | null;
 
@@ -129,134 +97,9 @@ export class GraphQLSchemaGenerator<TContext extends SessionGraphQLContext> {
     adminSchema: AdminSchema | null;
     publishedSchema: PublishedSchema | null;
   }) {
+    super();
     this.adminSchema = adminSchema;
     this.publishedSchema = publishedSchema;
-  }
-
-  addType(type: GraphQLNamedType): void {
-    if (this.#types.find((x) => x.name === type.name)) {
-      throw new Error(`Type with name ${type.name} already exists`);
-    }
-    this.#types.push(type);
-  }
-
-  getType(name: string): GraphQLNamedType {
-    const type = this.#types.find((x) => x.name === name);
-    if (!type) {
-      throw new Error(`Type with name ${name} doesn't exist`);
-    }
-    return type;
-  }
-
-  getOutputType(name: string): GraphQLOutputType {
-    const type = this.getType(name);
-    if (isOutputType(type)) {
-      return type;
-    }
-    throw new Error(`Type ${name} is not an output type`);
-  }
-
-  getEnumType(name: string): GraphQLEnumType {
-    const type = this.getType(name);
-    if (isEnumType(type)) {
-      return type;
-    }
-    throw new Error(`Type ${name} is not an enum type`);
-  }
-
-  getInputType(name: string): GraphQLInputType {
-    const type = this.getType(name);
-    if (isInputType(type)) {
-      return type;
-    }
-    throw new Error(`Type ${name} is not an input type`);
-  }
-
-  getInterface(name: string): GraphQLInterfaceType {
-    const type = this.getType(name);
-    if (isInterfaceType(type)) {
-      return type;
-    }
-    throw new Error(`Type ${name} is not an interface`);
-  }
-
-  getInterfaces(...names: string[]): GraphQLInterfaceType[] {
-    return names.map((name) => this.getInterface(name));
-  }
-
-  getOrCreateEntityUnion(isAdmin: boolean, names: string[]): GraphQLOutputType {
-    if (names.length === 0) {
-      return this.getOutputType(toAdminTypeName('Entity', isAdmin));
-    }
-
-    // Remove duplicates, sort alphabetically
-    const filteredNames = [...new Set(names)];
-    filteredNames.sort();
-
-    if (filteredNames.length === 1) {
-      return this.getOutputType(toAdminTypeName(filteredNames[0], isAdmin));
-    }
-
-    const enumName = `_${toAdminTypeName(filteredNames.join('Or'), isAdmin)}`;
-    const existingEnum = this.#types.find((x) => x.name === enumName);
-    if (existingEnum) {
-      if (isOutputType(existingEnum)) {
-        return existingEnum;
-      }
-      throw new Error(`Type ${enumName} is not an output type`);
-    }
-
-    const enumValues: GraphQLEnumValueConfigMap = {};
-    filteredNames.forEach((name) => (enumValues[toAdminTypeName(name, isAdmin)] = {}));
-    const enumType = new GraphQLEnumType({
-      name: enumName,
-      values: enumValues,
-    });
-
-    this.addType(enumType);
-    return enumType;
-  }
-
-  getOrCreateValueUnion(isAdmin: boolean, names: string[]): GraphQLOutputType {
-    if (names.length === 0) {
-      return this.getOutputType(toAdminTypeName('Value', isAdmin));
-    }
-
-    // Remove duplicates, sort alphabetically
-    const filteredNames = [...new Set(names)];
-    filteredNames.sort();
-
-    if (filteredNames.length === 1) {
-      return this.getOutputType(toAdminTypeName(filteredNames[0], isAdmin));
-    }
-
-    const enumName = `_${toAdminTypeName(filteredNames.join('Or'), isAdmin)}`;
-    const existingEnum = this.#types.find((x) => x.name === enumName);
-    if (existingEnum) {
-      if (isOutputType(existingEnum)) {
-        return existingEnum;
-      }
-      throw new Error(`Type ${enumName} is not an output type`);
-    }
-
-    const enumValues: GraphQLEnumValueConfigMap = {};
-    filteredNames.forEach((name) => (enumValues[toAdminTypeName(name, isAdmin)] = {}));
-    const enumType = new GraphQLEnumType({
-      name: enumName,
-      values: enumValues,
-    });
-
-    this.addType(enumType);
-    return enumType;
-  }
-
-  getValueInputType(names: string[]): GraphQLInputType | null {
-    const uniqueNames = [...new Set(names)];
-    if (uniqueNames.length !== 1) {
-      return null; //There's no support for polymorphism on input types
-    }
-
-    return this.getInputType(toAdminValueInputTypeName(uniqueNames[0]));
   }
 
   addSharedSupportingTypes(): void {
@@ -542,7 +385,7 @@ export class GraphQLSchemaGenerator<TContext extends SessionGraphQLContext> {
     // PublishedFoo
     this.addType(
       new GraphQLObjectType<PublishedEntity, TContext>({
-        name: toAdminTypeName(entitySpec.name, false),
+        name: toPublishedTypeName(entitySpec.name),
         interfaces: this.getInterfaces('Node', 'PublishedEntity'),
         isTypeOf: (source, _context, _info) => source.info.type === entitySpec.name,
         fields: () => {
@@ -1793,7 +1636,7 @@ export class GraphQLSchemaGenerator<TContext extends SessionGraphQLContext> {
       mutationType = this.buildMutationType<TSource>(this.adminSchema);
     }
 
-    return { query: queryType, mutation: mutationType, types: this.#types };
+    return { query: queryType, mutation: mutationType, types: this.getTypes() };
   }
 
   buildSchema<TSource>(): GraphQLSchema {
