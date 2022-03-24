6722716f03ed4ff14ad431aab3c544526a5639b8
Jonas Bengtsson <jonas.b@gmail.com>
2022-03-24 23:36:59 +0100

    fix(sql.js): Pass in instantiated db to createSqlJsAdapter

--- libraries/database-adapter-sqlite-sqljs/src/test/TestUtils.ts
+++ libraries/database-adapter-sqlite-sqljs/src/test/TestUtils.ts
@@ -2,13 +2,16 @@ import type { ErrorType, PromiseResult } from '@jonasb/datadata-core';
 import { NoOpLogger } from '@jonasb/datadata-core';
 import type { DatabaseAdapter } from '@jonasb/datadata-database-adapter';
 import type { TestSuite } from '@jonasb/datadata-database-adapter-test-integration';
+import initSqlJs from 'sql.js';
 import { createSqlJsAdapter } from '..';
 
 export async function createSqlJsTestAdapter(): PromiseResult<
   DatabaseAdapter,
   ErrorType.BadRequest | ErrorType.Generic
 > {
-  return await createSqlJsAdapter({ logger: NoOpLogger });
+  const SQL = await initSqlJs();
+  const db = new SQL.Database();
+  return await createSqlJsAdapter({ logger: NoOpLogger }, db);
 }
 
 export function registerTestSuite(testSuite: TestSuite): void {
