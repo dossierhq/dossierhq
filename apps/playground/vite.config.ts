4a856202d7878fcf970c4be5562399f6a14cad51
Jonas Bengtsson <jonas.b@gmail.com>
2022-03-24 23:41:17 +0100

    feat(playground): Switch to Vite, initialize server

---
+++ apps/playground/vite.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    commonjsOptions: { include: [] },
+  },
+  optimizeDeps: {
+    include: [
+      "@jonasb/datadata-core",
+      "@jonasb/datadata-database-adapter-sqlite-sql.js",
+      "@jonasb/datadata-server",
+    ],
+  },
+});
