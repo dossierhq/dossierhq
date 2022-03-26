0d760b9c6b357738ec5dfede558a6dbe453bd708
Jonas Bengtsson <jonas.b@gmail.com>
2022-03-26 20:57:15 +0100

    chore(play): Configure Prettier

--- apps/playground/src/App.tsx
+++ apps/playground/src/App.tsx
@@ -1,9 +1,9 @@
-import { Tag } from "@jonasb/datadata-admin-react-components";
-import { createConsoleLogger } from "@jonasb/datadata-core";
-import { FullscreenContainer } from "@jonasb/datadata-design";
-import { useContext, useEffect } from "react";
-import { ServerProvider } from "./components/ServerProvider";
-import { ServerContext } from "./contexts/ServerContext";
+import { Tag } from '@jonasb/datadata-admin-react-components';
+import { createConsoleLogger } from '@jonasb/datadata-core';
+import { FullscreenContainer } from '@jonasb/datadata-design';
+import { useContext, useEffect } from 'react';
+import { ServerProvider } from './components/ServerProvider';
+import { ServerContext } from './contexts/ServerContext';
 
 function App() {
   return (
@@ -21,32 +21,30 @@ function AppContent() {
       if (!server) return;
       const adminClient = server.createAdminClient(() =>
         server.createSession({
-          provider: "sys",
-          identifier: "test",
-          defaultAuthKeys: ["none", "subject"],
+          provider: 'sys',
+          identifier: 'test',
+          defaultAuthKeys: ['none', 'subject'],
           logger: createConsoleLogger(console),
         })
       );
       const schemaResult = await adminClient.updateSchemaSpecification({
-        entityTypes: [
-          { name: "TitleOnly", fields: [{ name: "title", type: "String" }] },
-        ],
+        entityTypes: [{ name: 'TitleOnly', fields: [{ name: 'title', type: 'String' }] }],
       });
-      console.log("Created schema", schemaResult.valueOrThrow());
+      console.log('Created schema', schemaResult.valueOrThrow());
       const createResult = await adminClient.createEntity({
-        info: { type: "TitleOnly", name: "Hello world", authKey: "none" },
-        fields: { title: "Hello world" },
+        info: { type: 'TitleOnly', name: 'Hello world', authKey: 'none' },
+        fields: { title: 'Hello world' },
       });
-      console.log("Created entity", createResult.valueOrThrow());
+      console.log('Created entity', createResult.valueOrThrow());
       const totalCount = await adminClient.getTotalCount();
-      console.log("COUNT", totalCount.valueOrThrow());
+      console.log('COUNT', totalCount.valueOrThrow());
     })();
   }, [server]);
 
   return (
     <FullscreenContainer>
       <FullscreenContainer.Row>
-        server: {server ? "initialized" : ""}, error: {error}
+        server: {server ? 'initialized' : ''}, error: {error}
         <Tag kind="danger" text="HELLO from ARC" />
       </FullscreenContainer.Row>
     </FullscreenContainer>
--- apps/playground/src/main.tsx
+++ apps/playground/src/main.tsx
@@ -1,13 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-import "@jonasb/datadata-admin-react-components/main.css";
-import "@jonasb/datadata-admin-react-components/icons.css";
-import "@jonasb/datadata-design/main.css";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import '@jonasb/datadata-admin-react-components/main.css';
+import '@jonasb/datadata-admin-react-components/icons.css';
+import '@jonasb/datadata-design/main.css';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
