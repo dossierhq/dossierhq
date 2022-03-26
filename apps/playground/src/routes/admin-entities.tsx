5a2f513a732348ce8e7c018dedccdd4fb4b88d0c
Jonas Bengtsson <jonas.b@gmail.com>
2022-03-26 22:17:14 +0100

    feat(play): Add nav bar

--- apps/playground/src/routes/admin-entities.tsx
+++ apps/playground/src/routes/admin-entities.tsx
@@ -1,5 +1,12 @@
 import { EntityListScreen } from '@jonasb/datadata-admin-react-components';
+import { NavBar } from '../components/NavBar';
 
 export function AdminEntitiesRoute() {
-  return <EntityListScreen onCreateEntity={console.log} onOpenEntity={console.log} />;
+  return (
+    <EntityListScreen
+      header={<NavBar current="admin-entities" />}
+      onCreateEntity={console.log}
+      onOpenEntity={console.log}
+    />
+  );
 }
