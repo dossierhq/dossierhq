0e2f28389c566e01263ca3816be15e8151e5e501
Jonas Bengtsson <jonas.b@gmail.com>
2022-04-04 18:21:04 +0200

    feat(arc): Show review changes dialog for schema editor

--- libraries/design/src/components/Card/Card.stories.tsx
+++ libraries/design/src/components/Card/Card.stories.tsx
@@ -1,5 +1,6 @@
 import type { Meta, Story } from '@storybook/react/types-6-0.js';
 import React from 'react';
+import { Text } from '../Text/Text.js';
 import type { CardProps } from './Card.js';
 import { Card } from './Card.js';
 
@@ -20,6 +21,25 @@ Normal.args = {
     <>
       <Card.Header>Header</Card.Header>
       <Card.Content>Content</Card.Content>
+      <Card.Footer>
+        <Card.FooterItem>First</Card.FooterItem>
+        <Card.FooterItem>
+          <a>Second footer</a>
+        </Card.FooterItem>
+      </Card.Footer>
+    </>
+  ),
+};
+
+export const TwoParagraphsInContent = Template.bind({});
+TwoParagraphsInContent.args = {
+  children: (
+    <>
+      <Card.Header>Header</Card.Header>
+      <Card.Content>
+        <Text textStyle="body1">First paragraph</Text>
+        <Text textStyle="body2">Second paragraph</Text>
+      </Card.Content>
       <Card.Footer>
         <Card.FooterItem>First</Card.FooterItem>
         <Card.FooterItem>
