8700911544d2458c4310e7cffd416b8cbe8d7f6a
Jonas Bengtsson <jonas.b@gmail.com>
2022-04-04 18:21:04 +0200

    feat(arc): Show review changes dialog for schema editor

--- libraries/admin-react-components/src/screens/SchemaEditorScreen/SchemaEditorScreen.tsx
+++ libraries/admin-react-components/src/screens/SchemaEditorScreen/SchemaEditorScreen.tsx
@@ -1,13 +1,17 @@
 import { AdminSchema } from '@jonasb/datadata-core';
+import type { AdminSchemaSpecificationUpdate } from '@jonasb/datadata-core';
 import {
   Button,
+  Card,
+  Dialog,
   EmptyStateMessage,
   FullscreenContainer,
   NotificationContext,
   Text,
+  TextArea,
 } from '@jonasb/datadata-design';
 import type { Dispatch } from 'react';
-import React, { useCallback, useContext, useEffect, useReducer } from 'react';
+import React, { useCallback, useContext, useEffect, useMemo, useReducer, useState } from 'react';
 import { useSWRConfig } from 'swr';
 import { DataDataContext2 } from '../..';
 import { SchemaTypeEditor } from '../../components/SchemaTypeEditor/SchemaTypeEditor';
@@ -37,6 +41,7 @@ export function SchemaEditorScreen({ header, footer }: SchemaEditorScreenProps) 
     undefined,
     initializeSchemaEditorState
   );
+  const [showSaveDialog, setShowSaveDialog] = useState(false);
 
   useEffect(() => {
     if (schema) {
@@ -52,11 +57,16 @@ export function SchemaEditorScreen({ header, footer }: SchemaEditorScreenProps) 
       {header ? <FullscreenContainer.Row fullWidth>{header}</FullscreenContainer.Row> : null}
       <FullscreenContainer.ScrollableRow>
         <FullscreenContainer.Row flexDirection="row" paddingVertical={5}>
-          <AddEntityTypeButton dispatchSchemaEditorState={dispatchSchemaEditorState} />
-          <SaveSchemaButton
-            schemaEditorState={schemaEditorState}
+          <AddEntityTypeButton
+            disabled={!schema}
             dispatchSchemaEditorState={dispatchSchemaEditorState}
           />
+          <Button
+            disabled={schemaEditorState.status !== 'changed'}
+            onClick={() => setShowSaveDialog(true)}
+          >
+            Review &amp; save schema
+          </Button>
         </FullscreenContainer.Row>
         {isEmpty ? (
           <EmptyStateMessage
@@ -84,44 +94,100 @@ export function SchemaEditorScreen({ header, footer }: SchemaEditorScreenProps) 
         )}
       </FullscreenContainer.ScrollableRow>
       {footer ? <FullscreenContainer.Row fullWidth>{footer}</FullscreenContainer.Row> : null}
+      <SaveSchemaDialog
+        show={showSaveDialog}
+        schemaEditorState={schemaEditorState}
+        dispatchSchemaEditorState={dispatchSchemaEditorState}
+        onClose={() => setShowSaveDialog(false)}
+      />
     </FullscreenContainer>
   );
 }
 
-function SaveSchemaButton({
+function SaveSchemaDialog({
+  show,
   schemaEditorState,
   dispatchSchemaEditorState,
+  onClose,
 }: {
+  show: boolean;
   schemaEditorState: SchemaEditorState;
   dispatchSchemaEditorState: Dispatch<SchemaEditorStateAction>;
+  onClose: () => void;
 }) {
   const { adminClient } = useContext(DataDataContext2);
   const { showNotification } = useContext(NotificationContext);
   const { cache, mutate } = useSWRConfig();
-  const handleClick = useCallback(async () => {
-    const schemaSpecUpdate = getSchemaSpecificationUpdateFromEditorState(schemaEditorState);
-    const result = await adminClient.updateSchemaSpecification(schemaSpecUpdate);
-    if (result.isOk()) {
-      showNotification({ color: 'success', message: 'Updated schema.' });
-      const adminSchema = new AdminSchema(result.value.schemaSpecification);
-      dispatchSchemaEditorState(
-        new SchemaEditorActions.UpdateSchemaSpecification(adminSchema, { force: true })
-      );
-      updateCacheSchemas(cache, mutate, adminSchema);
-    } else {
-      showNotification({ color: 'error', message: 'Failed saving schema.' });
-    }
-  }, [adminClient, cache, dispatchSchemaEditorState, mutate, schemaEditorState, showNotification]);
+
+  const schemaSpecUpdate = useMemo(
+    () => (show ? getSchemaSpecificationUpdateFromEditorState(schemaEditorState) : null),
+    [schemaEditorState, show]
+  );
+
+  const handleClose = useCallback(
+    async (event: Event, returnValue: string) => {
+      if (returnValue === 'save' && schemaSpecUpdate) {
+        const result = await adminClient.updateSchemaSpecification(schemaSpecUpdate);
+        if (result.isOk()) {
+          showNotification({ color: 'success', message: 'Updated schema.' });
+          const adminSchema = new AdminSchema(result.value.schemaSpecification);
+          dispatchSchemaEditorState(
+            new SchemaEditorActions.UpdateSchemaSpecification(adminSchema, { force: true })
+          );
+          updateCacheSchemas(cache, mutate, adminSchema);
+        } else {
+          showNotification({ color: 'error', message: 'Failed saving schema.' });
+        }
+      }
+      onClose();
+    },
+    [
+      adminClient,
+      cache,
+      dispatchSchemaEditorState,
+      mutate,
+      onClose,
+      schemaSpecUpdate,
+      showNotification,
+    ]
+  );
+
   return (
-    <Button onClick={handleClick} disabled={schemaEditorState.status !== 'changed'}>
-      Save schema
-    </Button>
+    <Dialog show={show} modal onClose={handleClose}>
+      {show && schemaSpecUpdate ? (
+        <SaveSchemaDialogContent schemaSpecUpdate={schemaSpecUpdate} />
+      ) : null}
+    </Dialog>
+  );
+}
+
+function SaveSchemaDialogContent({
+  schemaSpecUpdate,
+}: {
+  schemaSpecUpdate: AdminSchemaSpecificationUpdate;
+}) {
+  return (
+    <Card>
+      <Card.Header>Save schema</Card.Header>
+      <Card.Content>
+        <Text textStyle="body1">Do you want to save the following changes?</Text>
+        <TextArea fixedSize textStyle="code2" readOnly style={{ minHeight: '300px' }}>
+          {JSON.stringify(schemaSpecUpdate, null, 2)}
+        </TextArea>
+      </Card.Content>
+      <Card.Footer>
+        <Card.FooterButton>Cancel</Card.FooterButton>
+        <Card.FooterButton value="save">Save</Card.FooterButton>
+      </Card.Footer>
+    </Card>
   );
 }
 
 function AddEntityTypeButton({
+  disabled,
   dispatchSchemaEditorState,
 }: {
+  disabled: boolean;
   dispatchSchemaEditorState: Dispatch<SchemaEditorStateAction>;
 }) {
   const handleClick = useCallback(() => {
@@ -130,7 +196,11 @@ function AddEntityTypeButton({
       dispatchSchemaEditorState(new SchemaEditorActions.AddEntityType(name));
     }
   }, [dispatchSchemaEditorState]);
-  return <Button onClick={handleClick}>Add entity type</Button>;
+  return (
+    <Button disabled={disabled} onClick={handleClick}>
+      Add entity type
+    </Button>
+  );
 }
 
 function TypeEditorRows({
