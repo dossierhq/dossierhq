8b17549b508ddc42d73266f1445319a4893e645f
Jonas Bengtsson <jonas.b@gmail.com>
2022-04-11 10:51:48 +0200

    feat(design): Initial draft of TagInput and TagInputSelector

---
+++ libraries/design/src/components/TagInput/TagInput.stories.tsx
@@ -0,0 +1,49 @@
+import type { Meta, Story } from '@storybook/react/types-6-0.js';
+import React from 'react';
+import { Tag } from '../Tag/Tag';
+import type { TagInputProps } from './TagInput';
+import { TagInput } from './TagInput';
+
+type StoryProps = TagInputProps;
+
+const meta: Meta<StoryProps> = {
+  title: 'Components/TagInput',
+  component: TagInput,
+  args: {},
+};
+export default meta;
+
+const Template: Story<StoryProps> = (args) => {
+  return <Wrapper {...args} />;
+};
+
+function Wrapper({ ...args }: StoryProps) {
+  return <TagInput {...args} />;
+}
+
+export const Normal = Template.bind({});
+Normal.args = {
+  children: (
+    <>
+      <Tag color="archived">
+        One
+        <Tag.Remove />
+      </Tag>
+      <Tag color="archived">One two three</Tag>
+      <Tag color="archived">One two three</Tag>
+    </>
+  ),
+};
+
+export const ManyTags = Template.bind({});
+ManyTags.args = {
+  children: (
+    <>
+      <Tag color="archived">One two three</Tag>
+      <Tag color="archived">One wlekjlwerkjwelkrj lksdjf</Tag>
+      <Tag color="archived">One wlekjlwerkjwelkrj lksdjf</Tag>
+      <Tag color="archived">One wlekjlwerkjwelkrj lksdjf</Tag>
+      <Tag color="archived">One wlekjlwerkjwelkrj lksdjf</Tag>
+    </>
+  ),
+};
