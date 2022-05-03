e063af9844a9605bf2259523558dd8812053487f
Jonas Bengtsson <jonas.b@gmail.com>
2022-05-03 23:56:34 +0200

    feat(arc): Beginning of entity ref field editor

--- libraries/design/src/components/HoverRevealContainer/HoverRevealContainer.tsx
+++ libraries/design/src/components/HoverRevealContainer/HoverRevealContainer.tsx
@@ -3,12 +3,14 @@ import React from 'react';
 import { toClassName } from '../../utils/ClassNameUtils';
 import type { FlexContainerProps, FlexItemProps } from '../../utils/FlexboxUtils';
 import { toFlexContainerClassName, toFlexItemClassName } from '../../utils/FlexboxUtils';
+import type { GapProps, PaddingProps } from '../../utils/LayoutPropsUtils';
+import { toSpacingClassName } from '../../utils/LayoutPropsUtils';
 
-export interface HoverRevealContainerProps extends FlexContainerProps {
+export interface HoverRevealContainerProps extends FlexContainerProps, GapProps {
   children: ReactNode;
 }
 
-interface HoverRevealContainerItemProps extends FlexItemProps {
+interface HoverRevealContainerItemProps extends FlexItemProps, PaddingProps {
   style?: CSSProperties;
   forceVisible?: boolean;
   children: ReactNode;
@@ -23,7 +25,13 @@ export const HoverRevealContainer: HoverRevealContainerComponent = ({
   ...props
 }: HoverRevealContainerProps) => {
   return (
-    <div className={toClassName('hover-reveal-container', toFlexContainerClassName(props))}>
+    <div
+      className={toClassName(
+        'hover-reveal-container',
+        toFlexContainerClassName(props),
+        toSpacingClassName(props)
+      )}
+    >
       {children}
     </div>
   );
@@ -37,7 +45,12 @@ HoverRevealContainer.Item = ({
 }: HoverRevealContainerItemProps) => {
   return (
     <div
-      className={toClassName('item', forceVisible && 'is-visible', toFlexItemClassName(props))}
+      className={toClassName(
+        'item',
+        forceVisible && 'is-visible',
+        toFlexItemClassName(props),
+        toSpacingClassName(props)
+      )}
       style={style}
     >
       {children}
--- libraries/design/src/components/Text/Text.tsx
+++ libraries/design/src/components/Text/Text.tsx
@@ -7,15 +7,26 @@ import { toTextStyleClassName } from '../../utils/TextStylePropsUtils';
 export interface TextProps {
   as?: 'span' | 'p' | 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6';
   textStyle: TextStyle;
+  noBottomMargin?: boolean;
   className?: string;
   style?: CSSProperties;
   children: ReactNode;
 }
 
-export function Text({ as, className, textStyle, style, children }: TextProps): JSX.Element {
+export function Text({
+  as,
+  className,
+  textStyle,
+  noBottomMargin,
+  style,
+  children,
+}: TextProps): JSX.Element {
   const Element = as ?? 'p';
   return (
-    <Element className={toClassName(toTextStyleClassName(textStyle), className)} style={style}>
+    <Element
+      className={toClassName(toTextStyleClassName(textStyle), className, noBottomMargin && 'mb-0')}
+      style={style}
+    >
       {children}
     </Element>
   );
