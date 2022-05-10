d0e0c067f4f3e71fa9ca2ad26ed744907fcc33bf
Jonas Bengtsson <jonas.b@gmail.com>
2022-05-10 21:28:57 +0200

    feat(arc): Support list fields

--- libraries/admin-react-components/src/components/EntityEditor/FieldEditor.tsx
+++ libraries/admin-react-components/src/components/EntityEditor/FieldEditor.tsx
@@ -1,7 +1,15 @@
 import type { FieldSpecification } from '@jonasb/datadata-core';
-import { isEntityTypeField, isStringField, isValueTypeField } from '@jonasb/datadata-core';
+import {
+  isEntityTypeField,
+  isEntityTypeListField,
+  isStringField,
+  isStringListField,
+  isValueTypeField,
+  isValueTypeListField,
+} from '@jonasb/datadata-core';
 import React from 'react';
 import { EntityTypeFieldEditor } from './EntityTypeFieldEditor';
+import { FieldListWrapper } from './FieldListWrapper';
 import { StringFieldEditor } from './StringFieldEditor';
 import { ValueTypeFieldEditor } from './ValueTypeFieldEditor';
 
@@ -16,10 +24,16 @@ export function FieldEditor({ value, ...props }: FieldEditorProps<unknown>) {
   let editor;
   if (isEntityTypeField(fieldSpec, value)) {
     editor = <EntityTypeFieldEditor {...props} value={value} />;
+  } else if (isEntityTypeListField(fieldSpec, value)) {
+    editor = <FieldListWrapper {...props} value={value} Editor={EntityTypeFieldEditor} />;
   } else if (isStringField(fieldSpec, value)) {
     editor = <StringFieldEditor {...props} value={value} />;
+  } else if (isStringListField(fieldSpec, value)) {
+    editor = <FieldListWrapper {...props} value={value} Editor={StringFieldEditor} />;
   } else if (isValueTypeField(fieldSpec, value)) {
     editor = <ValueTypeFieldEditor {...props} value={value} />;
+  } else if (isValueTypeListField(fieldSpec, value)) {
+    editor = <FieldListWrapper {...props} value={value} Editor={ValueTypeFieldEditor} />;
   } else {
     editor = <div>{`${fieldSpec.type} (list: ${!!fieldSpec.list})`} is not supported</div>;
   }
---
+++ libraries/admin-react-components/src/components/EntityEditor/FieldListWrapper.tsx
@@ -0,0 +1,47 @@
+import { Column } from '@jonasb/datadata-design';
+import React, { useCallback } from 'react';
+import type { FieldEditorProps } from './FieldEditor';
+
+interface Props<Item> extends FieldEditorProps<Item[]> {
+  Editor: React.JSXElementConstructor<FieldEditorProps<Item>>;
+}
+
+export function FieldListWrapper<Item>({
+  value,
+  fieldSpec,
+  onChange,
+  Editor,
+}: Props<Item>): JSX.Element {
+  const handleItemChange = useCallback(
+    (itemValue: Item | null, index: number) => {
+      if (onChange) {
+        const newValue = value ? [...value] : [];
+        if (itemValue === null || itemValue === undefined) {
+          newValue.splice(index, 1);
+        } else {
+          newValue[index] = itemValue;
+        }
+        onChange(newValue.length > 0 ? newValue : null);
+      }
+    },
+    [value, onChange]
+  );
+
+  const itemsAndNew = value ? [...value, null] : [null];
+
+  return (
+    <Column gap={3}>
+      {itemsAndNew.map((it, index) => {
+        return (
+          <div key={index} className="nested-value-item-indentation">
+            <Editor
+              value={it}
+              fieldSpec={fieldSpec}
+              onChange={(newItemValue) => handleItemChange(newItemValue, index)}
+            />
+          </div>
+        );
+      })}
+    </Column>
+  );
+}
