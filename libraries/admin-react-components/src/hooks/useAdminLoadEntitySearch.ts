b29fa558c820b9c64d9d91c4a7a1495c877b58af
Jonas Bengtsson <jonas.b@gmail.com>
2022-05-13 23:30:54 +0200

    chore(arc): Extract useAdminLoadEntitySearch

---
+++ libraries/admin-react-components/src/hooks/useAdminLoadEntitySearch.ts
@@ -0,0 +1,54 @@
+import type { AdminQuery, AdminSearchQuery } from '@jonasb/datadata-core';
+import type { Dispatch } from 'react';
+import { useContext, useEffect } from 'react';
+import type { SearchEntityStateAction } from '..';
+import {
+  AdminDataDataContext,
+  SearchEntityStateActions,
+  useAdminSearchEntities,
+  useAdminTotalCount,
+} from '..';
+import type { SearchEntityState } from '../shared';
+import { useAdminSampleEntities } from './useAdminSampleEntities';
+
+export function useAdminLoadEntitySearch(
+  searchEntityState: SearchEntityState,
+  dispatchSearchEntityState: Dispatch<SearchEntityStateAction>
+) {
+  const { adminClient } = useContext(AdminDataDataContext);
+
+  // search
+  const searchQuery = searchEntityState.paging
+    ? (searchEntityState.query as AdminSearchQuery)
+    : undefined;
+  const { connection, connectionError } = useAdminSearchEntities(
+    adminClient,
+    searchQuery,
+    searchEntityState.paging
+  );
+  const { totalCount } = useAdminTotalCount(adminClient, searchQuery);
+
+  // sample
+  const sampleQuery = !searchQuery ? (searchEntityState.query as AdminQuery) : undefined;
+  const { entitySamples, entitySamplesError } = useAdminSampleEntities(
+    adminClient,
+    sampleQuery,
+    searchEntityState.sampling
+  );
+
+  useEffect(() => {
+    dispatchSearchEntityState(
+      new SearchEntityStateActions.UpdateSearchResult(connection, connectionError)
+    );
+  }, [connection, connectionError, dispatchSearchEntityState]);
+
+  useEffect(() => {
+    dispatchSearchEntityState(new SearchEntityStateActions.UpdateTotalCount(totalCount ?? null));
+  }, [totalCount, dispatchSearchEntityState]);
+
+  useEffect(() => {
+    dispatchSearchEntityState(
+      new SearchEntityStateActions.UpdateSampleResult(entitySamples, entitySamplesError)
+    );
+  }, [entitySamples, entitySamplesError, dispatchSearchEntityState]);
+}
