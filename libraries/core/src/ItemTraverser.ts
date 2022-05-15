b05ab52d168a94371a9622c5f634d51a22b6b003
Jonas Bengtsson <jonas.b@gmail.com>
2022-05-15 23:06:06 +0200

    feat(core): Support fieldItem in traverseAdminItem()

--- libraries/core/src/ItemTraverser.ts
+++ libraries/core/src/ItemTraverser.ts
@@ -8,33 +8,43 @@ import type {
 } from '.';
 import { isItemValueItem, isRichTextItemField, isValueTypeItemField, RichTextBlockType } from '.';
 
-export enum AdminItemTraverseNodeType {
-  error = 'error',
-  field = 'field',
-  valueItem = 'valueItem',
-}
+export const AdminItemTraverseNodeType = {
+  error: 'error',
+  field: 'field',
+  fieldItem: 'fieldItem',
+  valueItem: 'valueItem',
+} as const;
+export type AdminItemTraverseNodeType = keyof typeof AdminItemTraverseNodeType;
 
 export type AdminItemTraverseNode =
   | AdminItemTraverseNodeError
   | AdminItemTraverseNodeField
+  | AdminItemTraverseNodeFieldItem
   | AdminItemTraverseNodeValueItem;
 
 interface AdminItemTraverseNodeError {
   path: ItemValuePath;
-  type: AdminItemTraverseNodeType.error;
+  type: 'error';
   message: string;
 }
 
 interface AdminItemTraverseNodeField {
   path: ItemValuePath;
-  type: AdminItemTraverseNodeType.field;
+  type: 'field';
+  fieldSpec: FieldSpecification;
+  value: unknown;
+}
+
+interface AdminItemTraverseNodeFieldItem {
+  path: ItemValuePath;
+  type: 'fieldItem';
   fieldSpec: FieldSpecification;
   value: unknown;
 }
 
 interface AdminItemTraverseNodeValueItem {
   path: ItemValuePath;
-  type: AdminItemTraverseNodeType.valueItem;
+  type: 'valueItem';
   valueSpec: AdminValueTypeSpecification;
   valueItem: ValueItem;
 }
@@ -87,35 +97,46 @@ export function* traverseAdminItem(
   for (const fieldSpec of fieldSpecs) {
     const fieldPath = [...path, fieldSpec.name];
     const fieldValue = fields?.[fieldSpec.name];
-    const fieldNode: AdminItemTraverseNodeField = {
-      type: AdminItemTraverseNodeType.field,
-      path: fieldPath,
-      fieldSpec,
-      value: fieldValue,
-    };
-    yield fieldNode;
-    if (fieldSpec.list) {
-      if (fieldValue === null || fieldValue === undefined) {
-        continue;
-      }
-      if (!Array.isArray(fieldValue)) {
-        const errorNode: AdminItemTraverseNodeError = {
-          type: AdminItemTraverseNodeType.error,
-          path,
-          message: `Expected list got ${typeof fieldValue}`,
-        };
-        yield errorNode;
-        continue;
-      }
 
-      for (let i = 0; i < fieldValue.length; i += 1) {
-        const fieldItemPath = [...fieldPath, i];
-        const fieldItem = fieldValue[i];
-        yield* traverseItemFieldValue(schema, fieldItemPath, fieldSpec, fieldItem);
-      }
-    } else {
-      yield* traverseItemFieldValue(schema, fieldPath, fieldSpec, fieldValue);
+    yield* traverseAdminItemField(schema, fieldPath, fieldSpec, fieldValue);
+  }
+}
+
+export function* traverseAdminItemField(
+  schema: AdminSchema,
+  path: ItemValuePath,
+  fieldSpec: FieldSpecification,
+  value: unknown
+): Generator<AdminItemTraverseNode> {
+  const fieldNode: AdminItemTraverseNodeField = {
+    type: AdminItemTraverseNodeType.field,
+    path,
+    fieldSpec,
+    value,
+  };
+  yield fieldNode;
+
+  if (fieldSpec.list) {
+    if (value === null || value === undefined) {
+      return;
     }
+    if (!Array.isArray(value)) {
+      const errorNode: AdminItemTraverseNodeError = {
+        type: AdminItemTraverseNodeType.error,
+        path,
+        message: `Expected list got ${typeof value}`,
+      };
+      yield errorNode;
+      return;
+    }
+
+    for (let i = 0; i < value.length; i += 1) {
+      const fieldItemPath = [...path, i];
+      const fieldItem = value[i];
+      yield* traverseItemFieldValue(schema, fieldItemPath, fieldSpec, fieldItem);
+    }
+  } else {
+    yield* traverseItemFieldValue(schema, path, fieldSpec, value);
   }
 }
 
@@ -124,7 +145,15 @@ function* traverseItemFieldValue(
   path: ItemValuePath,
   fieldSpec: FieldSpecification,
   itemValue: unknown
-) {
+): Generator<AdminItemTraverseNode> {
+  const fieldValueNode: AdminItemTraverseNodeFieldItem = {
+    type: AdminItemTraverseNodeType.fieldItem,
+    path,
+    fieldSpec,
+    value: itemValue,
+  };
+  yield fieldValueNode;
+
   if (isValueTypeItemField(fieldSpec, itemValue) && itemValue) {
     yield* traverseAdminItem(schema, path, itemValue);
   } else if (isRichTextItemField(fieldSpec, itemValue) && itemValue) {
