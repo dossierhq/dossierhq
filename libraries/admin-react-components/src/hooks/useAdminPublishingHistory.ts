e3ebf311d47b224966ab257b229f2274e7150480
Jonas Bengtsson <jonas.b@gmail.com>
2022-05-17 13:21:20 +0200

    feat(arc): Add entity events to sidebar

---
+++ libraries/admin-react-components/src/hooks/useAdminPublishingHistory.ts
@@ -0,0 +1,47 @@
+import type {
+  AdminClient,
+  EntityReference,
+  ErrorResult,
+  ErrorType,
+  PublishingHistory,
+} from '@jonasb/datadata-core';
+import { useCallback } from 'react';
+import useSWR from 'swr';
+import { CACHE_KEYS } from '../utils/CacheUtils';
+
+export function useAdminPublishingHistory(
+  adminClient: AdminClient,
+  reference: EntityReference | undefined
+): {
+  publishingHistory: PublishingHistory | undefined;
+  publishingHistoryError:
+    | ErrorResult<
+        unknown,
+        ErrorType.BadRequest | ErrorType.NotFound | ErrorType.NotAuthorized | ErrorType.Generic
+      >
+    | undefined;
+} {
+  const fetcher = useCallback(
+    (_action: string, reference: EntityReference) => fetchPublishingHistory(adminClient, reference),
+    [adminClient]
+  );
+  const { data, error } = useSWR(
+    reference ? CACHE_KEYS.adminPublishingHistory(reference) : null,
+    fetcher
+  );
+
+  // useDebugLogChangedValues('useAdminPublishingHistory changed values', { data, error });
+
+  return { publishingHistory: data, publishingHistoryError: error };
+}
+
+async function fetchPublishingHistory(
+  adminClient: AdminClient,
+  reference: EntityReference
+): Promise<PublishingHistory> {
+  const result = await adminClient.getPublishingHistory(reference);
+  if (result.isError()) {
+    throw result; // throw result, don't convert to Error
+  }
+  return result.value;
+}
--- libraries/admin-react-components/src/screens/EntityEditorScreen/EntityEditorDraftSidebar.tsx
+++ libraries/admin-react-components/src/screens/EntityEditorScreen/EntityEditorDraftSidebar.tsx
@@ -1,7 +1,12 @@
+import type { AdminClient, EntityReference, PublishingEvent } from '@jonasb/datadata-core';
 import { assertIsDefined } from '@jonasb/datadata-core';
-import { Text } from '@jonasb/datadata-design';
-import React from 'react';
+import { InstantDisplay, Row, Tag, Text } from '@jonasb/datadata-design';
+import { Temporal } from '@js-temporal/polyfill';
+import React, { Fragment, useContext } from 'react';
 import { StatusTag } from '../../components/StatusTag/StatusTag';
+import { AdminDataDataContext } from '../../contexts/AdminDataDataContext';
+import { useAdminEntityHistory } from '../../hooks/useAdminEntityHistory';
+import { useAdminPublishingHistory } from '../../hooks/useAdminPublishingHistory';
 import type { EntityEditorState } from '../../reducers/EntityEditorReducer/EntityEditorReducer';
 
 interface Props {
@@ -9,6 +14,7 @@ interface Props {
 }
 
 export function EntityEditorDraftSidebar({ entityEditorState }: Props) {
+  const { adminClient } = useContext(AdminDataDataContext);
   const { activeEntityId } = entityEditorState;
 
   if (!activeEntityId) return null;
@@ -26,8 +32,70 @@ export function EntityEditorDraftSidebar({ entityEditorState }: Props) {
         <>
           <Text textStyle="body2">{entity.id}</Text>
           <StatusTag status={entity.info.status} />
+          <ActivityList adminClient={adminClient} reference={{ id: activeEntityId }} />
         </>
       ) : null}
+    </>
+  );
+}
+
+interface ActivityListEvent {
+  instant: Temporal.Instant;
+  version: number | null;
+  kind: PublishingEvent['kind'] | 'create';
+}
+
+function ActivityList({
+  adminClient,
+  reference,
+}: {
+  adminClient: AdminClient;
+  reference: EntityReference;
+}) {
+  const { entityHistory, entityHistoryError: _1 } = useAdminEntityHistory(adminClient, reference);
+  const { publishingHistory, publishingHistoryError: _2 } = useAdminPublishingHistory(
+    adminClient,
+    reference
+  );
+
+  if (!entityHistory && !publishingHistory) return null;
+
+  const events: ActivityListEvent[] = [];
+
+  if (entityHistory) {
+    events.push(
+      ...entityHistory.versions.map<ActivityListEvent>((it) => ({
+        instant: it.createdAt,
+        version: it.version,
+        kind: 'create',
+      }))
+    );
+  }
+  if (publishingHistory) {
+    events.push(
+      ...publishingHistory.events.map<ActivityListEvent>((it) => ({
+        instant: it.publishedAt,
+        version: it.version,
+        kind: it.kind,
+      }))
+    );
+  }
+
+  events.sort((a, b) => {
+    return Temporal.Instant.compare(b.instant, a.instant); // descending
+  });
+
+  return (
+    <>
+      {events.map((event, index) => (
+        <Row key={index} gap={2}>
+          <Text textStyle="body1">v{event.version ?? '—'}</Text>
+          <Row.Item flexGrow={1}>
+            <InstantDisplay instant={event.instant} />
+          </Row.Item>
+          <Tag>{event.kind}</Tag>
+        </Row>
+      ))}
     </>
   );
 }
