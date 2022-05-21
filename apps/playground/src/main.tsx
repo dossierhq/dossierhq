0db7f367e7d79682451a808f84fa1b156f51ceae
Jonas Bengtsson <jonas.b@gmail.com>
2022-05-21 12:19:48 +0200

    fix(playground): Upgrade to react-18

--- apps/playground/src/main.tsx
+++ apps/playground/src/main.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import App from './App';
 import '@jonasb/datadata-admin-react-components/main.css';
 import '@jonasb/datadata-admin-react-components/icons.css';
 import '@jonasb/datadata-design/main.css';
 import 'leaflet/dist/leaflet.css';
 
-ReactDOM.render(
+const root = createRoot(document.getElementById('root')!);
+
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
