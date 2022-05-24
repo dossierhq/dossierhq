d59ea03d5fc38dde13e7591d20ad3075f985ee33
Jonas Bengtsson <jonas.b@gmail.com>
2022-05-24 17:52:46 +0200

    fix(arc): Fix import

--- apps/playground/vite.config.ts
+++ apps/playground/vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import visualizer from 'rollup-plugin-visualizer';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,6 +8,9 @@ export default defineConfig({
   build: {
     commonjsOptions: { include: [] },
     chunkSizeWarningLimit: 2500,
+    rollupOptions: {
+      plugins: [visualizer()],
+    },
   },
   css: {
     postcss: {
