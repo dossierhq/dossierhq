6baf102d719f2744baacd8661adfd475536d6180
Jonas Bengtsson <jonas.b@gmail.com>
2022-05-26 15:39:52 +0200

    feat(playground): Include starwars example

--- apps/playground/src/routes/IndexRoute.tsx
+++ apps/playground/src/routes/IndexRoute.tsx
@@ -1,4 +1,15 @@
-import { Card, Field, File, FullscreenContainer, Text } from '@jonasb/datadata-design';
+import {
+  Button,
+  Card,
+  File,
+  FullscreenContainer,
+  NotificationContext,
+  NotificationInfo,
+  Text,
+  toClassName,
+  toSpacingClassName,
+} from '@jonasb/datadata-design';
+import starwarsUrl from 'playground-example-generator/dist/starwars.sqlite?url';
 import { ChangeEvent, useCallback, useContext } from 'react';
 import { Database } from 'sql.js';
 import useSWR from 'swr';
@@ -8,6 +19,7 @@ import { bytesToHumanSize } from '../utils/DisplayUtils';
 
 export function IndexRoute() {
   const { database, createDatabase } = useContext(DatabaseContext);
+  const { showNotification } = useContext(NotificationContext);
   const { data } = useSWR(database, queryDatabaseSize);
 
   const handleDownloadOnClick = useCallback(() => {
@@ -17,7 +29,7 @@ export function IndexRoute() {
   const handleFileChange = useCallback((event: ChangeEvent<HTMLInputElement>) => {
     if (event.target.files && event.target.files.length > 0) {
       const file = event.target.files[0];
-      uploadDatabase(file, createDatabase);
+      uploadDatabase(file, createDatabase, showNotification);
       event.target.files = null;
     }
   }, []);
@@ -38,29 +50,54 @@ export function IndexRoute() {
           </Card.Header>
           <Card.Content>
             <strong>Size:</strong> {data ? bytesToHumanSize(data.byteSize) : '–'}
-            <Field>
-              <Field.Label>Upload new database</Field.Label>
-              <Field.Control>
-                <File accept=".sqlite, application/x-sqlite3" onChange={handleFileChange} />
-              </Field.Control>
-            </Field>
+            <Button disabled={!database} iconLeft="download" onClick={handleDownloadOnClick}>
+              Download
+            </Button>
           </Card.Content>
-          <Card.Footer>
-            <Card.FooterButton
+        </Card>
+        <article
+          className={toClassName('message is-warning', toSpacingClassName({ marginTop: 5 }))}
+        >
+          <div className="message-header">
+            <p>Change database</p>
+          </div>
+          <div className="message-body">
+            <Text textStyle="headline5">Reset</Text>
+            <p>Reset the database to start from scratch.</p>
+            <Button
+              className={toSpacingClassName({ marginTop: 2, marginBottom: 4 })}
               disabled={!database}
-              onClick={() => {
-                if (window.confirm('Are you sure you want to delete the current database?')) {
-                  createDatabase(null);
-                }
-              }}
+              onClick={() => resetDatabase(createDatabase, showNotification)}
             >
               Reset
-            </Card.FooterButton>
-            <Card.FooterButton disabled={!database} onClick={handleDownloadOnClick}>
-              Download
-            </Card.FooterButton>
-          </Card.Footer>
-        </Card>
+            </Button>
+            <Text textStyle="headline5">Example: Star Wars</Text>
+            <p>
+              An example database with information about Star Wars films, people, species, planets,
+              star ships, transports and vehicles.
+            </p>
+            <p>
+              The information comes from{' '}
+              <a href="https://swapi.dev/" target="_blank" rel="noopener noreferrer">
+                SWAPI
+              </a>{' '}
+              (The Star Wars API).
+            </p>
+            <Button
+              className={toSpacingClassName({ marginTop: 2, marginBottom: 4 })}
+              onClick={() => loadDatabaseFromUrl(starwarsUrl, createDatabase, showNotification)}
+            >
+              Load Star Wars
+            </Button>
+            <Text textStyle="headline5">Upload new database</Text>
+            <p>You can upload a database that you've downloaded from the Playground before.</p>
+            <File
+              className={toSpacingClassName({ marginTop: 2 })}
+              accept=".sqlite, application/x-sqlite3"
+              onChange={handleFileChange}
+            />
+          </div>
+        </article>
       </FullscreenContainer.Row>
     </FullscreenContainer>
   );
@@ -89,11 +126,57 @@ function downloadDatabase(database: Database) {
   a.click();
 }
 
-function uploadDatabase(file: File, createDatabase: (data: Uint8Array | null) => void) {
+function resetDatabase(
+  createDatabase: (data: Uint8Array | null) => void,
+  showNotification: (notification: NotificationInfo) => void
+) {
+  if (!window.confirm('Are you sure you want to delete the current database?')) {
+    return;
+  }
+
+  createDatabase(null);
+  showNotification({ color: 'success', message: 'New database created' });
+}
+
+async function loadDatabaseFromUrl(
+  url: string,
+  createDatabase: (data: Uint8Array | null) => void,
+  showNotification: (notification: NotificationInfo) => void
+) {
+  if (!window.confirm('Are you sure you want to delete the current database?')) {
+    return;
+  }
+
+  // Cleanup url when running in dev mode
+  if (url.startsWith('/../')) {
+    url = url.replace('/../', '/node_modules/');
+  }
+
+  const response = await fetch(url);
+  if (response.ok) {
+    const buffer = await response.arrayBuffer();
+    const data = new Uint8Array(buffer);
+    createDatabase(data);
+    showNotification({ color: 'success', message: 'New database loaded' });
+  } else {
+    showNotification({ color: 'error', message: 'Failed downloading database' });
+  }
+}
+
+function uploadDatabase(
+  file: File,
+  createDatabase: (data: Uint8Array | null) => void,
+  showNotification: (notification: NotificationInfo) => void
+) {
+  if (!window.confirm('Are you sure you want to delete the current database?')) {
+    return;
+  }
+
   const reader = new FileReader();
   reader.onload = () => {
     const data = new Uint8Array(reader.result as ArrayBuffer);
     createDatabase(data);
+    showNotification({ color: 'success', message: 'New database loaded' });
   };
   reader.readAsArrayBuffer(file);
 }
