c1167954f40ea774953ca06071c82634a09460ae
Jonas Bengtsson <jonas.b@gmail.com>
2022-05-26 15:38:25 +0200

    feat(design): Add download icon, expose class names

--- libraries/design/src/components/File/File.tsx
+++ libraries/design/src/components/File/File.tsx
@@ -5,14 +5,15 @@ import { toClassName } from '../../utils/ClassNameUtils.js';
 import { icons } from '../Icon/Icon.js';
 
 export interface FileProps {
+  className?: string;
   accept?: string;
   boxed?: boolean;
   onChange?: ChangeEventHandler<HTMLInputElement>;
 }
 
-export function File({ accept, boxed, onChange }: FileProps): JSX.Element {
+export function File({ className, accept, boxed, onChange }: FileProps): JSX.Element {
   return (
-    <div className={toClassName('file', boxed && 'is-boxed')}>
+    <div className={toClassName('file', boxed && 'is-boxed', className)}>
       <label className="file-label">
         <input className="file-input" type="file" accept={accept} onChange={onChange} />
         <span className="file-cta">
