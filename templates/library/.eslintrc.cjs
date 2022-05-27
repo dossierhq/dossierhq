13eff98e5dc119b5cae3a9af0d1ebc4deefbd15c
Jonas Bengtsson <jonas.b@gmail.com>
2022-05-28 01:32:13 +0200

    chore: Add eslint-config-generic

--- templates/library/.eslintrc.cjs
+++ templates/library/.eslintrc.cjs
@@ -1,28 +1,5 @@
+require('@jonasb/eslint-config-datadata-generic/patch/modern-module-resolution.js');
+
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-  },
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaVersion: 12,
-    sourceType: 'module',
-  },
-  plugins: ['@typescript-eslint'],
-  reportUnusedDisableDirectives: true,
-  rules: {
-    '@typescript-eslint/consistent-type-imports': ['warn'],
-    '@typescript-eslint/no-parameter-properties': ['error'],
-    '@typescript-eslint/no-unused-vars': [
-      'warn',
-      {
-        vars: 'local',
-        args: 'after-used',
-        ignoreRestSiblings: true,
-        varsIgnorePattern: '^_',
-        argsIgnorePattern: '^_',
-      },
-    ],
-  },
+  extends: ['@jonasb/eslint-config-datadata-generic/profile/library-esm.js'],
 };
