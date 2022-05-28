ebad2a61d6be619ab9409d816ab6ac467ea95133
Jonas Bengtsson <jonas.b@gmail.com>
2022-05-28 22:24:56 +0200

    chore(dql.js): Export ESM/CJS, switch to vitest and shared eslint config

--- libraries/database-adapter-sqlite-sqljs/src/test/integration/PublishedEntityTest.test.ts
+++ libraries/database-adapter-sqlite-sqljs/src/test/integration/PublishedEntityTest.test.ts
@@ -5,6 +5,7 @@ import {
   createReadOnlyEntityRepository,
 } from '@jonasb/datadata-database-adapter-test-integration';
 import type { Server } from '@jonasb/datadata-server';
+import { afterAll, beforeAll } from 'vitest';
 import { registerTestSuite } from '../TestUtils';
 import { initializeSqlJsServer } from './SqlJsTestUtils';
 
