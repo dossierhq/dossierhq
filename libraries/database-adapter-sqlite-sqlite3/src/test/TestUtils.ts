1909edc6df05ad26f148bf9bf7d6628ec24cd4ec
Jonas Bengtsson <jonas.b@gmail.com>
2022-05-28 21:38:14 +0200

    chore(sqlite3): Export ESM/CJS, switch to vitest and shared eslint config

--- libraries/database-adapter-sqlite-sqlite3/src/test/TestUtils.ts
+++ libraries/database-adapter-sqlite-sqlite3/src/test/TestUtils.ts
@@ -1,7 +1,8 @@
 import type { TestSuite } from '@jonasb/datadata-database-adapter-test-integration';
+import { test } from 'vitest';
 
 export function registerTestSuite(testSuite: TestSuite): void {
   for (const [testName, testFunction] of Object.entries(testSuite)) {
-    test(testName, testFunction as jest.ProvidesCallback);
+    test(testName, testFunction);
   }
 }
