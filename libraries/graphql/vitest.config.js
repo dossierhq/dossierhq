4d5e0ed8ea957f549dfa6f793c9b029e67031f77
Jonas Bengtsson <jonas.b@gmail.com>
2022-05-28 23:09:17 +0200

    chore(gql): Reenable threading on vitest

--- libraries/graphql/vitest.config.js
+++ libraries/graphql/vitest.config.js
@@ -3,8 +3,5 @@ export default {
     include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
     watch: false,
     setupFiles: ['./src/test/setup.ts'],
-    threads: false,
-    minThreads: 1,
-    maxThreads: 1,
   },
 };
