a4f2ee9636d9e0060b8efd9a636ec8856ded1134
Jonas Bengtsson <jonas.b@gmail.com>
2022-05-28 23:52:53 +0200

    chore(next): Replace Jest with vitest

--- examples/next-web/jest.config.cjs
+++
@@ -1,7 +0,0 @@
-const nextJest = require('next/jest');
-
-const createJestConfig = nextJest({ dir: './' });
-
-const customJestConfig = {};
-
-module.exports = createJestConfig(customJestConfig);
--- examples/next-web/utils/PageUtils.test.ts
+++ examples/next-web/utils/PageUtils.test.ts
@@ -1,7 +1,8 @@
+import { describe, expect, test } from 'vitest';
 import { urls } from './PageUtils';
 
 describe('urls', () => {
   test('editPage', () => {
-    expect(urls.editPage(['id1', 'id2'])).toBe('/entities/edit?ids=id1,id2');
+    expect(urls.editPage(['id1', 'id2'])).toBe('/entities/edit?id=id1&id=id2');
   });
 });
