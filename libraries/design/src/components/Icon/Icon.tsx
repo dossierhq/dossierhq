8972512650d2fbcea689e92b9e277a97fa995141
Jonas Bengtsson <jonas.b@gmail.com>
2022-05-30 14:20:01 +0200

    chore(design): Add icons page

--- libraries/design/src/components/Icon/Icon.tsx
+++ libraries/design/src/components/Icon/Icon.tsx
@@ -19,11 +19,13 @@ import {
   faXmark,
 } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import type { CSSProperties } from 'react';
 import React from 'react';
 import { toClassName } from '../../utils/ClassNameUtils.js';
 
 export interface IconProps {
   className?: string;
+  style?: CSSProperties;
   icon?: IconName | null;
   text?: boolean;
   size?: 'small' | '' | 'medium' | 'large';
@@ -64,17 +66,24 @@ const iconSize = {
   large: '2x',
 } as const;
 
-export function Icon({ className, icon, text, size }: IconProps): JSX.Element {
+export function Icon({ className, style, icon, text, size }: IconProps): JSX.Element {
   const iconImage = icon ? icons[icon] : null;
   const bulmaIcon = (
-    <span className={toClassName('icon', size && containerSize[size], !text && className)}>
+    <span
+      className={toClassName('icon', size && containerSize[size], !text && className)}
+      style={text ? undefined : style}
+    >
       {iconImage ? (
         <FontAwesomeIcon icon={iconImage} size={size ? iconSize[size] : undefined} />
       ) : null}
     </span>
   );
   if (text) {
-    return <span className={toClassName(className, 'icon-text')}>{bulmaIcon}</span>;
+    return (
+      <span className={toClassName(className, 'icon-text')} style={style}>
+        {bulmaIcon}
+      </span>
+    );
   }
   return bulmaIcon;
 }
---
+++ libraries/design/src/stories/IconSwatch.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import type { IconName, IconProps } from '../components/Icon/Icon.js';
+import { icons } from '../components/Icon/Icon.js';
+import { Icon, Table } from '../components/index.js';
+
+const sizes = ['small', '', 'medium', 'large'] as const;
+
+export function IconSwatch({ icon }: IconProps): JSX.Element {
+  return (
+    <Table.Row>
+      <Table.Cell>{icon}</Table.Cell>
+      {sizes.map((size, i) => (
+        <Table.Cell key={i}>
+          <Icon size={size} icon={icon} style={{ border: '1px solid lightgray' }} />
+        </Table.Cell>
+      ))}
+    </Table.Row>
+  );
+}
+
+export function AllIconSwatches(): JSX.Element {
+  return (
+    <Table>
+      <Table.Head>
+        <Table.Header>Name</Table.Header>
+        {sizes.map((size, i) => (
+          <Table.Header key={i}>{`Size: ${size}`}</Table.Header>
+        ))}
+      </Table.Head>
+      <Table.Body>
+        {Object.keys(icons).map((icon) => (
+          <IconSwatch key={icon} icon={icon as IconName} />
+        ))}
+      </Table.Body>
+    </Table>
+  );
+}
