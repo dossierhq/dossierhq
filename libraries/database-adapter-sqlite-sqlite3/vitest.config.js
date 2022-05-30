3299df2b67a37da0f96de286351e0760b57c333c
Jonas Bengtsson <jonas.b@gmail.com>
2022-05-30 19:00:13 +0200

    chore(sqlite3): Enable multithreaded vitest (since upgrading node-sqlite3)

--- libraries/database-adapter-sqlite-sqlite3/vitest.config.js
+++ libraries/database-adapter-sqlite-sqlite3/vitest.config.js
@@ -2,8 +2,5 @@ export default {
   test: {
     include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
     watch: false,
-    // Disable multi-threading. For some reason it results in a v8 crash
-    minThreads: 1,
-    maxThreads: 1,
   },
 };
