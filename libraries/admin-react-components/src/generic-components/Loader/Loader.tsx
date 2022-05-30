42b81b84e0a06b5182c5c23fd04f78b59a9f5de8
Jonas Bengtsson <jonas.b@gmail.com>
2022-05-30 15:12:41 +0200

    chore(arc): Remove generic-components

--- libraries/admin-react-components/src/generic-components/Loader/Loader.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export type LoaderProps = Record<string, never>;
-
-export function Loader(): JSX.Element {
-  return <div className="dd-loader" />;
-}
--- libraries/admin-react-components/src/generic-components/Segment/Segment.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-interface SegmentProps {
-  children: React.ReactNode;
-}
-
-export function Segment({ children }: SegmentProps): JSX.Element {
-  return <div className="dd-segment dd-has-shadow">{children}</div>;
-}
