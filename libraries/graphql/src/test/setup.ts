ab8ca557875a38f22123cdb881d5c7809014eb50
Jonas Bengtsson <jonas.b@gmail.com>
2022-05-30 20:44:09 +0200

    chore(arc): Switch to sqlite3

--- libraries/graphql/src/test/setup.ts
+++
@@ -1,7 +0,0 @@
-import * as dotenv from 'dotenv';
-import * as path from 'path';
-
-dotenv.config({ path: path.resolve(__dirname, '..', '..', '.env') });
-
-// eslint-disable-next-line @typescript-eslint/no-empty-function
-export default function (): void {}
