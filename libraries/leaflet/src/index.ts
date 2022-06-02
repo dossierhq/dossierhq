6aa7b6d8fccf48395b257ada0728ba4f26ed0c12
Jonas Bengtsson <jonas.b@gmail.com>
2022-06-02 23:56:39 +0200

    feat(leaflet): Export some utilities

--- libraries/leaflet/src/index.ts
+++ libraries/leaflet/src/index.ts
@@ -1,1 +1,4 @@
+export type { BoundingBox, Location } from './CoreTypes.js';
 export { MapContainer } from './MapContainer.js';
+export { expandBoundingBoxForMarkers } from './MarkerUtils.js';
+export type { ZoomMetrics } from './Types.js';
