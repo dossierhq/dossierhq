67e903a80cccd7fd3d4a9e6ffba80d3dbc2f0214
Jonas Bengtsson <jonas.b@gmail.com>
2022-06-06 23:05:48 +0200

    feat(leaflet): Export MarkerColor

--- libraries/leaflet/src/index.ts
+++ libraries/leaflet/src/index.ts
@@ -1,4 +1,5 @@
 export type { BoundingBox, Location } from './CoreTypes.js';
 export { MapContainer } from './MapContainer.js';
 export { expandBoundingBoxForMarkers } from './MarkerUtils.js';
+export type { MarkerColor } from './MarkerUtils.js';
 export type { ZoomMetrics } from './Types.js';
