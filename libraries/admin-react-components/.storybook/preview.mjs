d5783300a0dd407537bb867a67a2f0a382b21b2b
Jonas Bengtsson <jonas.b@gmail.com>
2022-06-06 17:19:33 +0200

    fix(design/arc): Fix brokwn storybook

--- libraries/admin-react-components/.storybook/preview.mjs
+++ libraries/admin-react-components/.storybook/preview.mjs
@@ -2,7 +2,6 @@ import '@jonasb/datadata-design/main.css';
 import '@jonasb/datadata-leaflet/icons.css';
 import '@jonasb/datadata-leaflet/main.css';
 import '../src/main.css';
-import '../src/icons.css';
 import 'leaflet/dist/leaflet.css';
 
 export const parameters = {
