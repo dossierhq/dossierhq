cf9509e0498b02240f4e1624c27c18da95985113
Jonas Bengtsson <jonas.b@gmail.com>
2022-06-07 23:27:15 +0200

    chore(next-web): Cleanup URLSearchParams

--- examples/next-web/components/EntityEditorPage/EntityEditorPage.tsx
+++ examples/next-web/components/EntityEditorPage/EntityEditorPage.tsx
@@ -1,33 +1,14 @@
 import { EntityEditorScreen } from '@jonasb/datadata-admin-react-components';
 import Head from 'next/head';
-import { useRouter } from 'next/router';
-import { useCallback, useMemo, useState } from 'react';
+import { useState } from 'react';
 import { DataDataSharedProvider } from '../../contexts/DataDataSharedProvider';
+import { useUrlSearchParams } from '../../hooks/useUrlSearchParams';
 import { useWarningOnExit } from '../../hooks/useWarningOnExit';
 import { NavBar } from '../NavBar/NavBar';
 
 export default function EntityEditorPage(): JSX.Element {
-  const router = useRouter();
+  const { onUrlSearchParamsChange, urlSearchParams } = useUrlSearchParams();
   const [hasChanges, setHasChanges] = useState(false);
-
-  const urlSearchParams = useMemo(() => {
-    const result = new URLSearchParams();
-    for (const [key, value] of Object.entries(router.query)) {
-      if (Array.isArray(value)) {
-        value.forEach((valueItem) => result.append(key, valueItem));
-      } else if (typeof value === 'string') {
-        result.append(key, value);
-      }
-    }
-    return result;
-  }, [router.query]);
-
-  const handleUrlSearchParamsChange = useCallback(
-    (urlSearchParams: URLSearchParams) => {
-      router.replace({ pathname: router.pathname, query: urlSearchParams.toString() });
-    },
-    [router]
-  );
 
   useWarningOnExit('Changes will be lost, are you sure you want to leave the page?', hasChanges);
 
@@ -39,7 +20,7 @@ export default function EntityEditorPage(): JSX.Element {
       <EntityEditorScreen
         header={<NavBar current="entities" />}
         urlSearchParams={urlSearchParams}
-        onUrlSearchParamsChange={handleUrlSearchParamsChange}
+        onUrlSearchParamsChange={onUrlSearchParamsChange}
         onEditorHasChangesChange={setHasChanges}
       />
     </DataDataSharedProvider>
