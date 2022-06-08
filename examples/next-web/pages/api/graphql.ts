29ca9ef4dec94e0383dfc2eecb6d9d7da62159f2
Jonas Bengtsson <jonas.b@gmail.com>
2022-06-08 22:35:32 +0200

    chore(next-web): Cleanup and remove joi and boom

--- examples/next-web/pages/api/graphql.ts
+++ examples/next-web/pages/api/graphql.ts
@@ -4,7 +4,6 @@ import { GraphQLSchemaGenerator } from '@jonasb/datadata-graphql';
 import type { ExecutionResult, GraphQLSchema } from 'graphql';
 import { graphql } from 'graphql';
 import type { NextApiRequest, NextApiResponse } from 'next';
-import { errorResultToBoom } from '../../utils/ErrorUtils';
 import { handlePostWithoutLocation } from '../../utils/HandlerUtils';
 import { getServerConnection, getSessionContextForRequest } from '../../utils/ServerUtils';
 
@@ -17,10 +16,9 @@ export default async function graphQlHandler(
   await handlePostWithoutLocation(req, res, async () => {
     const { server, schema } = await getServerConnection();
     const authResult = await getSessionContextForRequest(server, req);
-    if (authResult.isError()) {
-      throw errorResultToBoom(authResult);
-    }
+    if (authResult.isError()) return authResult;
     const { adminClient, publishedClient } = authResult.value;
+
     const context: SessionGraphQLContext = {
       adminClient: ok(adminClient),
       publishedClient: ok(publishedClient),
@@ -42,6 +40,6 @@ export default async function graphQlHandler(
       operationName,
     });
 
-    return result;
+    return ok(result);
   });
 }
--- examples/next-web/pages/api/published/[operationName].ts
+++ examples/next-web/pages/api/published/[operationName].ts
@@ -1,86 +1,58 @@
-import Boom from '@hapi/boom';
-import type { PublishedClientJsonOperation } from '@jonasb/datadata-core';
+import type {
+  PublishedClientJsonOperation,
+  PublishedClientOperationName,
+} from '@jonasb/datadata-core';
 import {
   decodeUrlQueryStringifiedParam,
   executePublishedClientOperationFromJson,
-  PublishedClientOperationName,
+  notOk,
 } from '@jonasb/datadata-core';
-import Joi from 'joi';
 import type { NextApiRequest, NextApiResponse } from 'next';
 import type { PublishedOperationRequest } from '../../../types/RequestTypes';
-import { errorResultToBoom } from '../../../utils/ErrorUtils';
-import {
-  handleError,
-  handleGet,
-  handlePut,
-  validateRequestQuery,
-} from '../../../utils/HandlerUtils';
+import { handleGet, handlePut, sendMethodNotAllowedError } from '../../../utils/HandlerUtils';
 import { getServerConnection, getSessionContextForRequest } from '../../../utils/ServerUtils';
-
-interface RequestQuery {
-  operationName: keyof typeof PublishedClientOperationName;
-  operation?: string;
-}
-
-const requestSchema = Joi.object<RequestQuery>({
-  operationName: Joi.string()
-    .required()
-    .allow(...Object.values(PublishedClientOperationName)),
-  operation: Joi.string().optional(),
-});
 
 export default async function publishedOperationHandler(
   req: NextApiRequest,
   res: NextApiResponse
 ): Promise<void> {
-  const { operationName } = validateRequestQuery(req.query, requestSchema);
   if (req.method === 'GET') {
     await handleGet(req, res, async () => {
-      const { server } = await getServerConnection();
-      const authResult = await getSessionContextForRequest(server, req);
-      if (authResult.isError()) {
-        throw errorResultToBoom(authResult);
-      }
-      const { publishedClient } = authResult.value;
-
       const operation: PublishedClientJsonOperation | undefined = decodeUrlQueryStringifiedParam(
         'operation',
         req.query
       );
       if (!operation) {
-        throw Boom.badRequest('Missing operation query');
+        return notOk.BadRequest('Missing operation query');
       }
 
-      const result = await executePublishedClientOperationFromJson(
-        publishedClient,
-        operationName,
-        operation
-      );
-      if (result.isError()) {
-        throw errorResultToBoom(result);
-      }
-      return result.value;
+      return executePublishedOperation(req, operation);
     });
   } else if (req.method === 'PUT') {
     await handlePut(req, res, async (body: PublishedOperationRequest) => {
-      const { server } = await getServerConnection();
-      const authResult = await getSessionContextForRequest(server, req);
-      if (authResult.isError()) {
-        throw errorResultToBoom(authResult);
-      }
-      const { publishedClient } = authResult.value;
-
-      const result = await executePublishedClientOperationFromJson(
-        publishedClient,
-        operationName,
-        body
-      );
-      if (result.isError()) {
-        throw errorResultToBoom(result);
-      }
-      return result.value;
+      return executePublishedOperation(req, body);
     });
   } else {
-    handleError(res, Boom.methodNotAllowed(undefined, undefined, ['GET', 'PUT']));
+    sendMethodNotAllowedError(res, ['GET', 'PUT']);
   }
+}
+
+async function executePublishedOperation(
+  req: NextApiRequest,
+  operation: PublishedClientJsonOperation
+) {
+  const { operationName } = req.query;
+  if (typeof operationName !== 'string') return notOk.BadRequest('Operation name not provided');
+
+  const { server } = await getServerConnection();
+  const authResult = await getSessionContextForRequest(server, req);
+  if (authResult.isError()) return authResult;
+  const { publishedClient } = authResult.value;
+
+  const result = await executePublishedClientOperationFromJson(
+    publishedClient,
+    operationName as keyof typeof PublishedClientOperationName,
+    operation
+  );
+  return result;
 }
