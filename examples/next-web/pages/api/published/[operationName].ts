2515b39c55e43a7b534616e3f05960661d73ea63
Jonas Bengtsson <jonas.b@gmail.com>
2022-06-08 23:02:32 +0200

    chore(next-web): Cleanup request handling

--- examples/next-web/pages/api/published/[operationName].ts
+++ examples/next-web/pages/api/published/[operationName].ts
@@ -1,48 +1,53 @@
 import type {
+  ErrorType,
   PublishedClientJsonOperation,
   PublishedClientOperationName,
+  Result,
 } from '@jonasb/datadata-core';
 import {
   decodeUrlQueryStringifiedParam,
   executePublishedClientOperationFromJson,
   notOk,
+  ok,
 } from '@jonasb/datadata-core';
 import type { NextApiRequest, NextApiResponse } from 'next';
-import type { PublishedOperationRequest } from '../../../types/RequestTypes';
-import { handleGet, handlePut, sendMethodNotAllowedError } from '../../../utils/HandlerUtils';
+import { handleRequest, sendMethodNotAllowedError } from '../../../utils/HandlerUtils';
 import { getServerConnection, getSessionContextForRequest } from '../../../utils/ServerUtils';
 
 export default async function publishedOperationHandler(
   req: NextApiRequest,
   res: NextApiResponse
 ): Promise<void> {
-  if (req.method === 'GET') {
-    await handleGet(req, res, async () => {
-      const operation: PublishedClientJsonOperation | undefined = decodeUrlQueryStringifiedParam(
-        'operation',
-        req.query
-      );
-      if (!operation) {
-        return notOk.BadRequest('Missing operation query');
-      }
-
-      return executePublishedOperation(req, operation);
-    });
-  } else if (req.method === 'PUT') {
-    await handlePut(req, res, async (body: PublishedOperationRequest) => {
-      return executePublishedOperation(req, body);
+  if (req.method === 'GET' || req.method === 'PUT') {
+    await handleRequest(res, async () => {
+      return executePublishedOperation(req);
     });
   } else {
     sendMethodNotAllowedError(res, ['GET', 'PUT']);
   }
 }
 
-async function executePublishedOperation(
-  req: NextApiRequest,
-  operation: PublishedClientJsonOperation
-) {
+function getOperation(
+  req: NextApiRequest
+): Result<PublishedClientJsonOperation, typeof ErrorType.BadRequest> {
+  let operation: PublishedClientJsonOperation | undefined;
+  if (req.method === 'GET') {
+    operation = decodeUrlQueryStringifiedParam('operation', req.query);
+  } else {
+    operation = req.body;
+  }
+  if (!operation) {
+    return notOk.BadRequest('Missing operation');
+  }
+  return ok(operation);
+}
+
+async function executePublishedOperation(req: NextApiRequest) {
   const { operationName } = req.query;
   if (typeof operationName !== 'string') return notOk.BadRequest('Operation name not provided');
+
+  const operationResult = getOperation(req);
+  if (operationResult.isError()) return operationResult;
 
   const { server } = await getServerConnection();
   const authResult = await getSessionContextForRequest(server, req);
@@ -52,7 +57,7 @@ async function executePublishedOperation(
   const result = await executePublishedClientOperationFromJson(
     publishedClient,
     operationName as keyof typeof PublishedClientOperationName,
-    operation
+    operationResult.value
   );
   return result;
 }
--- examples/next-web/utils/HandlerUtils.ts
+++ examples/next-web/utils/HandlerUtils.ts
@@ -1,100 +1,38 @@
-import type { ErrorType, PromiseResult } from '@jonasb/datadata-core';
-import { createConsoleLogger, createErrorResultFromError, ok } from '@jonasb/datadata-core';
-import debug from 'debug';
-import type { NextApiRequest, NextApiResponse } from 'next';
+import type { ErrorResult, ErrorType, PromiseResult } from '@jonasb/datadata-core';
+import { createConsoleLogger, notOk } from '@jonasb/datadata-core';
+import type { NextApiResponse } from 'next';
 
-const debugWarn = debug('app:w:handler');
 const logger = createConsoleLogger(console);
 
-export function handleError<T>(res: NextApiResponse<T>, error: Error | unknown): void {
-  const result = createErrorResultFromError({ logger }, error);
-
-  sendErrorResponse(res, result.httpStatus, result.message);
-}
-
-function sendErrorResponse<T>(res: NextApiResponse<T>, httpStatus: number, message: string) {
-  res.status(httpStatus).json({
-    message,
-  } as unknown as T);
+function handleError<T>(res: NextApiResponse<T>, error: ErrorResult<unknown, ErrorType>): void {
+  res.status(error.httpStatus).json({ message: error.message } as any);
 }
 
 export function sendMethodNotAllowedError<T>(
   res: NextApiResponse<T>,
-  allowMethods: ('GET' | 'HEAD' | 'POST' | 'PUT' | 'DELETE')[]
+  allowedMethods: ('GET' | 'HEAD' | 'POST' | 'PUT' | 'DELETE')[]
 ) {
-  sendErrorResponse(res, 405, 'Method Not allowed');
-  res.setHeader('Allow', allowMethods.join(', '));
+  res.setHeader('Allow', allowedMethods.join(', '));
+  res.status(405).end();
 }
 
-async function handleRequest<T>(
-  res: NextApiResponse<T>,
-  handler: () => PromiseResult<void, ErrorType>
-) {
-  try {
-    const result = await handler();
-    if (result.isError()) {
-      sendErrorResponse(res, result.httpStatus, result.message);
-    }
-  } catch (error) {
-    debugWarn('Failed request', error);
-    handleError(res, error);
-  }
-}
-
-export async function handleGet<T>(
-  req: NextApiRequest,
+export async function handleRequest<T>(
   res: NextApiResponse<T>,
   handler: () => PromiseResult<T, ErrorType>
 ): Promise<void> {
-  await handleRequest(res, async () => {
-    if (req.method !== 'GET') {
-      sendMethodNotAllowedError(res, ['GET']);
-      return ok(undefined);
-    }
+  try {
     const bodyResult = await handler();
-    if (bodyResult.isError()) return bodyResult;
-
+    if (bodyResult.isError()) {
+      handleError(res, bodyResult);
+      return;
+    }
     // next.js built-in json conversion doesn't handle null (sends empty response)
     res
       .status(200)
       .setHeader('Content-Type', 'application/json; charset=utf-8')
       .send(JSON.stringify(bodyResult.value, null, 2) as unknown as T);
-    return ok(undefined);
-  });
-}
-
-export async function handlePostWithoutLocation<T, R>(
-  req: NextApiRequest,
-  res: NextApiResponse<R>,
-  handler: (body: T) => PromiseResult<R, ErrorType>
-): Promise<void> {
-  await handleRequest(res, async () => {
-    if (req.method !== 'POST') {
-      sendMethodNotAllowedError(res, ['POST']);
-      return ok(undefined);
-    }
-    const result = await handler(req.body);
-    if (result.isError()) return result;
-
-    res.status(200).json(result.value);
-    return ok(undefined);
-  });
-}
-
-export async function handlePut<T, R>(
-  req: NextApiRequest,
-  res: NextApiResponse<R>,
-  handler: (body: T) => PromiseResult<R, ErrorType>
-): Promise<void> {
-  await handleRequest(res, async () => {
-    if (req.method !== 'PUT') {
-      sendMethodNotAllowedError(res, ['PUT']);
-      return ok(undefined);
-    }
-    const bodyResult = await handler(req.body);
-    if (bodyResult.isError()) return bodyResult;
-
-    res.status(200).json(bodyResult.value);
-    return ok(undefined);
-  });
+  } catch (error) {
+    const result = notOk.GenericUnexpectedException({ logger }, error);
+    handleError(res, result);
+  }
 }
