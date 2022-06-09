94e592443e905dc01a8f1a43cea5308bc98e575b
Jonas Bengtsson <jonas.b@gmail.com>
2022-06-09 23:41:51 +0200

    base react eslint-config on generic

--- stack/eslint-config-react/config.js
+++ stack/eslint-config-react/config.js
@@ -1,45 +1,16 @@
 function buildRules(profile) {
   return {
-    root: true,
-    env: {
-      browser: true,
-      es2021: true,
-    },
     extends: [
-      "eslint:recommended",
-      "plugin:@typescript-eslint/recommended",
+      "@jonasb/eslint-config-datadata-generic/profile/library-esm.js",
       "plugin:react/recommended",
       "plugin:react-hooks/recommended",
     ],
     parserOptions: {
       ecmaFeatures: { jsx: true },
-      ecmaVersion: 12,
-      sourceType: "module",
     },
-    parser: "@typescript-eslint/parser",
-    plugins: ["@typescript-eslint", "react", "react-hooks"],
     rules: {
-      "@typescript-eslint/consistent-type-imports": ["warn"],
-      "@typescript-eslint/no-parameter-properties": ["error"],
-      "@typescript-eslint/no-unused-vars": [
-        "warn",
-        {
-          vars: "local",
-          args: "after-used",
-          ignoreRestSiblings: true,
-          varsIgnorePattern: "^_",
-          argsIgnorePattern: "^_",
-        },
-      ],
       "react/react-in-jsx-scope": "off",
     },
-    overrides: [
-      {
-        // Enable export/import for config files
-        files: ["*.js", "*.cjs"],
-        env: { node: true },
-      },
-    ],
     settings: { react: { version: "detect" } },
   };
 }
