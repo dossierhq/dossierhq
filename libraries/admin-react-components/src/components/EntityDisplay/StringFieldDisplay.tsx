852370faf6d72c0959ac04f3984579810783efaa
Jonas Bengtsson <jonas.b@gmail.com>
2022-06-12 20:23:16 +0200

    feat(arc): Support multiline

--- libraries/admin-react-components/src/components/EntityDisplay/StringFieldDisplay.tsx
+++ libraries/admin-react-components/src/components/EntityDisplay/StringFieldDisplay.tsx
@@ -1,9 +1,13 @@
-import { Input } from '@jonasb/datadata-design';
+import { Input, TextArea } from '@jonasb/datadata-design';
 import React from 'react';
 import type { FieldDisplayProps } from './FieldDisplay';
 
 type Props = FieldDisplayProps<string>;
 
-export function StringFieldDisplay({ value }: Props) {
-  return <Input value={value ?? ''} readOnly />;
+export function StringFieldDisplay({ fieldSpec, value }: Props) {
+  return fieldSpec.multiline ? (
+    <TextArea value={value ?? ''} readOnly />
+  ) : (
+    <Input value={value ?? ''} readOnly />
+  );
 }
