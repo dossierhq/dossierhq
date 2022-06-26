a888752846739a295eeb99eb05f1bdfc0459c518
Jonas Bengtsson <jonas.b@gmail.com>
2022-06-26 11:37:27 +0200

    chore(cli): Remove package

--- libraries/cli/src/CliContext.ts
+++
@@ -1,7 +0,0 @@
-import type { AdminClient, PublishedClient, AdminSchema } from '@jonasb/datadata-core';
-
-export interface CliContext {
-  schema: AdminSchema;
-  adminClient: AdminClient;
-  publishedClient: PublishedClient;
-}
