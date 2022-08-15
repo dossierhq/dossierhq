069a649895da9367a75df726735f12c666356b79
Jonas Bengtsson <jonas.b@gmail.com>
2022-08-15 18:14:10 +0200

    chore(arc): Remove icons script

--- libraries/admin-react-components/src/icons.ts
+++
@@ -1,6 +0,0 @@
-export default {
-  'entity-tool':
-    '<svg viewBox="0 0 14 14" xmlns="http://www.w3.org/2000/svg" xml:space="preserve" style="fill-rule:evenodd;clip-rule:evenodd;stroke-linecap:round;stroke-linejoin:round;stroke-miterlimit:1.5"><path d="M9 10H5V4h4M8 7H5" style="fill:none;stroke:#707684;stroke-width:1.3px"/></svg>',
-  'value-item-tool':
-    '<svg viewBox="0 0 14 14" xmlns="http://www.w3.org/2000/svg" xml:space="preserve" style="fill-rule:evenodd;clip-rule:evenodd;stroke-linecap:round;stroke-linejoin:round;stroke-miterlimit:1.5"><path d="m5 4 2 6M9 4l-2 6" style="fill:none;stroke:#707684;stroke-width:1.3px"/></svg>',
-};
