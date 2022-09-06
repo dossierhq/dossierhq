514a3d45c40ebf5acd002d133ec73fb69301b623
Jonas Bengtsson <jonas.b@gmail.com>
2022-09-06 12:04:03 +0200

    fix(next-web): Remove warning when navigating within the editor page

--- examples/next-web/components/EntityEditorPage/EntityEditorPage.tsx
+++ examples/next-web/components/EntityEditorPage/EntityEditorPage.tsx
@@ -1,16 +1,24 @@
 import { EntityEditorScreen } from '@jonasb/datadata-admin-react-components';
 import Head from 'next/head';
-import { useState } from 'react';
+import { useMemo, useState } from 'react';
 import { DataDataSharedProvider } from '../../contexts/DataDataSharedProvider';
 import { useUrlSearchParams } from '../../hooks/useUrlSearchParams';
 import { useWarningOnExit } from '../../hooks/useWarningOnExit';
+import { urls } from '../../utils/PageUtils';
 import { NavBar } from '../NavBar/NavBar';
 
 export default function EntityEditorPage(): JSX.Element {
   const { onUrlSearchParamsChange, urlSearchParams } = useUrlSearchParams();
   const [hasChanges, setHasChanges] = useState(false);
 
-  useWarningOnExit('Changes will be lost, are you sure you want to leave the page?', hasChanges);
+  const shouldWarn = useMemo(() => {
+    if (!hasChanges) return false;
+    return (_fromUrl: string, toUrl: string) => {
+      return !urls.isEditPage(toUrl);
+    };
+  }, [hasChanges]);
+
+  useWarningOnExit('Changes will be lost, are you sure you want to leave the page?', shouldWarn);
 
   return (
     <DataDataSharedProvider>
