0a23032223e9ec7b77cb5714283defd4c1173d8a
Jonas Bengtsson <jonas.b@gmail.com>
2022-09-07 22:26:43 +0200

    feat(arc): Filter auth keys for new entity

--- libraries/admin-react-components/src/components/EntityEditor/AuthKeyPicker.tsx
+++ libraries/admin-react-components/src/components/EntityEditor/AuthKeyPicker.tsx
@@ -1,25 +1,39 @@
 import { ButtonDropdown } from '@jonasb/datadata-design';
-import React from 'react';
-import { useContext } from 'react';
+import { useContext, useEffect } from 'react';
 import { AdminDataDataContext } from '../../contexts/AdminDataDataContext';
 
 interface Props {
+  patternName: string | null;
   value: string | null;
   onValueChange: (value: string) => void;
 }
 
-export function AuthKeyPicker({ value, onValueChange }: Props) {
-  const { authKeys } = useContext(AdminDataDataContext);
+export function AuthKeyPicker({ patternName, value, onValueChange }: Props) {
+  const { schema, authKeys } = useContext(AdminDataDataContext);
 
-  const items = authKeys.map((it) => ({ id: it.authKey, displayName: it.displayName }));
+  const pattern = patternName ? schema?.getPattern(patternName) : null;
+  const regexp = pattern ? new RegExp(pattern.pattern) : null;
+  const filteredAuthKeys = regexp
+    ? authKeys.filter((authKey) => regexp.test(authKey.authKey))
+    : authKeys;
+
+  const items = filteredAuthKeys.map((it) => ({ id: it.authKey, displayName: it.displayName }));
   let text = 'Select authorization key';
   if (value) {
     const key = authKeys.find((it) => it.authKey === value);
     text = key ? key.displayName : value;
   }
 
+  // Auto-select the first item if there is only one
+  useEffect(() => {
+    if (!value && items.length === 1) {
+      onValueChange(items[0].id);
+    }
+  }, [items, onValueChange, value]);
+
   return (
     <ButtonDropdown
+      disabled={!schema}
       items={items}
       renderItem={(item) => item.displayName}
       onItemClick={(item) => onValueChange(item.id)}
