4abb98e2c5debcb14717c15c7810d70bfe3ba924
Jonas Bengtsson <jonas.b@gmail.com>
2022-09-26 11:33:16 +0200

    feat(arc): Open draft when clicking on entity link node

--- libraries/admin-react-components/src/components/RichTextEditor/EntityLinkPlugin.tsx
+++ libraries/admin-react-components/src/components/RichTextEditor/EntityLinkPlugin.tsx
@@ -1,12 +1,22 @@
 import { useLexicalComposerContext } from '@lexical/react/LexicalComposerContext';
 import { COMMAND_PRIORITY_EDITOR } from 'lexical';
 import { useEffect } from 'react';
-import { toggleEntityLink, TOGGLE_ADMIN_ENTITY_LINK_COMMAND } from './AdminEntityLinkNode.js';
+import {
+  AdminEntityLinkNode,
+  toggleEntityLink,
+  TOGGLE_ADMIN_ENTITY_LINK_COMMAND,
+} from './AdminEntityLinkNode.js';
 
 export function EntityLinkPlugin(): null {
   const [editor] = useLexicalComposerContext();
 
   useEffect(() => {
+    if (!editor.hasNodes([AdminEntityLinkNode])) {
+      throw new Error(
+        'EntityLinkPlugin: AdminEntityLinkNode not registered on editor (initialConfig.nodes)'
+      );
+    }
+
     return editor.registerCommand(
       TOGGLE_ADMIN_ENTITY_LINK_COMMAND,
       (payload) => {
