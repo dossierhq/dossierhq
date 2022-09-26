5c46d34df6dab7f1e8f3e2ee27a887e56d66d0d0
Jonas Bengtsson <jonas.b@gmail.com>
2022-09-26 13:35:55 +0200

    fix(arc): Use value type picker dialog and create insert dropdown

---
+++ libraries/admin-react-components/src/utils/TypeSelectionUtils.ts
@@ -0,0 +1,33 @@
+import type {
+  AdminEntityTypeSpecification,
+  AdminSchema,
+  AdminValueTypeSpecification,
+} from '@jonasb/datadata-core';
+
+export interface TypeSelectionFilter {
+  showEntityTypes?: boolean;
+  entityTypes?: string[];
+  showValueTypes?: boolean;
+  valueTypes?: string[];
+}
+
+type Item = AdminEntityTypeSpecification | AdminValueTypeSpecification;
+
+export function filterTypeSpecifications(schema: AdminSchema, filter: TypeSelectionFilter): Item[] {
+  const items: Item[] = [];
+  if (filter.showEntityTypes) {
+    items.push(...filterItems(schema.spec.entityTypes, filter.entityTypes));
+  }
+  if (filter.showValueTypes) {
+    items.push(...filterItems(schema.spec.valueTypes, filter.valueTypes));
+  }
+  return items;
+}
+
+function filterItems(items: Item[], filterNames: string[] | undefined): Item[] {
+  let types = items;
+  if (filterNames && filterNames.length > 0) {
+    types = types.filter((it) => filterNames.indexOf(it.name) >= 0);
+  }
+  return types;
+}
