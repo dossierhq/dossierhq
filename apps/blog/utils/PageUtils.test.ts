ba75292d5efa644533f916a35b84725093a4f6e0
Jonas Bengtsson <jonas.b@gmail.com>
2022-10-25 22:43:17 +0200

    fix(blog): Fix broken test

--- apps/blog/utils/PageUtils.test.ts
+++ apps/blog/utils/PageUtils.test.ts
@@ -3,6 +3,6 @@ import { urls } from './PageUtils.js';
 
 describe('urls', () => {
   test('editPage', () => {
-    expect(urls.editPage(['id1', 'id2'])).toBe('/entities/edit?id=id1&id=id2');
+    expect(urls.editPage(['id1', 'id2'])).toBe('/admin-entities/edit?id=id1&id=id2');
   });
 });
