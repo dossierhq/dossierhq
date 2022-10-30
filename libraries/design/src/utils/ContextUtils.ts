ff46c8849ff2313afeca53681f2adc3532fea08b
Jonas Bengtsson <jonas.b@gmail.com>
2022-10-30 19:27:16 +0100

    fix(design): Support usage on Next.js server (except notifications)

---
+++ libraries/design/src/utils/ContextUtils.ts
@@ -0,0 +1,8 @@
+import type { Context } from 'react';
+import { createContext } from 'react';
+
+export function createClientContext<T>(defaultValue: T | undefined): Context<T> {
+  // When on server, return undefined since we can't use contexts on e.g. Next.js server components
+  if (typeof createContext === 'undefined') return undefined as unknown as Context<T>;
+  return createContext(defaultValue as unknown as T);
+}
