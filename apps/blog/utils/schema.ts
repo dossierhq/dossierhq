ed9c3e4323206664a554f0fb7482e8bc66987308
Jonas Bengtsson <jonas.b@gmail.com>
2022-11-03 22:34:57 +0100

    feat(blog): Add GlossaryTerm to schema and add some entities

--- apps/blog/utils/schema.ts
+++ apps/blog/utils/schema.ts
@@ -1,75 +1,43 @@
 import type { AdminSchemaSpecificationUpdate } from '@jonasb/datadata-core';
+import { FieldType, RichTextNodeType } from '@jonasb/datadata-core';
 
 export const schemaSpecification: AdminSchemaSpecificationUpdate = {
   entityTypes: [
     {
-      name: 'BlogPost',
+      name: 'GlossaryTerm',
+      authKeyPattern: 'none',
       fields: [
         {
           name: 'title',
-          type: 'String',
+          type: FieldType.String,
           isName: true,
+          required: true,
         },
         {
-          name: 'summary',
-          type: 'String',
+          name: 'slug',
+          type: FieldType.String,
+          index: 'glossarySlug',
+          matchPattern: 'slug',
+          required: true,
         },
         {
-          name: 'location',
-          type: 'Location',
-        },
-        {
-          name: 'category',
-          type: 'EntityType',
-          entityTypes: ['Category'],
-        },
-        {
-          name: 'categories',
-          type: 'EntityType',
-          list: true,
-          entityTypes: ['Category'],
-        },
-        {
-          name: 'annotatedCategory',
-          type: 'ValueType',
-          valueTypes: ['AnnotatedCategory'],
-        },
-        {
-          name: 'tags',
-          type: 'String',
-          list: true,
-        },
-        {
-          name: 'body',
-          type: 'RichText',
-        },
-      ],
-    },
-    {
-      name: 'Category',
-      fields: [
-        {
-          name: 'title',
-          type: 'String',
-          isName: true,
+          name: 'description',
+          type: FieldType.RichText,
+          required: true,
+          richTextNodes: [
+            RichTextNodeType.root,
+            RichTextNodeType.paragraph,
+            RichTextNodeType.text,
+            RichTextNodeType.entityLink,
+          ],
+          linkEntityTypes: ['GlossaryTerm'],
         },
       ],
     },
   ],
-  valueTypes: [
-    {
-      name: 'AnnotatedCategory',
-      fields: [
-        {
-          name: 'annotation',
-          type: 'String',
-        },
-        {
-          name: 'category',
-          type: 'EntityType',
-          entityTypes: ['Category'],
-        },
-      ],
-    },
+  indexes: [{ name: 'glossarySlug', type: 'unique' }],
+  patterns: [
+    { name: 'none', pattern: '^none$' },
+    { name: 'slug', pattern: '^[a-z0-9-]+$' },
   ],
 };
