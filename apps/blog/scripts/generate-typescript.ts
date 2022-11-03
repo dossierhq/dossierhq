8a72a932e839a6a8dd86794a2c7837b9e57bdf19
Jonas Bengtsson <jonas.b@gmail.com>
2022-11-03 23:24:58 +0100

    chore(blog): Generate typescript types

--- apps/blog/scripts/generate-typescript.ts
+++ apps/blog/scripts/generate-typescript.ts
@@ -1,7 +1,8 @@
-#!/usr/bin/env -S deno run -q --allow-run=npx --allow-write=./src/SchemaTypes.ts
+#!/usr/bin/env -S bun
 import { AdminSchema, type AdminSchemaSpecificationUpdate } from '@jonasb/datadata-core';
 import { generateTypescriptForSchema } from '@jonasb/datadata-typescript-generator';
-import { IntegrationTestSchema } from '../src/IntegrationTestSchema.ts';
+import { writeFile } from 'node:fs/promises';
+import { schemaSpecification } from '../utils/schema.js';
 
 async function generateTypes(schemaSpec: AdminSchemaSpecificationUpdate, filename: string) {
   const adminSchema = new AdminSchema({
@@ -14,10 +15,10 @@ async function generateTypes(schemaSpec: AdminSchemaSpecificationUpdate, filenam
     .valueOrThrow();
 
   const publishedSchema = adminSchema.toPublishedSchema();
-  const sourceCode = generateTypescriptForSchema({ adminSchema, publishedSchema });
-  await Deno.writeTextFile(filename, sourceCode);
+  const sourceCode = generateTypescriptForSchema({ adminSchema: null, publishedSchema });
+  await writeFile(filename, sourceCode);
 
-  await Deno.run({ cmd: ['npx', 'prettier', '-w', filename] }).status();
+  await Bun.spawn({ cmd: ['npx', 'prettier', '-w', filename] }).exited;
 }
 
-await generateTypes(IntegrationTestSchema, './src/SchemaTypes.ts');
+await generateTypes(schemaSpecification, './utils/SchemaTypes.ts');
---
+++ apps/blog/utils/SchemaTypes.ts
@@ -0,0 +1,25 @@
+import type { PublishedEntity, RichText } from '@jonasb/datadata-core';
+
+export interface PublishedGlossaryTermFields {
+  title: string | null;
+  slug: string | null;
+  description: RichText | null;
+}
+
+export type PublishedGlossaryTerm = PublishedEntity<'GlossaryTerm', PublishedGlossaryTermFields>;
+
+export function isPublishedGlossaryTerm(
+  entity: PublishedEntity | PublishedGlossaryTerm
+): entity is PublishedGlossaryTerm {
+  return entity.info.type === 'GlossaryTerm';
+}
+
+export function assertIsPublishedGlossaryTerm(
+  entity: PublishedEntity | PublishedGlossaryTerm
+): asserts entity is PublishedGlossaryTerm {
+  if (entity.info.type !== 'GlossaryTerm') {
+    throw new Error('Expected info.type = GlossaryTerm (but was ' + entity.info.type + ')');
+  }
+}
+
+export type AllPublishedValueItems = never;
