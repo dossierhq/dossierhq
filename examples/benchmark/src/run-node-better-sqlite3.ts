f9f8861250bb4dba5be734b1a2417968e231c477
Jonas Bengtsson <jonas.b@gmail.com>
2022-11-28 20:35:57 +0100

    feat(benchmark): Add better-sqlite3

---
+++ examples/benchmark/src/run-node-better-sqlite3.ts
@@ -0,0 +1,39 @@
+import { NoOpLogger } from '@jonasb/datadata-core';
+import { createBetterSqlite3Adapter } from '@jonasb/datadata-database-adapter-better-sqlite3';
+import * as fs from 'node:fs/promises';
+import Database from 'better-sqlite3';
+import { initializeAndRunTests } from './benchmark.js';
+
+async function createSqliteDatabaseAdapter(databasePath: string) {
+  try {
+    // delete database to have consistent results
+    await fs.unlink(databasePath);
+  } catch (error) {
+    // ignore
+  }
+
+  const context = { logger: NoOpLogger };
+  const database = new Database(databasePath);
+  database.pragma('journal_mode = WAL');
+  return await createBetterSqlite3Adapter(context, database);
+}
+
+async function main(runName: string, ciOrLocal: { githubSha: string | undefined } | 'local') {
+  const adapter = await createSqliteDatabaseAdapter('output/db-better-sqlite3.sqlite');
+  const result = await initializeAndRunTests({
+    runName,
+    variant: 'node-better-sqlite3',
+    databaseAdapter: adapter.valueOrThrow(),
+    ciOrLocal,
+  });
+  result.throwIfError();
+}
+
+const runNameOrCiSwitch = process.argv[2] || '';
+main(
+  runNameOrCiSwitch,
+  runNameOrCiSwitch === 'ci' ? { githubSha: process.env.GITHUB_SHA } : 'local'
+).catch((error) => {
+  console.warn(error);
+  process.exit(1);
+});
