b8f73d0afae58919b4515c70c8144ae57349ab26
Jonas Bengtsson <jonas.b@gmail.com>
2022-11-29 13:28:37 +0100

    chore(play): Hide use client warnings

--- apps/playground/vite.config.ts
+++ apps/playground/vite.config.ts
@@ -9,6 +9,12 @@ export default defineConfig({
     chunkSizeWarningLimit: 1500,
     rollupOptions: {
       plugins: [visualizer()],
+      onwarn(warning, warn) {
+        if (warning.code === 'MODULE_LEVEL_DIRECTIVE' && warning.message.includes("'use client'")) {
+          return;
+        }
+        warn(warning);
+      },
     },
   },
   css: {
