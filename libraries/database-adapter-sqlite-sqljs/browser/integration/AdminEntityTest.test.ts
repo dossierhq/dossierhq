e110b299b2a77581707ed14e0c66e3b0e13c9b5a
Jonas Bengtsson <jonas.b@gmail.com>
2022-11-30 23:50:56 +0100

    chore(sqljs): Include AdminEntityTest and run playwright on build

---
+++ libraries/database-adapter-sqlite-sqljs/browser/integration/AdminEntityTest.test.ts
@@ -0,0 +1,39 @@
+import { assertIsDefined } from '@jonasb/datadata-core';
+import type { ReadOnlyEntityRepository } from '@jonasb/datadata-database-adapter-test-integration';
+import {
+  createAdminEntityTestSuite,
+  createReadOnlyEntityRepository,
+} from '@jonasb/datadata-database-adapter-test-integration';
+import test from '@playwright/test';
+import { registerTestSuite } from '../TestUtils.js';
+import type { ServerInit } from './SqlJsTestUtils.js';
+import { initializeSqlJsServer } from './SqlJsTestUtils.js';
+
+let serverInit: ServerInit | null = null;
+let readOnlyEntityRepository: ReadOnlyEntityRepository;
+
+test.beforeAll(async () => {
+  serverInit = (await initializeSqlJsServer()).valueOrThrow();
+  readOnlyEntityRepository = (
+    await createReadOnlyEntityRepository(serverInit.server)
+  ).valueOrThrow();
+});
+test.afterAll(async () => {
+  if (serverInit) {
+    (await serverInit.server.shutdown()).throwIfError();
+    serverInit = null;
+  }
+});
+
+registerTestSuite(
+  createAdminEntityTestSuite({
+    before: async () => {
+      assertIsDefined(serverInit);
+      const { adminSchema, server } = serverInit;
+      return [{ adminSchema, server, readOnlyEntityRepository }, undefined];
+    },
+    after: async () => {
+      //empty
+    },
+  })
+);
