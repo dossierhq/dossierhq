f9025c2756ce2677947a96f1f7561cf6a88f5ca0
Jonas Bengtsson <jonas.b@gmail.com>
2022-12-04 18:55:26 +0000

    chore(ci): Create blog db automatically

---
+++ apps/blog/scripts/create-database-if-missing.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+if [ -f .env ] && grep -q "DATABASE_SQLITE_FILE" .env; then
+  echo "A database is already configuerd"
+  exit 0
+fi
+
+echo "DATABASE_SQLITE_FILE=data/database.sqlite" >> .env
+./scripts/create-database-from-disk.ts data/database.sqlite
