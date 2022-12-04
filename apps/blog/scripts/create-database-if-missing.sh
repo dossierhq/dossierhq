da196745a3bf375987f055cee583cb3edf39898e
Jonas Bengtsson <jonas.b@gmail.com>
2022-12-04 19:17:45 +0000

    chore(ci): Fix build warning

--- apps/blog/scripts/create-database-if-missing.sh
+++ apps/blog/scripts/create-database-if-missing.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 if [ -f .env ] && grep -q "DATABASE_SQLITE_FILE" .env; then
-  echo "A database is already configuerd"
+  echo "A database is already configured"
   exit 0
 fi
 
 echo "DATABASE_SQLITE_FILE=data/database.sqlite" >> .env
-./scripts/create-database-from-disk.ts data/database.sqlite
+./scripts/create-database-from-disk.ts data/database.sqlite 2>&1
--- libraries/typescript-generator/vitest.config.js
+++ libraries/typescript-generator/vitest.config.js
@@ -1,6 +1,7 @@
 export default {
   test: {
     include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+    teardownTimeout: 3000,
     watch: false,
   },
 };
