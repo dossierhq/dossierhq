787683ca22673f8a19c2bc010413611b1cfa3083
Jonas Bengtsson <jonas.b@gmail.com>
2022-12-06 22:23:31 +0100

    feat(blog): Update content

--- apps/blog/app/page.tsx
+++ apps/blog/app/page.tsx
@@ -20,24 +20,18 @@ export default function Page() {
       <FullscreenContainer.Row fullWidth>
         <NavBar current="home" />
       </FullscreenContainer.Row>
-      <FullscreenContainer.Row paddingTop={3} paddingHorizontal={4}>
-        <Image
-          className={toSpacingClassName({ paddingBottom: 5 })}
-          src={logo}
-          alt="Data data logo"
-          height={80}
-        />
-        <Text as="h1" className={toSpacingClassName({ paddingBottom: 2 })} textStyle="headline2">
-          Stay in control of your content
-        </Text>
-        <Text className={toSpacingClassName({ paddingBottom: 5 })} textStyle="headline4">
-          Data data is an embeddable content platform. Open source.
-        </Text>
+      <FullscreenContainer.Row paddingTop={3} paddingHorizontal={4} gap={5}>
+        <Image src={logo} alt="Data data logo" height={80} />
+        <div>
+          <Text as="h1" className={toSpacingClassName({ paddingBottom: 2 })} textStyle="headline2">
+            Stay in control of your content
+          </Text>
+          <Text textStyle="headline4">
+            Data data is an embeddable content platform. Open source.
+          </Text>
+        </div>
         <LinkButton
-          className={toClassName(
-            toFlexItemClassName({ alignSelf: 'flex-start' }),
-            toSpacingClassName({ marginBottom: 5 })
-          )}
+          className={toClassName(toFlexItemClassName({ alignSelf: 'flex-start' }))}
           href={BrowserUrls.docs}
           color="primary"
         >
@@ -53,7 +47,7 @@ export default function Page() {
         />
         <Feature
           title="Bring your own auth 🔑"
-          description="Data data doesn’t come with its own auth, instead it allows you to integrate with any existing solution you already have. Auth0 examples are provided."
+          description="Data data doesn’t come with its own auth, instead it allows you to integrate with any existing solution you already have."
         />
         <Feature
           title="Secure all content 🤫"
@@ -61,20 +55,15 @@ export default function Page() {
         />
         <Feature
           title="Developer friendly ⌨️"
-          description="Generated TypeScript and GraphQL out of the box. Create local test environments in under a second. Everything is open source so you can see how everything is built and contribute improvements."
+          description="Generated TypeScript types and GraphQL schema out of the box. Create local test environments in under a second. Everything is open source so you can see how everything is built and contribute improvements."
         />
-        <Card2
-          className={toClassName(
-            toSpacingClassName({ marginTop: 5, marginBottom: 5 }),
-            toFlexItemClassName({ alignSelf: 'flex-start' })
-          )}
-        >
+        <Card2 className={toClassName(toFlexItemClassName({ alignSelf: 'flex-start' }))}>
           <Card2.Header>
             <Card2.HeaderTitle>Now what? 🗺️</Card2.HeaderTitle>
           </Card2.Header>
           <Card2.Content>
             <Text className={toSpacingClassName({ marginBottom: 3 })} textStyle="body1">
-              Read the docs to learn more or jump straight in and try it in the Playground.
+              Read the docs to learn more or jump straight in and try it out in the Playground.
             </Text>
             <Button.Group>
               <LinkButton href={BrowserUrls.docs} color="primary">
@@ -93,14 +82,16 @@ export default function Page() {
         <Message color="warning">
           <Message.Body>
             Data data in <strong>active development</strong> so everything is not yet supported. Get
-            in touch if it doesn’t support what you want to do.
+            in touch if you’re missing something.
           </Message.Body>
         </Message>
-        <Text textStyle="headline4">About</Text>
-        <Text textStyle="body1">
-          Developed with ❤️ by Jonas Bengtsson. Send an{' '}
-          <a href="mailto:jonas.b@gmail.com">✉️ email</a> if you have any questions!
-        </Text>
+        <div>
+          <Text textStyle="headline4">About</Text>
+          <Text textStyle="body1">
+            Developed with ❤️ by Jonas Bengtsson. Send an{' '}
+            <a href="mailto:jonas.b@gmail.com">✉️ email</a> if you have any questions!
+          </Text>
+        </div>
       </FullscreenContainer.Row>
     </FullscreenContainer>
   );
@@ -108,7 +99,7 @@ export default function Page() {
 
 function Feature({ title, description }: { title: string; description: string }) {
   return (
-    <div className={toSpacingClassName({ marginBottom: 4 })}>
+    <div>
       <Text as="h2" textStyle="headline4">
         {title}
       </Text>
