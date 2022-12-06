6fdbe9601256475821ea6a20e495a4171bc427de
Jonas Bengtsson <jonas.b@gmail.com>
2022-12-06 15:02:23 +0100

    fix(ci): Remove deno dependency for playground-example-generator for Vercel

--- apps/playground-example-generator/scripts/generate-typescript.ts
+++ apps/playground-example-generator/scripts/generate-typescript.ts
@@ -1,17 +1,19 @@
-#!/usr/bin/env -S deno run -q --allow-run=npx --allow-write=./src/blog/schema-types.ts,./src/reviews/schema-types.ts,./src/starwars/schema-types.ts
+#!/usr/bin/env -S npx ts-node-esm
 import { AdminSchema, type AdminSchemaSpecificationUpdate } from '@jonasb/datadata-core';
 import { generateTypescriptForSchema } from '@jonasb/datadata-typescript-generator';
-import { SCHEMA as BLOG_SCHEMA } from '../src/blog/schema.ts';
-import { SCHEMA as REVIEWS_SCHEMA } from '../src/reviews/schema.ts';
-import { SCHEMA as STARWARS_SCHEMA } from '../src/starwars/schema.ts';
+import { execFileSync } from 'node:child_process';
+import { writeFile } from 'node:fs/promises';
+import { SCHEMA as BLOG_SCHEMA } from '../src/blog/schema.js';
+import { SCHEMA as REVIEWS_SCHEMA } from '../src/reviews/schema.js';
+import { SCHEMA as STARWARS_SCHEMA } from '../src/starwars/schema.js';
 
 async function generateTypes(schemaSpec: AdminSchemaSpecificationUpdate, filename: string) {
   const adminSchema = AdminSchema.createAndValidate(schemaSpec).valueOrThrow();
 
   const sourceCode = generateTypescriptForSchema({ adminSchema, publishedSchema: null });
-  await Deno.writeTextFile(filename, sourceCode);
+  await writeFile(filename, sourceCode, { encoding: 'utf8' });
 
-  await Deno.run({ cmd: ['npx', 'prettier', '-w', filename] }).status();
+  execFileSync('npx', ['prettier', '-w', filename]);
 }
 
 await generateTypes(BLOG_SCHEMA, './src/blog/schema-types.ts');
