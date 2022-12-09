d9f13d0aa7e700c5b74d8e6ee3bb203e6821c90c
Jonas Bengtsson <jonas.b@gmail.com>
2022-12-09 21:52:42 +0100

    feat(blog): Add html lang and viewport

--- apps/blog/app/layout.tsx
+++ apps/blog/app/layout.tsx
@@ -6,8 +6,9 @@ interface Props {
 
 export default function Layout({ children }: Props) {
   return (
-    <html>
+    <html lang="en-US">
       <head>
+        <meta name="viewport" content="width=device-width" />
         <title>Data data</title>
       </head>
       <body>{children}</body>
