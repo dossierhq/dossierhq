58ee55d1b293b0ca9392c42ce7abe20839de5425
Jonas Bengtsson <jonas.b@gmail.com>
2022-12-13 16:02:41 +0100

    feat(started): Start/shutdown server

--- examples/getting-started/server.ts
+++ examples/getting-started/server.ts
@@ -1,42 +1,81 @@
-import { createConsoleLogger, Logger, notOk, ok } from '@jonasb/datadata-core';
-import { createBetterSqlite3Adapter } from '@jonasb/datadata-database-adapter-better-sqlite3';
+import { createConsoleLogger, notOk, ok } from '@jonasb/datadata-core';
+import {
+  BetterSqlite3DatabaseAdapter,
+  createBetterSqlite3Adapter,
+} from '@jonasb/datadata-database-adapter-better-sqlite3';
+import { createServer, NoneAndSubjectAuthorizationAdapter } from '@jonasb/datadata-server';
 import BetterSqlite, { type Database } from 'better-sqlite3';
 import express from 'express';
 
 const app = express();
 const port = 3000;
 
-async function initializeDatabase(logger: Logger) {
-  const context = { logger };
+async function initializeDatabase() {
   let database: Database;
   try {
     database = new BetterSqlite('database.sqlite');
   } catch (error) {
-    return notOk.GenericUnexpectedException(context, error);
+    return notOk.GenericUnexpectedException({ logger }, error);
   }
 
-  return await createBetterSqlite3Adapter(context, database, {
+  return await createBetterSqlite3Adapter({ logger }, database, {
     migrate: true,
     fts: { version: 'fts5' },
     journalMode: 'wal',
   });
 }
 
-async function initialize() {
-  const logger = createConsoleLogger(console);
-
-  const databaseResult = await initializeDatabase(logger);
-  if (databaseResult.isError()) return databaseResult;
-
-  return ok(undefined);
+async function initializeServer(databaseAdapter: BetterSqlite3DatabaseAdapter) {
+  return await createServer({
+    databaseAdapter,
+    logger,
+    authorizationAdapter: NoneAndSubjectAuthorizationAdapter,
+  });
 }
 
-(await initialize()).throwIfError();
+async function initialize() {
+  const databaseResult = await initializeDatabase();
+  if (databaseResult.isError()) return databaseResult;
+
+  const serverResult = await initializeServer(databaseResult.value);
+  if (serverResult.isError()) return serverResult;
+  const server = serverResult.value;
+
+  return ok(server);
+}
+
+const logger = createConsoleLogger(console);
+const server = (await initialize()).valueOrThrow();
 
 app.get('/api/hello-world', (req, res) => {
   res.send({ message: 'Hello World!' });
 });
 
-app.listen(port, () => {
+const httpServer = app.listen(port, () => {
   console.log(`Listening on http://localhost:${port}`);
 });
+
+process.on('SIGINT', shutdown);
+process.on('SIGTERM', shutdown);
+
+async function shutdown(signal: NodeJS.Signals) {
+  logger.info('Received signal %s, shutting down', signal);
+  httpServer.closeAllConnections();
+
+  const shutdownResult = await server.shutdown();
+  if (shutdownResult.isError()) {
+    logger.error(
+      'Error while shutting down: %s (%s)',
+      shutdownResult.error,
+      shutdownResult.message
+    );
+  }
+
+  httpServer.close((error) => {
+    if (error) {
+      logger.error('Error while shutting down: %s', error.message);
+    }
+    logger.info('Server shut down');
+    process.exit(error ? 1 : 0);
+  });
+}
