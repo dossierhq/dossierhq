ec88cf9f3fbd13859d806fe04c5bc5e93bd4b606
Jonas Bengtsson <jonas.b@gmail.com>
2022-12-13 10:56:04 +0100

    feat(started): Add Getting started project

---
+++ examples/getting-started/src/App.tsx
@@ -0,0 +1,19 @@
+import { useEffect, useState } from 'react';
+import './App.css';
+
+export default function App() {
+  const [message, setMessage] = useState<string | null>(null);
+
+  useEffect(() => {
+    fetch('/api/hello-world')
+      .then((res) => res.json())
+      .then((data) => setMessage(data.message))
+  }, []);
+
+  return (
+    <div className="App">
+      <h1>Data data</h1>
+      {message && <div className="card">Message: {message}</div>}
+    </div>
+  );
+}
