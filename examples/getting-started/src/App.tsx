20d1bba7205712dc292c332640c822afdbd46b7f
Jonas Bengtsson <jonas.b@gmail.com>
2022-12-20 12:32:15 +0100

    feat(started): Add admin REST API

--- examples/getting-started/src/App.tsx
+++ examples/getting-started/src/App.tsx
@@ -1,8 +1,16 @@
+import { EntitySamplingPayload } from '@jonasb/datadata-core';
 import { useEffect, useState } from 'react';
 import './App.css';
+import { createAdminClient } from './ClientUtils.js';
+import { AllAdminEntities } from './SchemaTypes.js';
+
+const adminClient = createAdminClient();
 
 export default function App() {
   const [message, setMessage] = useState<string | null>(null);
+  const [adminSample, setAdminSample] = useState<EntitySamplingPayload<AllAdminEntities> | null>(
+    null
+  );
 
   useEffect(() => {
     fetch('/api/message')
@@ -14,10 +22,27 @@ export default function App() {
       .then((data) => setMessage(data.message));
   }, []);
 
+  useEffect(() => {
+    adminClient.sampleEntities().then((result) => {
+      if (result.isOk()) setAdminSample(result.value);
+    });
+  }, []);
+
   return (
     <div className="App">
       <h1>Data data</h1>
       {message && <div className="card">Got: {message}</div>}
+      <h2>Sample admin entities</h2>
+
+      {adminSample && (
+        <ul>
+          {adminSample.items.map((it) => (
+            <li key={it.id}>
+              {it.info.type}: {it.info.name}
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 }
---
+++ examples/getting-started/src/ClientUtils.ts
@@ -0,0 +1,63 @@
+import {
+  AdminClientOperation,
+  buildUrlWithUrlQuery,
+  ClientContext,
+  convertJsonAdminClientResult,
+  createBaseAdminClient,
+  createConsoleLogger,
+  notOk,
+  ok,
+  stringifyUrlQueryParams,
+} from '@jonasb/datadata-core';
+import { AppAdminClient } from './SchemaTypes.js';
+
+const logger = createConsoleLogger(console);
+
+export function createAdminClient() {
+  return createBaseAdminClient<ClientContext, AppAdminClient>({
+    context: { logger },
+    pipeline: [adminBackendMiddleware],
+  });
+}
+
+async function adminBackendMiddleware(
+  context: ClientContext,
+  operation: AdminClientOperation
+): Promise<void> {
+  let response: Response;
+  if (operation.modifies) {
+    response = await fetch(`/api/admin/${operation.name}`, {
+      method: 'PUT',
+      headers: { 'content-type': 'application/json' },
+      body: JSON.stringify(operation.args),
+    });
+  } else {
+    response = await fetch(
+      buildUrlWithUrlQuery(
+        `/api/admin/${operation.name}`,
+        stringifyUrlQueryParams({ args: operation.args }, { keepEmptyObjects: true })
+      )
+    );
+  }
+
+  const result = await getBodyAsJsonResult(response);
+  operation.resolve(convertJsonAdminClientResult(operation.name, result));
+}
+
+async function getBodyAsJsonResult(response: Response) {
+  if (response.ok) {
+    try {
+      return ok(await response.json());
+    } catch (error) {
+      return notOk.Generic('Failed parsing response');
+    }
+  } else {
+    let text = 'Failed fetching response';
+    try {
+      text = await response.text();
+    } catch (error) {
+      // ignore
+    }
+    return notOk.fromHttpStatus(response.status, text);
+  }
+}
