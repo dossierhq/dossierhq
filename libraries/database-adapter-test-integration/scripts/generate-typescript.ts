6256865b471eeb1e911a8210371e1c8b94175cc8
Jonas Bengtsson <jonas.b@gmail.com>
2022-12-21 00:16:23 +0100

    chore(integ): Use ts-node instead of deno and run ts-gen even on Vercel

--- libraries/database-adapter-test-integration/scripts/generate-typescript.ts
+++ libraries/database-adapter-test-integration/scripts/generate-typescript.ts
@@ -1,15 +1,17 @@
-#!/usr/bin/env -S deno run -q --allow-run=npx --allow-write=./src/SchemaTypes.ts
+#!/usr/bin/env -S npx ts-node-esm
 import { AdminSchema, type AdminSchemaSpecificationUpdate } from '@jonasb/datadata-core';
 import { generateTypescriptForSchema } from '@jonasb/datadata-typescript-generator';
-import { IntegrationTestSchema } from '../src/IntegrationTestSchema.ts';
+import { execFileSync } from 'node:child_process';
+import { writeFile } from 'node:fs/promises';
+import { IntegrationTestSchema } from '../src/IntegrationTestSchema.js';
 
 async function generateTypes(schemaSpec: AdminSchemaSpecificationUpdate, filename: string) {
   const adminSchema = AdminSchema.createAndValidate(schemaSpec).valueOrThrow();
   const publishedSchema = adminSchema.toPublishedSchema();
   const sourceCode = generateTypescriptForSchema({ adminSchema, publishedSchema });
-  await Deno.writeTextFile(filename, sourceCode);
 
-  await Deno.run({ cmd: ['npx', 'prettier', '-w', filename] }).status();
+  await writeFile(filename, sourceCode, { encoding: 'utf8' });
+  execFileSync('npx', ['prettier', '-w', filename]);
 }
 
 await generateTypes(IntegrationTestSchema, './src/SchemaTypes.ts');
