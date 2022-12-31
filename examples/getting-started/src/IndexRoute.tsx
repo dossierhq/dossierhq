fbd2ec57b58f5ed5b706554d6a1b616ca13f910f
Jonas Bengtsson <jonas.b@gmail.com>
2022-12-31 14:37:25 +0100

    feat(tut): Update layout on index route

--- examples/getting-started/src/IndexRoute.tsx
+++ examples/getting-started/src/IndexRoute.tsx
@@ -1,6 +1,7 @@
 import { EntitySamplingPayload } from '@jonasb/datadata-core';
 import { useCallback, useEffect, useState } from 'react';
 import { createAdminClient } from './ClientUtils.js';
+import { Navbar } from './Navbar.js';
 import { AllAdminEntities } from './SchemaTypes.js';
 
 const adminClient = createAdminClient();
@@ -45,30 +46,33 @@ export function IndexRoute() {
   }, [adminSampleSeed]);
 
   return (
-    <div className="App">
-      <h1>Data data</h1>
-      {message && <div className="card">Got: {message}</div>}
+    <>
+      <Navbar current="home" />
+      <div style={{ maxWidth: '30rem', marginRight: 'auto', marginLeft: 'auto' }}>
+        <h1 style={{ fontSize: '2em' }}>Data data</h1>
+        {message && <div>Got: {message}</div>}
 
-      <h2>Create message entity</h2>
-      <div className="card">
-        <input onChange={(e) => setNewMessage(e.target.value)} value={newMessage} />
-        <br />
-        <button disabled={!newMessage} onClick={handleSendMessageClick}>
-          Create
-        </button>
+        <h2 style={{ fontSize: '1.75em' }}>Create message entity</h2>
+        <div>
+          <input onChange={(e) => setNewMessage(e.target.value)} value={newMessage} />
+          <br />
+          <button disabled={!newMessage} onClick={handleSendMessageClick}>
+            Create
+          </button>
+        </div>
+
+        <h2 style={{ fontSize: '1.75em' }}>Sample admin entities</h2>
+        {adminSample && (
+          <ul>
+            {adminSample.items.map((it) => (
+              <li key={it.id}>
+                {it.info.type}: {it.info.name}
+              </li>
+            ))}
+          </ul>
+        )}
+        <button onClick={() => setAdminSampleSeed(Math.random())}>Refresh</button>
       </div>
-
-      <h2>Sample admin entities</h2>
-      {adminSample && (
-        <ul>
-          {adminSample.items.map((it) => (
-            <li key={it.id}>
-              {it.info.type}: {it.info.name}
-            </li>
-          ))}
-        </ul>
-      )}
-      <button onClick={() => setAdminSampleSeed(Math.random())}>Refresh</button>
-    </div>
+    </>
   );
 }
