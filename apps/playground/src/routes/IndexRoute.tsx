415fda933b3fe41ac2516b1d58423f0513e949ec
Jonas Bengtsson <jonas.b@gmail.com>
2023-01-03 16:49:48 +0100

    fix(play): Make navbar fixed at top on start page

--- apps/playground/src/routes/IndexRoute.tsx
+++ apps/playground/src/routes/IndexRoute.tsx
@@ -9,23 +9,25 @@ export function IndexRoute() {
       <FullscreenContainer.Row fullWidth>
         <NavBar current="home" />
       </FullscreenContainer.Row>
-      <FullscreenContainer.Row paddingBottom={5}>
-        <Text as="h1" textStyle="headline4">
-          Welcome to datadata Playground!
-        </Text>
-        <Text textStyle="body1">This is a playground where you can explore datadata.</Text>
-        <Message className={toSpacingClassName({ marginTop: 5 })} color="danger">
-          <Message.Body>
-            <p>
-              The database in the Playground is only stored in your browser. If you close or refresh
-              the browser tab all changes will be lost.
-            </p>
-            <p>Make sure to download a copy of the database if you want to keep it.</p>
-          </Message.Body>
-        </Message>
-        <DatabaseInfoMessage className={toSpacingClassName({ marginTop: 3 })} />
-        <ChangeDatabaseMessage className={toSpacingClassName({ marginTop: 3 })} />
-      </FullscreenContainer.Row>
+      <FullscreenContainer.ScrollableRow>
+        <FullscreenContainer.Row paddingVertical={5} paddingHorizontal={2}>
+          <Text as="h1" textStyle="headline4">
+            Welcome to datadata Playground!
+          </Text>
+          <Text textStyle="body1">This is a playground where you can explore datadata.</Text>
+          <Message className={toSpacingClassName({ marginTop: 5 })} color="danger">
+            <Message.Body>
+              <p>
+                The database in the Playground is only stored in your browser. If you close or
+                refresh the browser tab all changes will be lost.
+              </p>
+              <p>Make sure to download a copy of the database if you want to keep it.</p>
+            </Message.Body>
+          </Message>
+          <DatabaseInfoMessage className={toSpacingClassName({ marginTop: 3 })} />
+          <ChangeDatabaseMessage className={toSpacingClassName({ marginTop: 3 })} />
+        </FullscreenContainer.Row>
+      </FullscreenContainer.ScrollableRow>
     </FullscreenContainer>
   );
 }
