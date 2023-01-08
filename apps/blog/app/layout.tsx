66af483b9b600d50ba7d5e3c115bd7cc249f47fa
Jonas Bengtsson <jonas.b@gmail.com>
2023-01-08 15:35:38 +0100

    chore(blog): Add title for landing page

--- apps/blog/app/layout.tsx
+++ apps/blog/app/layout.tsx
@@ -9,7 +9,7 @@ export default function Layout({ children }: Props) {
     <html lang="en-US">
       <head>
         <meta name="viewport" content="width=device-width" />
-        <title>Data data</title>
+        <title>Data data – an open source, embeddable content platform</title>
       </head>
       <body>{children}</body>
     </html>
