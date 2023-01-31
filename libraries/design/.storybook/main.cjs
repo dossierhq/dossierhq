fb45bac30d0f1046134193a4241342f5f813ff29
Jonas Bengtsson <jonas.b@gmail.com>
2023-01-31 12:01:44 +0100

    chore(design/react): Switch storybook from webpack5 to vite, change react to ESM

--- libraries/design/.storybook/main.cjs
+++ libraries/design/.storybook/main.cjs
@@ -4,42 +4,11 @@ module.exports = {
   reactOptions: {
     strictMode: true,
   },
-  typescript: {
-    // TODO: Disable docgen due to plenty of deprecation warnings when starting Storybook
-    reactDocgen: false,
-  },
-  webpackFinal: (config) => {
-    config.resolve.plugins = [...(config.resolve.plugins || []), new ResolveJsToTsPlugin()];
-    return config;
-  },
   framework: {
-    name: '@storybook/react-webpack5',
-    options: {
-      strictMode: true,
-    },
+    name: '@storybook/react-vite',
+    options: {},
   },
   docs: {
     autodocs: true,
   },
 };
-
-class ResolveJsToTsPlugin {
-  source = 'resolve';
-  target = 'resolve';
-
-  apply(resolver) {
-    const target = resolver.ensureHook(this.target);
-    resolver
-      .getHook(this.source)
-      .tapAsync('ResolveJsToTsPlugin', function (request, resolveContext, callback) {
-        const inTypeScriptFile = request.context.issuer?.match(/\.tsx?$/);
-        if (request.request.endsWith('.js') && inTypeScriptFile) {
-          // Could refer to .ts, .tsx
-          const requestWithoutFileEnding = request.request.slice(0, -'.js'.length);
-          const modifiedRequest = { ...request, request: requestWithoutFileEnding };
-          return resolver.doResolve(target, modifiedRequest, null, resolveContext, callback);
-        }
-        callback();
-      });
-  }
-}
