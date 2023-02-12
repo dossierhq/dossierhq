ff4496997b23ef2093f4246063b2aee410aaedc4
Jonas Bengtsson <jonas.b@gmail.com>
2023-02-12 18:39:15 +0100

    fix(react): Show validation errors for richtext editor

--- libraries/react-components/src/components/EntityEditor/RichTextFieldEditor.tsx
+++ libraries/react-components/src/components/EntityEditor/RichTextFieldEditor.tsx
@@ -1,9 +1,19 @@
 import type { RichText, RichTextFieldSpecification } from '@dossierhq/core';
+import { Text } from '@dossierhq/design';
 import { RichTextEditor } from '../RichTextEditor/RichTextEditor.js';
 import type { FieldEditorProps } from './FieldEditor.js';
 
 type Props = FieldEditorProps<RichTextFieldSpecification, RichText>;
 
-export function RichTextFieldEditor({ fieldSpec, value, onChange }: Props) {
-  return <RichTextEditor fieldSpec={fieldSpec} value={value} onChange={onChange} />;
+export function RichTextFieldEditor({ fieldSpec, value, validationErrors, onChange }: Props) {
+  return (
+    <>
+      <RichTextEditor fieldSpec={fieldSpec} value={value} onChange={onChange} />{' '}
+      {validationErrors.map((error, index) => (
+        <Text key={index} textStyle="body2" marginTop={1} color="danger">
+          {error.message}
+        </Text>
+      ))}
+    </>
+  );
 }
