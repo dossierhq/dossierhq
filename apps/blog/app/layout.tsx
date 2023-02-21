1c7e6ed64858fb58d4a0554338b46efa4fdd9988
Jonas Bengtsson <jonas.b@gmail.com>
2023-02-21 18:14:53 +0100

    fix(blog): Fix broken build script, include feed discovery in layout

--- apps/blog/app/layout.tsx
+++ apps/blog/app/layout.tsx
@@ -10,6 +10,12 @@ export default function Layout({ children }: Props) {
       <head>
         <meta name="viewport" content="width=device-width" />
         <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <link
+          href="https://www.dossierhq.dev/atom.xml"
+          rel="alternate"
+          title="Dossier blog"
+          type="application/atom+xml"
+        />
         <title>Dossier – the open source headless CMS toolkit</title>
       </head>
       <body>{children}</body>
