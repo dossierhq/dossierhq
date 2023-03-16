360b7ff2905c1761e2a4671870ddb22f665d2604
Jonas Bengtsson <jonas.b@gmail.com>
2023-03-16 22:29:55 +0100

    chore(better): Create schema.sql for documentation

---
+++ libraries/better-sqlite3/docs/schema.sql
@@ -0,0 +1,135 @@
+CREATE TABLE subjects (
+    id INTEGER PRIMARY KEY,
+    uuid TEXT NOT NULL,
+    created_at TEXT NOT NULL,
+    CONSTRAINT subjects_uuid UNIQUE (uuid)
+  ) STRICT;
+CREATE TABLE principals (
+    id INTEGER PRIMARY KEY,
+    provider TEXT NOT NULL,
+    identifier TEXT NOT NULL,
+    subjects_id INTEGER NOT NULL,
+    CONSTRAINT principals_pkey UNIQUE (provider, identifier),
+    FOREIGN KEY (subjects_id) REFERENCES subjects(id) ON DELETE CASCADE
+  ) STRICT;
+CREATE TABLE schema_versions (
+    id INTEGER PRIMARY KEY,
+    specification TEXT NOT NULL
+  ) STRICT;
+CREATE TABLE sequences (
+    name TEXT NOT NULL UNIQUE,
+    value INTEGER DEFAULT 0
+  ) STRICT;
+CREATE TABLE entities (
+    id INTEGER PRIMARY KEY,
+    uuid TEXT NOT NULL,
+    name TEXT NOT NULL,
+    type TEXT NOT NULL,
+    auth_key TEXT NOT NULL,
+    resolved_auth_key TEXT NOT NULL,
+    status TEXT NOT NULL,
+    never_published INTEGER NOT NULL DEFAULT TRUE,
+    created_at TEXT NOT NULL,
+    updated_at TEXT NOT NULL,
+    updated_seq INTEGER NOT NULL,
+    latest_entity_versions_id INTEGER,
+    published_entity_versions_id INTEGER,
+    CONSTRAINT entities_uuid UNIQUE (uuid),
+    CONSTRAINT entities_name UNIQUE (name),
+    CONSTRAINT entities_updated_seq UNIQUE (updated_seq),
+    FOREIGN KEY (latest_entity_versions_id) REFERENCES entity_versions(id),
+    FOREIGN KEY (published_entity_versions_id) REFERENCES entity_versions(id)
+  ) STRICT;
+CREATE VIRTUAL TABLE entities_latest_fts USING fts5 (
+    content
+  )
+/* entities_latest_fts(content) */;
+CREATE TABLE IF NOT EXISTS 'entities_latest_fts_data'(id INTEGER PRIMARY KEY, block BLOB);
+CREATE TABLE IF NOT EXISTS 'entities_latest_fts_idx'(segid, term, pgno, PRIMARY KEY(segid, term)) WITHOUT ROWID;
+CREATE TABLE IF NOT EXISTS 'entities_latest_fts_content'(id INTEGER PRIMARY KEY, c0);
+CREATE TABLE IF NOT EXISTS 'entities_latest_fts_docsize'(id INTEGER PRIMARY KEY, sz BLOB);
+CREATE TABLE IF NOT EXISTS 'entities_latest_fts_config'(k PRIMARY KEY, v) WITHOUT ROWID;
+CREATE VIRTUAL TABLE entities_published_fts USING fts5 (
+    content
+  )
+/* entities_published_fts(content) */;
+CREATE TABLE IF NOT EXISTS 'entities_published_fts_data'(id INTEGER PRIMARY KEY, block BLOB);
+CREATE TABLE IF NOT EXISTS 'entities_published_fts_idx'(segid, term, pgno, PRIMARY KEY(segid, term)) WITHOUT ROWID;
+CREATE TABLE IF NOT EXISTS 'entities_published_fts_content'(id INTEGER PRIMARY KEY, c0);
+CREATE TABLE IF NOT EXISTS 'entities_published_fts_docsize'(id INTEGER PRIMARY KEY, sz BLOB);
+CREATE TABLE IF NOT EXISTS 'entities_published_fts_config'(k PRIMARY KEY, v) WITHOUT ROWID;
+CREATE TABLE entity_versions (
+    id INTEGER PRIMARY KEY,
+    entities_id INTEGER NOT NULL,
+    version INTEGER NOT NULL,
+    created_at TEXT NOT NULL,
+    created_by INTEGER NOT NULL,
+    fields TEXT NOT NULL,
+    FOREIGN KEY (entities_id) REFERENCES entities(id) ON DELETE CASCADE,
+    FOREIGN KEY (created_by) REFERENCES subjects(id)
+  ) STRICT;
+CREATE TABLE entity_publishing_events (
+    id INTEGER PRIMARY KEY,
+    entities_id INTEGER NOT NULL,
+    entity_versions_id INTEGER,
+    published_by INTEGER NOT NULL,
+    published_at TEXT NOT NULL,
+    kind TEXT NOT NULL,
+    FOREIGN KEY (entities_id) REFERENCES entities(id) ON DELETE CASCADE,
+    FOREIGN KEY (entity_versions_id) REFERENCES entity_versions(id) ON DELETE CASCADE,
+    FOREIGN KEY (published_by) REFERENCES subjects(id)
+  ) STRICT;
+CREATE TABLE advisory_locks (
+    id INTEGER PRIMARY KEY,
+    name TEXT NOT NULL,
+    handle INTEGER NOT NULL,
+    acquired_at TEXT NOT NULL,
+    renewed_at TEXT NOT NULL,
+    expires_at INTEGER NOT NULL,
+    lease_duration INTEGER NOT NULL,
+    CONSTRAINT advisory_locks_name UNIQUE (name)
+  ) STRICT;
+CREATE TABLE entity_published_references (
+    id INTEGER PRIMARY KEY,
+    from_entities_id INTEGER NOT NULL,
+    to_entities_id INTEGER NOT NULL,
+    FOREIGN KEY (from_entities_id) REFERENCES entities(id) ON DELETE CASCADE,
+    FOREIGN KEY (to_entities_id) REFERENCES entities(id) ON DELETE CASCADE
+  ) STRICT;
+CREATE TABLE entity_latest_references (
+    id INTEGER PRIMARY KEY,
+    from_entities_id INTEGER NOT NULL,
+    to_entities_id INTEGER NOT NULL,
+    FOREIGN KEY (from_entities_id) REFERENCES entities(id) ON DELETE CASCADE,
+    FOREIGN KEY (to_entities_id) REFERENCES entities(id) ON DELETE CASCADE
+  ) STRICT;
+CREATE TABLE entity_published_locations (
+    id INTEGER PRIMARY KEY,
+    entities_id INTEGER NOT NULL,
+    lat REAL NOT NULL,
+    lng REAL NOT NULL,
+    FOREIGN KEY (entities_id) REFERENCES entities(id) ON DELETE CASCADE
+  ) STRICT;
+CREATE TABLE entity_latest_locations (
+    id INTEGER PRIMARY KEY,
+    entities_id INTEGER NOT NULL,
+    lat REAL NOT NULL,
+    lng REAL NOT NULL,
+    FOREIGN KEY (entities_id) REFERENCES entities(id) ON DELETE CASCADE
+  ) STRICT;
+CREATE TABLE unique_index_values (
+    id INTEGER PRIMARY KEY,
+    entities_id INTEGER NOT NULL,
+    index_name TEXT NOT NULL,
+    value TEXT NOT NULL,
+    latest INTEGER NOT NULL DEFAULT FALSE,
+    published INTEGER NOT NULL DEFAULT FALSE,
+    FOREIGN KEY (entities_id) REFERENCES entities(id) ON DELETE CASCADE,
+    CONSTRAINT unique_index_values_index_value UNIQUE (index_name, value)
+  ) STRICT;
+CREATE INDEX unique_index_values_entities_id ON unique_index_values(entities_id);
+CREATE INDEX entity_versions_entities_id ON entity_versions(entities_id);
+CREATE INDEX entity_published_references_from_entities_id ON entity_published_references(from_entities_id);
+CREATE INDEX entity_published_locations_entities_id ON entity_published_locations(entities_id);
+CREATE INDEX entity_latest_references_from_entities_id ON entity_latest_references(from_entities_id);
+CREATE INDEX entity_latest_locations_entities_id ON entity_latest_locations(entities_id);
