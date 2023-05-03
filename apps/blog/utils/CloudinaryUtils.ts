4b5c685b51e7bdaa8b3841dccd4a260460b55a49
Jonas Bengtsson <jonas.b@gmail.com>
2023-05-03 22:35:57 +0200

    fix(blog): Use limit fill instead of limit fit for Cloudinary images

--- apps/blog/utils/CloudinaryUtils.ts
+++ apps/blog/utils/CloudinaryUtils.ts
@@ -1,5 +1,5 @@
 import type { Cloudinary } from '@cloudinary/url-gen';
-import { limitFit } from '@cloudinary/url-gen/actions/resize';
+import { limitFill } from '@cloudinary/url-gen/actions/resize';
 
 export function getResponsiveImageUrlsForLimitFit(
   cld: Cloudinary,
@@ -7,8 +7,8 @@ export function getResponsiveImageUrlsForLimitFit(
   width: number,
   height: number | null
 ) {
-  const url1x = getImageUrlForLimitFit(cld, publicId, width, height);
-  const url2x = getImageUrlForLimitFit(
+  const url1x = getImageUrlForLimitFill(cld, publicId, width, height);
+  const url2x = getImageUrlForLimitFill(
     cld,
     publicId,
     2 * width,
@@ -20,19 +20,19 @@ export function getResponsiveImageUrlsForLimitFit(
 }
 
 export function getOpenGraphImageUrlForLimitFit(cld: Cloudinary, publicId: string) {
-  return getImageUrlForLimitFit(cld, publicId, 1200, 630);
+  return getImageUrlForLimitFill(cld, publicId, 1200, 630);
 }
 
 export function getJsonLdImageUrlsForLimitFit(cld: Cloudinary, publicId: string) {
   //  Google recommends 16x9, 4x3, 1x1 aspect ratios, with 50k pixels min
   return [
-    getImageUrlForLimitFit(cld, publicId, 480, 480),
-    getImageUrlForLimitFit(cld, publicId, 480, 480 / (4 / 3)),
-    getImageUrlForLimitFit(cld, publicId, 480, 480 / (16 / 9)),
+    getImageUrlForLimitFill(cld, publicId, 480, 480),
+    getImageUrlForLimitFill(cld, publicId, 480, 480 / (4 / 3)),
+    getImageUrlForLimitFill(cld, publicId, 480, 480 / (16 / 9)),
   ];
 }
 
-export function getImageUrlForLimitFit(
+export function getImageUrlForLimitFill(
   cld: Cloudinary,
   publicId: string,
   width: number,
@@ -40,6 +40,6 @@ export function getImageUrlForLimitFit(
 ) {
   return cld
     .image(publicId)
-    .resize(limitFit(width, height ?? undefined))
+    .resize(limitFill(width, height ?? undefined))
     .toURL();
 }
