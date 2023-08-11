1756221082ae45de3bde22c9f23c3c7199f869bb
Jonas Bengtsson <jonas.b@gmail.com>
2023-08-11 11:53:16 +0200

    chore(ci): Update dependabot/fetch-metadata to extract updated dependency

--- tools/generic-tools/scripts/dependabot-make-consistent.js
+++ tools/generic-tools/scripts/dependabot-make-consistent.js
@@ -5,48 +5,37 @@ const path = require("path");
 
 const ROOT_DIR = path.resolve(path.join(__dirname, "..", "..", ".."));
 
-async function main(prTitle) {
-  const { dependency, directory } = extractInfoFromPrTitle(prTitle);
-
-  const projectDirectory = path.join(ROOT_DIR, directory);
-  const packageJsonPath = path.join(projectDirectory, "package.json");
-  const packageJson = JSON.parse(await fs.readFile(packageJsonPath));
-  const dependencyVersion = packageJson.dependencies?.[dependency];
-  const devDependencyVersion = packageJson.devDependencies?.[dependency];
-  const optionalDependencyVersion =
-    packageJson.optionalDependencies?.[dependency];
-  const versions = [
-    dependencyVersion,
-    devDependencyVersion,
-    optionalDependencyVersion,
-  ].filter((it) => it);
-  if (versions.length === 0) {
-    throw new Error(
-      `Can't find dependency ${dependency} in ${packageJsonPath}`
-    );
-  } else if (versions.length !== 1) {
-    throw new Error(
-      `Dependency ${dependency} is in both defined multiple times in ${filename}`
-    );
-  }
-  await makeDependencyConsistent(
-    projectDirectory,
-    dependency,
-    versions[0],
-    devDependencyVersion
-  );
-}
-
-// e.g. chore(deps-dev): Bump @typescript-eslint/eslint-plugin from 4.33.0 to 5.0.0 in /libraries/graphql
-function extractInfoFromPrTitle(prTitle) {
-  const prTitlePattern = /^.*Bump (\S+).* in (\S+)$/i;
-  const match = prTitle.match(prTitlePattern);
-  if (!match) {
-    throw new Error(
-      `PR title (${prTitle}) doesn't match expected format (${prTitlePattern})`
+async function main(dependencies) {
+  const directory = "tools/all-dependencies";
+  for (const dependency of dependencies) {
+    const projectDirectory = path.join(ROOT_DIR, directory);
+    const packageJsonPath = path.join(projectDirectory, "package.json");
+    const packageJson = JSON.parse(await fs.readFile(packageJsonPath));
+    const dependencyVersion = packageJson.dependencies?.[dependency];
+    const devDependencyVersion = packageJson.devDependencies?.[dependency];
+    const optionalDependencyVersion =
+      packageJson.optionalDependencies?.[dependency];
+    const versions = [
+      dependencyVersion,
+      devDependencyVersion,
+      optionalDependencyVersion,
+    ].filter((it) => it);
+    if (versions.length === 0) {
+      throw new Error(
+        `Can't find dependency ${dependency} in ${packageJsonPath}`
+      );
+    } else if (versions.length !== 1) {
+      throw new Error(
+        `Dependency ${dependency} is in both defined multiple times in ${filename}`
+      );
+    }
+    await makeDependencyConsistent(
+      projectDirectory,
+      dependency,
+      versions[0],
+      devDependencyVersion
     );
   }
-  return { dependency: match[1], directory: match[2] };
 }
 
 async function makeDependencyConsistent(
@@ -76,11 +65,7 @@ async function makeDependencyConsistent(
 
 if (require.main === module) {
   const argv = process.argv.slice(2);
-  if (argv.length !== 1) {
-    console.warn(`Expected PR title as argument, got arguments`, argv);
-    process.exit(1);
-  }
-  main(argv[0]).catch((error) => {
+  main(argv).catch((error) => {
     console.warn(error);
     process.exitCode = 1;
   });
