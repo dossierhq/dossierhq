e629f1d067b1cc64062a05fcf15e39b7b183884b
Jonas Bengtsson <jonas.b@gmail.com>
2023-09-04 13:19:45 +0200

    feat(blog): Sync database using sync events instead of old entity json approach

--- apps/blog/scripts/create-database-if-missing.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [ -f .env.local ] && grep -q "DATABASE_SQLITE_FILE" .env.local; then
-  echo "A database is already configured"
-  exit 0
-fi
-
-echo "DATABASE_SQLITE_FILE=public/database.sqlite" >> .env.local
-npx ts-node -T --esm ./scripts/create-database-from-disk.ts public/database.sqlite 2>&1
