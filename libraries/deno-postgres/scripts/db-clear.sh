bcdba56ec0d35a193d519353d5c5334280751772
Jonas Bengtsson <jonas.b@gmail.com>
2023-09-07 11:00:14 +0200

    chore(pg): Improve clear scripts, delete subjects too

--- libraries/deno-postgres/scripts/db-clear.sh
+++ libraries/deno-postgres/scripts/db-clear.sh
@@ -8,5 +8,5 @@ THIS_DIR="$(pushd "$(dirname "$0")" > /dev/null; pwd; popd > /dev/null)"
   set -a
   source "$THIS_DIR/../.env"
   set +a
-  time psql -s "$DATABASE_URL" -c 'DELETE FROM events; DELETE FROM entities; DELETE FROM schema_versions;'
+  time yes | psql -s "$DATABASE_URL" -c 'DELETE FROM events; DELETE FROM entities; DELETE FROM schema_versions; DELETE FROM subjects;'
 )
--- libraries/pg/scripts/db-clear.sh
+++ libraries/pg/scripts/db-clear.sh
@@ -8,5 +8,7 @@ THIS_DIR="$(pushd "$(dirname "$0")" > /dev/null; pwd; popd > /dev/null)"
   set -a
   source "$THIS_DIR/../.env"
   set +a
-  time psql -s "$DATABASE_URL" -c 'DELETE FROM events; DELETE FROM entities; DELETE FROM schema_versions;'
+  time yes | psql -s "$DATABASE_URL" -c 'DELETE FROM events; DELETE FROM entities; DELETE FROM schema_versions; DELETE FROM subjects;'
+  time yes | psql -s "$DATABASE_A_URL" -c 'DELETE FROM events; DELETE FROM entities; DELETE FROM schema_versions; DELETE FROM subjects;'
+  time yes | psql -s "$DATABASE_B_URL" -c 'DELETE FROM events; DELETE FROM entities; DELETE FROM schema_versions; DELETE FROM subjects;'
 )
