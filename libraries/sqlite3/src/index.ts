b3f84e39f0cc198f5b2332f0614bd6fe123f68e2
Jonas Bengtsson <jonas.b@gmail.com>
2023-09-20 21:28:14 +0200

    break(sqlite3): Remove sqlite3 library

--- libraries/sqlite3/src/index.ts
+++
@@ -1,7 +0,0 @@
-export type {
-  SqliteDatabaseOptimizationOptions,
-  SqliteDatabaseOptions,
-} from '@dossierhq/sqlite-core';
-export { createSqlite3Adapter } from './Sqlite3Adapter.js';
-export type { Sqlite3DatabaseAdapter } from './Sqlite3Adapter.js';
-export { createDatabase } from './SqliteUtils.js';
--- libraries/sqlite3/vitest.config.js
+++
@@ -1,6 +0,0 @@
-export default {
-  test: {
-    include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
-    watch: false,
-  },
-};
