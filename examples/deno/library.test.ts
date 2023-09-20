09e9a0092f25d4797f8c266b4b8acaa4e09b3e4f
Jonas Bengtsson <jonas.b@gmail.com>
2023-09-20 23:33:12 +0200

    chore: Remove dependency on template library

--- examples/deno/library.test.ts
+++
@@ -1,8 +0,0 @@
-import { TEMPLATE_VALUE } from "@dossierhq/template-library";
-import { assertEquals } from "std/testing/asserts.ts";
-
-Deno.test("copyEntity()", () => {
-  assertEquals(TEMPLATE_VALUE, {
-    value: "hello world",
-  });
-});
