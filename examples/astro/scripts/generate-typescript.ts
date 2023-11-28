bfc066f26d05f324b5d66e1a8fd6f8921e10a660
Jonas Bengtsson <jonas.b@gmail.com>
2023-11-28 21:25:12 +0100

    fix(ci): Add Placeholder type to generated schema if there are no entity types

--- examples/astro/scripts/generate-typescript.ts
+++ examples/astro/scripts/generate-typescript.ts
@@ -1,11 +1,20 @@
 #!/usr/bin/env -S npx tsx
-import { AdminSchema, createConsoleLogger, type AdminClient, type Logger } from '@dossierhq/core';
+import {
+  AdminSchemaWithMigrations,
+  createConsoleLogger,
+  type AdminClient,
+  type Logger,
+} from '@dossierhq/core';
 import { generateTypescriptForSchema } from '@dossierhq/typescript-generator';
 import { writeFile } from 'node:fs/promises';
 import { format, resolveConfig } from 'prettier';
 import { getAuthenticatedAdminClient, getServer } from '../src/dossier/utils/ServerUtils.js';
 
-async function generateTypes(logger: Logger, adminSchema: AdminSchema, filename: string) {
+async function generateTypes(
+  logger: Logger,
+  adminSchema: AdminSchemaWithMigrations,
+  filename: string,
+) {
   const publishedSchema = adminSchema.toPublishedSchema();
   const sourceCode = generateTypescriptForSchema({
     adminSchema,
@@ -23,16 +32,25 @@ async function generateTypes(logger: Logger, adminSchema: AdminSchema, filename:
   logger.info(`Wrote ${filename}`);
 }
 
-async function getAdminSchema(adminClient: AdminClient) {
-  const schemaResult = await adminClient.getSchemaSpecification();
-  return new AdminSchema(schemaResult.valueOrThrow());
+async function getAdminSchema(logger: Logger, adminClient: AdminClient) {
+  const schemaResult = await adminClient.getSchemaSpecification({ includeMigrations: true });
+  let adminSchema = new AdminSchemaWithMigrations(schemaResult.valueOrThrow());
+
+  if (adminSchema.getEntityTypeCount() === 0) {
+    logger.info('No entities found, adding placeholder with name "Placeholder"');
+    adminSchema = adminSchema
+      .updateAndValidate({ entityTypes: [{ name: 'Placeholder', fields: [] }] })
+      .valueOrThrow();
+  }
+
+  return adminSchema;
 }
 
 async function main() {
   const logger = createConsoleLogger(console);
   const server = (await getServer()).valueOrThrow();
   const adminClient = (await getAuthenticatedAdminClient('editor')).valueOrThrow();
-  const adminSchema = await getAdminSchema(adminClient);
+  const adminSchema = await getAdminSchema(logger, adminClient);
 
   await generateTypes(logger, adminSchema, './src/generated/SchemaTypes.ts');
 
