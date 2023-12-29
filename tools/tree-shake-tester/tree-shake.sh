92c034f4dffbfcf72c20c0efd5f79f9e532ad575
Jonas Bengtsson <jonas.b@gmail.com>
2023-12-29 13:46:45 +0100

    chore(tree-shake): Test using rollup

--- tools/tree-shake-tester/tree-shake.sh
+++ tools/tree-shake-tester/tree-shake.sh
@@ -1,18 +1,33 @@
 #!/usr/bin/env bash
 set -eu
 
-function tree_shake() {
+function bundle_esbuild() {
   INFILE=$1
-  OUTFILE=${INFILE%.*}-out.${INFILE##*.}
+  OUTFILE=${INFILE%.*}-esbuild.${INFILE##*.}
   ARGS=${@:2}
-  (
   npx esbuild --bundle "$INFILE" --outfile="$OUTFILE" --analyze=verbose --tree-shaking=true $ARGS 2>&1
-  )
 }
 
-tree_shake test-cases/better-sqlite3-empty.js --platform=node
-tree_shake test-cases/core-empty.js
-tree_shake test-cases/design-empty.js
-tree_shake test-cases/graphql-empty.js
-tree_shake test-cases/react-components-empty.js
-tree_shake test-cases/server-empty.js
+function bundle_rollup {
+  INFILE=$1
+  OUTFILE=${INFILE%.*}-rollup.${INFILE##*.}
+  ARGS=${@:2}
+  npx rollup "$INFILE" --file "$OUTFILE" --format iife -p @rollup/plugin-node-resolve $ARGS 2>&1
+}
+
+rm test-cases/*-esbuild.js || true
+rm test-cases/*-rollup.js || true
+
+bundle_esbuild test-cases/better-sqlite3-empty.js --platform=node
+bundle_esbuild test-cases/core-empty.js
+bundle_esbuild test-cases/design-empty.js
+bundle_esbuild test-cases/graphql-empty.js
+bundle_esbuild test-cases/react-components-empty.js
+bundle_esbuild test-cases/server-empty.js
+
+bundle_rollup test-cases/better-sqlite3-empty.js
+bundle_rollup test-cases/core-empty.js
+bundle_rollup test-cases/design-empty.js -p @rollup/plugin-commonjs
+bundle_rollup test-cases/graphql-empty.js -p @rollup/plugin-commonjs
+bundle_rollup test-cases/react-components-empty.js -p @rollup/plugin-commonjs
+bundle_rollup test-cases/server-empty.js
