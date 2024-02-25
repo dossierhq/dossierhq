30298ae43c9e08fd621b20abcdcc9e0557719b2a
Jonas Bengtsson <jonas.b@gmail.com>
2024-02-25 13:07:29 +0100

    chore(amal): Support running with tsx

---
+++ tools/better-sqlite3-amalgamation/src/start-server.ts
@@ -0,0 +1,53 @@
+import { createBetterSqlite3Adapter } from '@dossierhq/better-sqlite3';
+import { FieldType, createConsoleLogger } from '@dossierhq/core';
+import { NoneAndSubjectAuthorizationAdapter, createServer } from '@dossierhq/server';
+import Database from 'better-sqlite3';
+
+async function main() {
+  const logger = createConsoleLogger(console);
+  const database = new Database('databases/test.sqlite');
+  const databaseAdapterResult = await createBetterSqlite3Adapter({ logger }, database, {
+    migrate: true,
+    fts: { version: 'fts5' },
+    journalMode: 'wal',
+  });
+  const databaseAdapter = databaseAdapterResult.valueOrThrow();
+  const serverResult = await createServer({
+    databaseAdapter,
+    authorizationAdapter: NoneAndSubjectAuthorizationAdapter,
+    logger,
+  });
+  const server = serverResult.valueOrThrow();
+
+  try {
+    const sessionResult = await server.createSession({
+      provider: 'sys',
+      identifier: 'anonymous',
+      defaultAuthKeys: ['none'],
+      logger: null,
+      databasePerformance: null,
+    });
+    const session = sessionResult.valueOrThrow();
+
+    const adminClient = server.createAdminClient(session.context).toExceptionClient();
+
+    await adminClient.updateSchemaSpecification({
+      entityTypes: [
+        {
+          name: 'TitleOnly',
+          nameField: 'title',
+          fields: [{ name: 'title', type: FieldType.String }],
+        },
+      ],
+    });
+
+    await adminClient.createEntity({
+      info: { type: 'TitleOnly', name: 'Hello', authKey: 'none' },
+      fields: { title: 'Hello, World!' },
+    });
+  } finally {
+    server.shutdown();
+  }
+}
+
+await main();
