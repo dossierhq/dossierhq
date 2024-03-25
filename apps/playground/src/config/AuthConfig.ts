6c886294e723e8618729b14f342d2c6365056206
Jonas Bengtsson <jonas.b@gmail.com>
2024-03-25 23:01:44 +0100

    chore(play): Remove none authkey

--- apps/playground/src/config/AuthConfig.ts
+++ apps/playground/src/config/AuthConfig.ts
@@ -2,6 +2,5 @@ import type { DisplayAuthKey } from '@dossierhq/react-components';
 
 export const DISPLAY_AUTH_KEYS: DisplayAuthKey[] = [
   { authKey: '', displayName: 'Default' },
-  { authKey: 'none', displayName: 'None' },
   { authKey: 'subject', displayName: 'User private' },
 ];
