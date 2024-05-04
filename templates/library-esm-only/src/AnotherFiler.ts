7efd67fcdee54c4f7ebffca6ae2a1f2802d98083
Jonas Bengtsson <jonas.b@gmail.com>
2024-05-04 15:55:41 +0200

    chore(templates): Sort imports with prettier plugin

--- templates/library-esm-only/src/AnotherFiler.ts
+++ templates/library-esm-only/src/AnotherFiler.ts
@@ -1,5 +1,4 @@
-import type { TemplateInterface } from './TemplateFile.js';
-import { TEMPLATE_VALUE } from './TemplateFile.js';
+import { TEMPLATE_VALUE, type TemplateInterface } from './TemplateFile.js';
 
 export function hello() {
   const a: TemplateInterface = TEMPLATE_VALUE;
