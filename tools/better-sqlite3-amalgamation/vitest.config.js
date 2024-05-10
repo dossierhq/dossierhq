758d718bcfefdae00cc2571bd06fd28f000a2293
Jonas Bengtsson <jonas.b@gmail.com>
2024-05-10 11:46:48 +0200

    chore(amal): Include database-adapter and sqlite-core tests

--- tools/better-sqlite3-amalgamation/vitest.config.js
+++ tools/better-sqlite3-amalgamation/vitest.config.js
@@ -6,11 +6,17 @@ export default {
         import.meta.url
       ).pathname,
       '@dossierhq/core': new URL('../../libraries/core/src/index.ts', import.meta.url).pathname,
+      '@dossierhq/database-adapter': new URL(
+        '../../libraries/database-adapter/src/index.ts',
+        import.meta.url
+      ).pathname,
       '@dossierhq/integration-test': new URL(
         '../../libraries/integration-test/src/index.ts',
         import.meta.url
       ).pathname,
       '@dossierhq/server': new URL('../../libraries/server/src/index.ts', import.meta.url).pathname,
+      '@dossierhq/sqlite-core': new URL('../../libraries/sqlite-core/src/index.ts', import.meta.url)
+        .pathname,
     },
     coverage: {
       allowExternal: true,
@@ -19,7 +25,9 @@ export default {
       'src/**/*.test.ts',
       '../../libraries/better-sqlite3/src/**/*.test.ts',
       '../../libraries/core/src/**/*.test.ts',
+      '../../libraries/database-adapter/src/**/*.test.ts',
       '../../libraries/server/src/**/*.test.ts',
+      '../../libraries/sqlite-core/src/**/*.test.ts',
     ],
     watch: false,
   },
