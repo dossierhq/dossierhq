267f7ba84557553fd6725217476641190206d4cd
Jonas Bengtsson <jonas.b@gmail.com>
2024-05-28 23:04:13 +0200

    chore(r2): Initialize dossier in story

--- libraries/react-components2/src/stories/Placeholder.stories.tsx
+++ libraries/react-components2/src/stories/Placeholder.stories.tsx
@@ -1,13 +1,55 @@
 import { Button } from '@/components/ui/button';
+import { createConsoleLogger } from '@dossierhq/core';
+import { BackgroundEntityProcessorPlugin, createServer } from '@dossierhq/server';
+import { createSqlJsAdapter } from '@dossierhq/sql.js';
 import type { Meta, StoryObj } from '@storybook/react';
 import { fn } from '@storybook/test';
+import { useEffect, useState } from 'react';
+import type { Database, SqlJsStatic } from 'sql.js';
+import initSqlJs from 'sql.js/dist/sql-wasm';
+import sqlJsWasm from 'sql.js/dist/sql-wasm.wasm?url';
 
 function Placeholder({ onClick }: { onClick: () => void }) {
+  const [_init, setInit] = useState<{ db: Database } | null>(null);
+
+  useEffect(() => {
+    (async () => {
+      const SQL = await getSql();
+      const db = new SQL.Database();
+      const logger = createConsoleLogger(console);
+
+      const databaseAdapter = (
+        await createSqlJsAdapter({ logger }, db, {
+          migrate: true,
+          fts: { version: 'fts4' },
+          journalMode: 'memory',
+        })
+      ).valueOrThrow();
+      const server = (await createServer({ databaseAdapter })).valueOrThrow();
+
+      const processorPlugin = new BackgroundEntityProcessorPlugin(server, logger);
+      server.addPlugin(processorPlugin);
+      processorPlugin.start();
+
+      setInit({ db });
+    })();
+  }, []);
+
   return (
     <div>
       <Button onClick={onClick}>Placeholder</Button>
     </div>
   );
+}
+
+let sqlPromise: Promise<SqlJsStatic> | null = null;
+async function getSql() {
+  if (!sqlPromise) {
+    sqlPromise = initSqlJs({
+      locateFile: (_file) => sqlJsWasm,
+    });
+  }
+  return sqlPromise;
 }
 
 const meta = {
