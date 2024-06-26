19c18e267e231255fd7d9593f9044f919983a40b
Jonas Bengtsson <jonas.b@gmail.com>
2024-06-26 09:42:28 +0200

    chore(ci): Increase long test timeout

--- libraries/libsql/src/test/TestUtils.ts
+++ libraries/libsql/src/test/TestUtils.ts
@@ -4,7 +4,7 @@ import { describe, it } from 'vitest';
 export function registerTestSuite(testSuiteName: string, testSuite: TestSuite): void {
   describe(testSuiteName, () => {
     for (const [testName, testFunction] of Object.entries(testSuite)) {
-      it(testName, { timeout: testFunction.timeout === 'long' ? 10_000 : undefined }, testFunction);
+      it(testName, { timeout: testFunction.timeout === 'long' ? 20_000 : undefined }, testFunction);
     }
   });
 }
