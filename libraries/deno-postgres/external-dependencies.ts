9e5ee5d81987b15ab2a318e076c5690ae52a658e
Jonas Bengtsson <jonas.b@gmail.com>
2024-07-13 15:32:22 +0200

    chore(deno): Tweak import maps/dependencies

--- libraries/deno-postgres/external-dependencies.ts
+++ libraries/deno-postgres/external-dependencies.ts
@@ -1,4 +1,6 @@
 import type {} from "postgres";
 import type {} from "std/encoding/base64.ts";
+import type {} from "tslib";
+import type {} from "uuid";
 
 // This file is used to cache external dependencies for postinstall
