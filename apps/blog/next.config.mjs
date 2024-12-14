39dcb9ebdcca5512d747b1614bd02e654f3ebddf
Jonas Bengtsson <jonas.b@gmail.com>
2024-12-14 11:36:41 +0100

    Revert "chore(deps): Upgrade to Next 15"
    This reverts commit 2f2e58902fc049b1c5635505d9ff6a88b0129454.

--- apps/blog/next.config.mjs
+++ apps/blog/next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const config = {
   reactStrictMode: true,
+  swcMinify: true,
   productionBrowserSourceMaps: true,
   images: {
     domains: ['res.cloudinary.com'],
