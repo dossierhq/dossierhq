fa36456264145f0e2d4f8c769d850891e2e76b57
Jonas Bengtsson <jonas.b@gmail.com>
2025-02-08 14:31:26 +0100

    chore(deps): Upgrade eslint to 9 in react-components2

--- apps/playground2/eslint.config.js
+++ apps/playground2/eslint.config.js
@@ -1,6 +1,6 @@
+import reactCompilerConfig from '@dossierhq/eslint-config/add-react-compiler.js';
 import sharedConfig from '@dossierhq/eslint-config/vite.js';
-import reactCompiler from 'eslint-plugin-react-compiler';
 
 /** @import { Linter } from "eslint" */
 /** @type {Linter.Config[]} */
-export default [...sharedConfig, reactCompiler.configs.recommended];
+export default [...sharedConfig, ...reactCompilerConfig];
---
+++ libraries/eslint-config/add-react-compiler.js
@@ -0,0 +1,5 @@
+import reactCompiler from "eslint-plugin-react-compiler";
+
+/** @import { Linter } from "eslint" */
+/** @type {Linter.Config[]} */
+export default [reactCompiler.configs.recommended];
