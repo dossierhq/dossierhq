84e94e59e7bad6f736ec08c6a0142fa5388c7c4b
Jonas Bengtsson <jonas.b@gmail.com>
2025-02-09 13:19:25 +0100

    chore(play2): Remove react compiler 18 compatibility

--- apps/playground2/vite.config.ts
+++ apps/playground2/vite.config.ts
@@ -3,9 +3,7 @@ import react from '@vitejs/plugin-react';
 import { visualizer } from 'rollup-plugin-visualizer';
 import { defineConfig } from 'vite';
 
-const ReactCompilerConfig = {
-  runtimeModule: '@/mycache',
-};
+const ReactCompilerConfig = {};
 
 // https://vitejs.dev/config/
 export default defineConfig({
