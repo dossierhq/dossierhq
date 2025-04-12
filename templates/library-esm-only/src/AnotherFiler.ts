3e78d2a5101b7d5bace501428cc7c7d77093f849
Jonas Bengtsson <jonas.b@gmail.com>
2025-04-12 12:19:03 +0200

    chore(ci): Start with knip

--- templates/library-esm-only/src/AnotherFiler.ts
+++
@@ -1,6 +0,0 @@
-import { TEMPLATE_VALUE, type TemplateInterface } from './TemplateFile.js';
-
-export function hello(): TemplateInterface {
-  const a: TemplateInterface = TEMPLATE_VALUE;
-  return a;
-}
