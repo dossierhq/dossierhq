0eed0aa125b5bf71b3717c8c3fc253373bc38ecb
Jonas Bengtsson <jonas.b@gmail.com>
2025-05-03 14:09:49 +0200

    chore(turbo): Add db:clear task

--- libraries/deno-postgres/scripts/db-clear.sh
+++ libraries/deno-postgres/scripts/db-clear.sh
@@ -8,5 +8,7 @@ THIS_DIR="$(pushd "$(dirname "$0")" > /dev/null; pwd; popd > /dev/null)"
   set -a
   source "$THIS_DIR/../.env"
   set +a
+  echo "(Clearing database automatically, ignore "press return to proceed")"
   time yes | psql -s "$DATABASE_URL" -c 'DELETE FROM events; DELETE FROM entities; DELETE FROM schema_versions; DELETE FROM subjects;'
+  echo "Done"
 )
--- libraries/pg/scripts/db-clear.sh
+++ libraries/pg/scripts/db-clear.sh
@@ -8,7 +8,11 @@ THIS_DIR="$(pushd "$(dirname "$0")" > /dev/null; pwd; popd > /dev/null)"
   set -a
   source "$THIS_DIR/../.env"
   set +a
+  echo "(Clearing database 1/3 automatically, ignore "press return to proceed")"
   time yes | psql -s "$DATABASE_URL" -c 'DELETE FROM events; DELETE FROM entities; DELETE FROM schema_versions; DELETE FROM subjects;'
+  echo "(Clearing database 2/3 automatically, ignore "press return to proceed")"
   time yes | psql -s "$DATABASE_A_URL" -c 'DELETE FROM events; DELETE FROM entities; DELETE FROM schema_versions; DELETE FROM subjects;'
+  echo "(Clearing database 3/3 automatically, ignore "press return to proceed")"
   time yes | psql -s "$DATABASE_B_URL" -c 'DELETE FROM events; DELETE FROM entities; DELETE FROM schema_versions; DELETE FROM subjects;'
+  echo "Done"
 )
