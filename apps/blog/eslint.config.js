6fad284ccb93b9293efa115f76fbcdfb098334b6
Jonas Bengtsson <jonas.b@gmail.com>
2025-11-19 21:39:28 +0100

    chore(deps): Upgrade Next.js 16

--- apps/blog/eslint.config.js
+++ apps/blog/eslint.config.js
@@ -2,4 +2,4 @@ import sharedConfig from '@dossierhq/eslint-config/next.js';
 
 /** @import { Linter } from "eslint" */
 /** @type {Linter.Config[]} */
-export default [...sharedConfig, { ignores: ['public/*.js', 'next-env.d.ts'] }];
+export default [...sharedConfig, { ignores: ['public/*.js'] }];
